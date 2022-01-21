7129619d514c610a5cdb839dbf297a72bb184a9b
jasonandrew <jasonxandrewth@gmail.com>
2022-01-21 21:17:18 +0100

    everything is centered now

--- components/Animation/styles.js
+++ components/Animation/styles.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import { breakpoints } from "styles/styles";
 
 export const SvgContainer = styled.div`
-    height: 100vh;
-    width: 100vw;
-    position: absolute;
-    top: 50%;  
-    left: 50%; 
-    transform: translate(-50%, -50%);
-  
+    div {
+        height: 100vh;
+        width: 100vw;
+        position: absolute;
+        top: 50%;  
+        left: 50%; 
+        transform: translate(-50%, -50%);  
+    }
 `;
