ac76a99019726beb35be18d20f6c8c09daa181a0
jasonandrew <jasonxandrewth@gmail.com>
2022-04-21 12:14:37 +0200

    mobile and desktop  center

--- components/Animation/styles.ts
+++ components/Animation/styles.ts
@@ -5,9 +5,14 @@ export const SvgContainer = styled.div`
     div {
         // height: calc(var(--vh, 1vh) * 100);
         width: 100vw;
+        height: 100vh;
         position: absolute;
         top: 50%;  
         left: 50%; 
         transform: translate(-50%, -50%);  
+
+        @media (max-width: ${breakpoints.m}px) {
+            height: 100%;
+          }
     }
 `;
