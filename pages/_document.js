e6c374fceca4b6dbb69de1804205687f39044c1d
little-portland <110906749+little-portland@users.noreply.github.com>
2022-09-22 13:37:34 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -43,6 +43,9 @@ export default class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
                         rel="stylesheet"
                     />
+                   <style>
+                        @import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+                    </style>
                 </Head>
                 <body>
                     <Main />
