427feb8615fc4cb03a45e29e2dce2fe86ebaef1f
little-portland <110906749+little-portland@users.noreply.github.com>
2022-09-28 18:30:25 +0100

    Update styles.ts

--- styles/styles.ts
+++ styles/styles.ts
@@ -10,6 +10,8 @@ export const breakpoints = {
 
 export const GlobalStyle = createGlobalStyle`
 
+@import url("https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+
 * {
   box-sizing: border-box;
   margin: 0;
