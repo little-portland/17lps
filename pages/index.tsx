d9e1573a69c645ab2fda937240d2f9bd71071ecd
little-portland <110906749+little-portland@users.noreply.github.com>
2022-09-28 18:49:52 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,11 @@ import Animation from "@components/Animation";
 import Layout from "@components/Layout";
 import { AnimatePresence } from "framer-motion";
 
+<link
+href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+rel="stylesheet"
+/>
+
 //hooks
 import useFetchContent from "@utils/useFetchContent";
 
