24362fb91446bc0499943ae7e7e3ff3da1bae248
little-portland <110906749+little-portland@users.noreply.github.com>
2022-12-07 11:30:28 +0000

    Added full stops to body text sentences

--- pages/bookings.tsx
+++ pages/bookings.tsx
@@ -55,14 +55,14 @@ const Bookings = () => {
         <h1 className="bookings-heading-1">Large Groups</h1>
           <ul>
             <li>For larger group bookings of 10 or more, please email us at <a href="mailto:eat@little-portland.com">eat@little-portland.com</a>.</li>
-            <li>Large groups can choose from 2 set menu options. <a href="https://www.little-portland.com/menu-options">Click here</a> to see the menus</li>
+            <li>Large groups can choose from 2 set menu options. <a href="https://www.little-portland.com/menu-options">Click here</a> to see the menus.</li>
           </ul>
           <h1 className="bookings-heading-2">Bookings &amp; Cancellations</h1>
           <ul>
-            <li>We ask that any changes to your reservation be made at least 48 hours before the reservation time in order to avoid a late cancellation fee</li>
-            <li>Late cancellations or no-shows will be subject to a £30pp late cancellation fee</li>
-            <li>Last minute reductions in group sizes may also be charged a cancellation fee</li>
-            <li>Children under the age of 16 are not permitted to enter the venue</li>
+            <li>We ask that any changes to your reservation be made at least 48 hours before the reservation time in order to avoid a late cancellation fee.</li>
+            <li>Late cancellations or no-shows will be subject to a £30pp late cancellation fee.</li>
+            <li>Last minute reductions in group sizes may also be charged a cancellation fee.</li>
+            <li>Children under the age of 16 are not permitted to enter the venue.</li>
           </ul>
         </div>
 
