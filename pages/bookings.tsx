ec1a8b36be8305031c6c9498d017d49601245b10
little-portland <110906749+little-portland@users.noreply.github.com>
2023-01-11 12:28:38 +0000

    Changes to Sundays bullet points

--- pages/bookings.tsx
+++ pages/bookings.tsx
@@ -54,9 +54,9 @@ const Bookings = () => {
         <div className="bookings-body-text">
         <h1 className="bookings-heading-0">Sundays</h1>
           <ul>
-            <li>Sundays are ticketed. You can buy tickets <a href="https://www.little-portland.com/events">here</a>.</li>
-            <li>Tickets are £30 and include entry and food (all you can eat).</li>
-            <li>Opening hours are 1pm-11pm (kitchen closes at 10pm).</li>
+            <li>Sundays are ticketed. You can buy tickets <a href="https://www.little-portland.com/events">here</a>. Limited tickets will be available on the door on the day. No promises tho, cuz.</li>
+            <li>Please note this is a party booking, not a restaurant booking. Your ticket purchase allows you entry between 1pm-11pm. Feel free to arrive whenever, cuz.</li>
+            <li>Check our instagram post for the menu. Ordering not required, food will be sent out throughout the day until we run out. There will be no changes to the menu, cuz.</li>
           </ul>
         <h1 className="bookings-heading-1">Large Groups</h1>
           <ul>
