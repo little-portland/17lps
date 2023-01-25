f657670c924c8da4eaf1a0dbdad90e48f2b204cf
little-portland <110906749+little-portland@users.noreply.github.com>
2023-01-25 14:11:13 +0000

    Change file to valentines flyer

--- pages/valentines-flyer.tsx
+++ pages/valentines-flyer.tsx
@@ -18,12 +18,12 @@ const Menu = ({ menuImage }) => {
       <CenterContainer>
         {menuImage && (
           <Image
-            src={"/images/chinese-ny-flyer.jpg"} 
+            src={"/images/valentines-flyer.jpg"} 
             alt={menuImage.title}
             className={"image"}
             width={menuImage.width} //automatically provided
             height={menuImage.height} //automatically provided
-            blurDataURL={"/images/chinese-ny-flyer.jpg"} //automatically provided
+            blurDataURL={"/images/valentines-flyer.jpg"} //automatically provided
             placeholder="blur" // Optional blur-up while loading
           />
         )}
