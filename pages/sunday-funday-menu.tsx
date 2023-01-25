9928129d213301d75904e802f3d119c66bb446f2
little-portland <110906749+little-portland@users.noreply.github.com>
2023-01-25 15:49:13 +0000

    Update menu image

--- pages/sunday-funday-menu.tsx
+++ pages/sunday-funday-menu.tsx
@@ -18,12 +18,12 @@ const Menu = ({ menuImage }) => {
       <CenterContainer>
         {menuImage && (
           <Image
-            src={"/images/sunday_funday_menu_instaV3.jpg"} 
+            src={"/images/sunday_funday_menu.jpg"} 
             alt={menuImage.title}
             className={"image"}
             width={menuImage.width} //automatically provided
             height={menuImage.height} //automatically provided
-            blurDataURL={"/images/sunday_funday_menu_instaV3.jpg"} //automatically provided
+            blurDataURL={"/images/sunday_funday_menu.jpg"} //automatically provided
             placeholder="blur" // Optional blur-up while loading
           />
         )}
