89ff910e018166fb44eb183b00699e648b130a43
little-portland <110906749+little-portland@users.noreply.github.com>
2023-02-08 17:05:47 +0000

    Update sunday funday flyer to 12 feb

--- pages/sunday-funday-flyer.tsx
+++ pages/sunday-funday-flyer.tsx
@@ -18,12 +18,12 @@ const Menu = ({ menuImage }) => {
       <CenterContainer>
         {menuImage && (
           <Image
-            src={"/images/sunday-funday-flyer-29-jan.jpg"} 
+            src={"/images/sunday-funday-poster-12-feb.jpg"} 
             alt={menuImage.title}
             className={"image"}
             width={menuImage.width} //automatically provided
             height={menuImage.height} //automatically provided
-            blurDataURL={"/images/sunday-funday-flyer-29-jan.jpg"} //automatically provided
+            blurDataURL={"/images/sunday-funday-poster-12-feb.jpg"} //automatically provided
             placeholder="blur" // Optional blur-up while loading
           />
         )}
