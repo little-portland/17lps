0add817cca22d7cd7073b56611a8f5228cd01eec
little-portland <110906749+little-portland@users.noreply.github.com>
2023-05-19 16:50:54 +0100

    Update booking-options.jsx

--- pages/booking-options.jsx
+++ pages/booking-options.jsx
@@ -1,87 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
-import Image from "next/image";
-
-//Components
-import CenterContainer from "@components/UX/CenterContainer/CenterContainer";
+import Script from "next/script";
 
 //hooks
-import useFetchContent from "@utils/useFetchContent";
+import useDeviceDetect from "@utils/useDeviceDetect";
 
-import Button from "@components/UX/Button";
+import CenterContainer from "@components/UX/CenterContainer/CenterContainer";
 
-import {
-  ButtonWrapper
-} from "../components/UX/Modal/styles";
+const BookingOptions = () => {
 
-const SundayMenu = ({ menuImage }) => {
   return (
     <>
       <Head>
-        <title>Sunday Menu</title>
+        <title>Bookings</title>
       </Head>
 
       <CenterContainer>
-          <ButtonWrapper className="button-wrapper">
-            {email && (
-              <a href={`mailto:${email}`}>
-                <Button
-                  classes="events-button"
-                  btnType={isMobile ? "hollow" : "solid"}
-                >
-                  {isMobile ? "email" : email}
-                </Button>
-              </a>
-            )}
-            {phone &&
-              (isMobile ? (
-                <a href={`tel:+${phone.replace(/\s/g, "")}`}>
-                  <Button classes="events-button" btnType="hollow">
-                    call
-                  </Button>
-                </a>
-              ) : (
-                <CopyToClipboard text={phone} onCopy={() => setCopied(true)}>
-                  <Button
-                    classes="events-button"
-                    onClick={clickHandler}
-                    btnType="hollow"
-                  >
-                    {phone}
-                  </Button>
-                </CopyToClipboard>
-              ))}
-          </ButtonWrapper>
+          <ul>
+            <li><a target="_blank" href="https://www.little-portland.com/bookings">Book Dinner (includes free club entry)</a>.</li>
+            <li><a target="_blank" href="https://www.little-portland.com/bookings">Buy Ticket / Get on Guest List</a>.</li>
+          </ul>
       </CenterContainer>
     </>
   );
 };
-export default SundayMenu;
 
-export async function getStaticProps() {
-  const sundaymenuData = await useFetchContent(`
-    {
-      sundayMenuCollection {
-        items {
-          sundayMenuImage {
-            title
-            description
-            url
-            width
-            height
-          }
-        }
-      }
-    }
-  `);
-
-  const menuImage =
-    sundaymenuData.sundayMenuCollection.items[0].sundayMenuImage;
-
-  return {
-    props: {
-      menuImage,
-    }, // will be passed to the page component as props
-    revalidate: 30, // In seconds
-  };
-}
+export default BookingOptions;
