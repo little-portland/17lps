9fff503697344409574bdd1aaf7ea01ac647412e
little-portland <110906749+little-portland@users.noreply.github.com>
2023-05-26 13:07:49 +0100

    Update booking-options.jsx

--- pages/booking-options.jsx
+++ pages/booking-options.jsx
@@ -19,13 +19,22 @@ const BookingOptions = () => {
 
       <div id="bookingOptions">
         <h1 class="event-name">Thursday Underground</h1>
+        <div class="event-info">
+            <div class="date">
+              <h2>01 June</h2>
+            </div>
+            <div class="artists">
+              <h3><span>STUDIO // </span>Ralph Lawson, Subb-An</h3>
+              <h3><span>TENT // </span>Georgia, Tintin</h3>
+            </div>
+        </div>
           <a href="https://www.little-portland.com/bookings">
-            <Button classes="events-button" btnType="solid">
+            <Button classes="events-button" btnType="hollow">
               Book Dinner (includes free club entry)
             </Button>
           </a>
           <a href="https://www.little-portland.com/bookings">
-            <Button classes="events-button" btnType="solid">
+            <Button classes="events-button" btnType="hollow">
               Buy Ticket / Get on Guest List
             </Button>
           </a>
