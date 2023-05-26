46d8e8b263d9c7e6f2d554a67e61da9feec2ec7f
little-portland <110906749+little-portland@users.noreply.github.com>
2023-05-26 16:09:30 +0100

    Update booking-options.jsx

--- pages/booking-options.jsx
+++ pages/booking-options.jsx
@@ -28,7 +28,7 @@ const BookingOptions = () => {
               <h3><span>TENT // </span>Georgia, Tintin</h3>
             </div>
         </div>
-        <div class"button-wrapper">
+        <div class="button-wrapper">
           <a href="https://www.little-portland.com/bookings">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner (includes free club entry)
