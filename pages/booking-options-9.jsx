6309fd088e073d0eacf6167ea3254962db76b170
little-portland <110906749+little-portland@users.noreply.github.com>
2023-05-30 12:39:47 +0100

    Create booking-options-9.jsx

---
+++ pages/booking-options-9.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Script from "next/script";
+
+//hooks
+import useDeviceDetect from "@utils/useDeviceDetect";
+
+import CenterContainer from "@components/UX/CenterContainer/CenterContainer";
+
+import Button from "@components/UX/Button";
+
+const BookingOptions = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Bookings </title>
+      </Head>
+
+      <div id="bookingOptions">
+        <h1 class="event-name">DISCO SATURDAYS</h1>
+        <div class="event-info">
+            <div class="info date">
+              <h2>17 Jun</h2>
+            </div>
+            <div class="info artists">
+              <h3><span>STUDIO <b>//</b> </span>SUPER PAOLO</h3>
+              <h3><span>TENT <b>//</b> </span>FUNKTONIO, MARI*</h3>
+            </div>
+        </div>
+        <div class="button-wrapper">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2023-06-17" target="_blank">
+            <Button classes="events-button"  btnType="hollow">
+              Book Dinner <span>(includes free club entry)</span>
+            </Button>
+          </a>
+          <a href="http://sevn.ly/x9Kl9yZm" target="_blank">
+            <Button classes="events-button ticket" btnType="hollow">
+              Book Club <span>Ticket</span>
+            </Button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookingOptions;
