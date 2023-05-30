a52c40738b328c40e64bf7d1a78b4f0dcb0cd6b6
little-portland <110906749+little-portland@users.noreply.github.com>
2023-05-30 12:31:53 +0100

    Create booking-options-6.jsx

---
+++ pages/booking-options-6.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Script from "next/script";
+
+//hooks
+import useDeviceDetect from "@utils/useDeviceDetect";
+
+import CenterContainer from "@components/UX/CenterContainer/CenterContainer";
+
+import Button from "@components/UX/Button";
+
+const BookingOptions = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Bookings </title>
+      </Head>
+
+      <div id="bookingOptions">
+        <h1 class="event-name">FRIDAY RESIDENTS</h1>
+        <div class="event-info">
+            <div class="info date">
+              <h2>16 Jun</h2>
+            </div>
+            <div class="info artists">
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
+              <h3><span>TENT <b>//</b> </span>GILES SMITH, ROBERTA CUTOLO</h3>
+            </div>
+        </div>
+        <div class="button-wrapper">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2023-06-16" target="_blank">
+            <Button classes="events-button"  btnType="hollow">
+              Book Dinner <span>(includes free club entry)</span>
+            </Button>
+          </a>
+          <a href="http://sevn.ly/xreVojrY" target="_blank">
+            <Button classes="events-button ticket" btnType="hollow">
+              GET ON CLUB <span>GUESTLIST</span>
+            </Button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookingOptions;
