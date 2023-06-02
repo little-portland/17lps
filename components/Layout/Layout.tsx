6718892bae690ee90e7fbf083d52b0e8554c8745
little-portland <110906749+little-portland@users.noreply.github.com>
2023-06-02 11:34:11 +0100

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -119,7 +119,7 @@ const Layout: React.FC<IProps> = ({ main, eatItem, hireItem }) => {
           </div>
           <div className="button-wrapper">
             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-07" target="_blank">
-              <Button className="events-button"  btnType="hollow">
+              <Button btnType="hollow">
                 Book Dinner <span>(includes free club entry)</span>
               </Button>
             </a>
@@ -136,7 +136,7 @@ const Layout: React.FC<IProps> = ({ main, eatItem, hireItem }) => {
           </div>
           <div className="button-wrapper">
             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-14" target="_blank">
-              <Button className="events-button"  btnType="hollow">
+              <Button btnType="hollow">
                 Book Dinner <span>(includes free club entry)</span>
               </Button>
             </a>
