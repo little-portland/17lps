ff8938aa977979113eb789157dcd9ddd743a8594
little-portland <110906749+little-portland@users.noreply.github.com>
2023-06-02 11:14:06 +0100

    Add Wednesday Jazz boxes

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -98,14 +98,51 @@ const Layout: React.FC<IProps> = ({ main, eatItem, hireItem }) => {
         }}
         // phoneNr={`tel:+${testNr.replace(/\s/g, "")}`}
       >
-       <Image
-          src={"/images/Eat.jpeg"}
-          alt={eatItem.image.description}
-          width={eatItem ? eatItem.image.width : 300} //automatically provided
-          height={eatItem ? eatItem.image.height : 800} //automatically provided
-          blurDataURL={"/images/Eat.jpeg"} //automatically provided
-          placeholder="blur" // Optional blur-up while loading
-        />
+    {<div
+        className="eat-note"
+      >     
+        <h1>The Tent</h1>
+        <h2>[At the end of the Universe]</h2>
+        <h3>What’s On</h3>
+            
+        <div id="bookingOptions">
+          <h1 class="event-name">Wednesday’s Jazz</h1>
+          <div class="event-info">
+              <div class="info date">
+                <h2>07 Jun</h2>
+              </div>
+              <div class="info artists">
+                <h3><span>STUDIO <b>//</b> </span>CLOSED</h3>
+                <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & MAFALDA RAMOS</h3>
+              </div>
+          </div>
+          <div class="button-wrapper">
+            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-07" target="_blank">
+              <Button classes="events-button"  btnType="hollow">
+                Book Dinner <span>(includes free club entry)</span>
+              </Button>
+            </a>
+          </div>
+          
+           <div class="event-info">
+              <div class="info date">
+                <h2>14 Jun</h2>
+              </div>
+              <div class="info artists">
+                <h3><span>STUDIO <b>//</b> </span>CLOSED</h3>
+                <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & JUNIOR TONIATO</h3>
+              </div>
+          </div>
+          <div class="button-wrapper">
+            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-14" target="_blank">
+              <Button classes="events-button"  btnType="hollow">
+                Book Dinner <span>(includes free club entry)</span>
+              </Button>
+            </a>
+          </div>
+        </div>
+            
+      </div>}
       </Modal>
       <Modal
         open={displayHire}
