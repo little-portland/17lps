53ec38b62e6e2a60517cf11a3c124df6e7f5d1fd
little-portland <110906749+little-portland@users.noreply.github.com>
2023-06-02 10:45:10 +0100

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -21,15 +21,15 @@ const BookingOptions = () => {
         <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>02 Jun</h2>
+              <h2>23 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>David Triana & Friends</h3>
-              <h3><span>TENT <b>//</b> </span>Harri Pepper, Paul Day</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
+              <h3><span>TENT <b>//</b> </span>MR SHIVER, PAUL DAY</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-02" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-23" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
