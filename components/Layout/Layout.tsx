4bac71a958384d7346c0fce3e6c1b46623cab7be
little-portland <110906749+little-portland@users.noreply.github.com>
2023-06-02 11:21:19 +0100

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -106,36 +106,36 @@ const Layout: React.FC<IProps> = ({ main, eatItem, hireItem }) => {
         <h3>What’s On</h3>
             
         <div id="bookingOptions">
-          <h1 class="event-name">Wednesday’s Jazz</h1>
-          <div class="event-info">
-              <div class="info date">
+          <h1 className="event-name">Wednesday’s Jazz</h1>
+          <div className="event-info">
+              <div className="info date">
                 <h2>07 Jun</h2>
               </div>
-              <div class="info artists">
+              <div className="info artists">
                 <h3><span>STUDIO <b>//</b> </span>CLOSED</h3>
                 <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & MAFALDA RAMOS</h3>
               </div>
           </div>
-          <div class="button-wrapper">
+          <div className="button-wrapper">
             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-07" target="_blank">
-              <Button classes="events-button"  btnType="hollow">
+              <Button className="events-button"  btnType="hollow">
                 Book Dinner <span>(includes free club entry)</span>
               </Button>
             </a>
           </div>
           
-           <div class="event-info">
-              <div class="info date">
+           <div className="event-info">
+              <div className="info date">
                 <h2>14 Jun</h2>
               </div>
-              <div class="info artists">
+              <div className="info artists">
                 <h3><span>STUDIO <b>//</b> </span>CLOSED</h3>
                 <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & JUNIOR TONIATO</h3>
               </div>
           </div>
-          <div class="button-wrapper">
+          <div className="button-wrapper">
             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-14" target="_blank">
-              <Button classes="events-button"  btnType="hollow">
+              <Button className="events-button"  btnType="hollow">
                 Book Dinner <span>(includes free club entry)</span>
               </Button>
             </a>
