0819065aeb94ef468669c2831ee07b51516a16ec
little-portland <110906749+little-portland@users.noreply.github.com>
2023-06-02 10:53:39 +0100

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>TBA</h3>
-              <h3><span>TENT <b>//</b> </span>DEMI , GREGORIO [LIVE MODULAR SET]</h3>
+              <h3><span>TENT <b>//</b> </span>DEMI, GREGORIO [LIVE MODULAR SET]</h3>
             </div>
         </div>
         <div class="button-wrapper">
