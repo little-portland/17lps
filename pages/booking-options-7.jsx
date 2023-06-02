d5b363a7bf12e02e636e261932a4514c6377c332
little-portland <110906749+little-portland@users.noreply.github.com>
2023-06-02 10:46:56 +0100

    Update booking-options-7.jsx

--- pages/booking-options-7.jsx
+++ pages/booking-options-7.jsx
@@ -18,18 +18,18 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name function-presents">function (NYC) presents</h1>
+        <h1 class="event-name disco-saturdays">DISCO SATURDAYS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>03 Jun</h2>
+              <h2>24 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, LUKE SOLOMON, MAKADSI</h3>
-              <h3><span>TENT <b>//</b> </span>NICK WILLIAMS, SUPER PAOLO</h3>
+              <h3><span>STUDIO <b>//</b> </span>SUPER PAOLO, TRIANA Y HERMANA JUANA [LIVE]</h3>
+              <h3><span>TENT <b>//</b> </span>GREG SONATA, KEM</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-03" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-06-24" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
