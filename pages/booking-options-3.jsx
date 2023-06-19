357df8e43dd75591be87a0a87e9f366577ae2871
little-portland <110906749+little-portland@users.noreply.github.com>
2023-06-19 12:37:27 +0100

    Update booking-options-3.jsx

--- pages/booking-options-3.jsx
+++ pages/booking-options-3.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name" class="focal-point">Focal Point Presents</h1>
+        <h1 class="event-name" class="openlab-presents">Focal Point Presents</h1>
         <div class="event-info">
             <div class="info date">
               <h2>06 Jul</h2>
