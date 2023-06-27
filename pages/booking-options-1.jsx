e767b21b864ee93ffc9279631e5e1b5be865c40d
little-portland <110906749+little-portland@users.noreply.github.com>
2023-06-27 12:13:40 +0100

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>13 Jul</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>DANIEL BELL, SHAUN REEVES	</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
               <h3><span>TENT <b>//</b> </span>DAVID TRIANA, PAUL DAY</h3>
             </div>
         </div>
