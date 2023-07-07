13b5eaf66d828de6a87a34fb8412f261eb7aa300
little-portland <110906749+little-portland@users.noreply.github.com>
2023-07-07 12:17:08 +0100

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -18,23 +18,23 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name urban-future">Urban Future</h1>
+        <h1 class="event-name">Thursdays Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>05 Jul</h2>
+              <h2>27 Jul</h2>
             </div>
             <div class="info artists">
-              <h3><span>JAZZ AT THE END OF THE UNIVERSE[8PM] <b>//</b> </span>MARIO BAKUNA & MAFALDA RAMOS</h3>
-              <h3><span>MIDNIGHT MIXTAPE[11PM] <b>//</b> </span>BILL PATRICK</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>TENT <b>//</b> </span>Harri Pepper, Mr Shiver </h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-07-05" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-07-27" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="http://sevn.ly/xUBYXlxZ" target="_blank">
+          <a href="" target="_blank">
             <Button classes="events-button ticket" btnType="hollow">
               Get on Club <span>Guestlist</span>
             </Button>
