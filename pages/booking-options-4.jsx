a02c7e0efe70318ee92a4d4ff654cf73cbbccb86
little-portland <110906749+little-portland@users.noreply.github.com>
2023-07-10 12:24:31 +0100

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>21 Jul</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Shaun Reeves, Marlie</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
               <h3><span>TENT <b>//</b> </span>Greg Sonata, Rhythms of Prescott</h3>
             </div>
         </div>
