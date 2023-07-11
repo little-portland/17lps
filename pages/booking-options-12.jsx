9004f6f85fe0ec140a4570bcb1ff7412cb3e2e0b
little-portland <110906749+little-portland@users.noreply.github.com>
2023-07-11 14:27:16 +0100

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>27 Jul</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>BALLAD, LUDI, NEB SPOOK</h3>
               <h3><span>TENT <b>//</b> </span>Harri Pepper, Mr Shiver </h3>
             </div>
         </div>
