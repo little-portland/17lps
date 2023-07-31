86450e6c594ba35561a0548a4a73e5c6484f5226
little-portland <110906749+little-portland@users.noreply.github.com>
2023-07-31 18:17:45 +0100

    Create the-tent-booking-option-5.jsx

---
+++ pages/the-tent-booking-option-5.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Script from "next/script";
+
+//hooks
+import useDeviceDetect from "@utils/useDeviceDetect";
+
+import CenterContainer from "@components/UX/CenterContainer/CenterContainer";
+
+import Button from "@components/UX/Button";
+
+const BookingOptions = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Bookings</title>
+      </Head>
+
+      <div id="bookingOptions" class="tent">
+        <h1 class="event-name">DINNER & TUNES</h1>
+        <div class="event-info">
+            <div class="info date">
+              <h2>11 AUG</h2>
+            </div>
+            <div class="info artists">
+              <h3><span>TENT <b>//</b> </span>FUNKTONIO B2B GREG SONATA </h3>
+            </div>
+        </div>
+        <div class="button-wrapper">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-08-11" target="_blank">
+            <Button classes="events-button"  btnType="hollow">
+              Book Dinner <span>(includes free club entry)</span>
+            </Button>
+          </a>
+          <a href="http://sevn.ly/xO1ABAsk" target="_blank">
+            <Button classes="events-button ticket" btnType="hollow">
+              Get on the Guestlist
+            </Button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookingOptions;
