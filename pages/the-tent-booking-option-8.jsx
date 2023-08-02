a8acb77d49fad63d13f409c569cab503676f9a37
little-portland <110906749+little-portland@users.noreply.github.com>
2023-08-02 13:18:32 +0100

    Update the-tent-booking-option-8.jsx

--- pages/the-tent-booking-option-8.jsx
+++ pages/the-tent-booking-option-8.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
+        <h1 class="event-name friday-residents">FRIDAY UNDERGROUND</h1>
         <div class="event-info">
             <div class="info date">
               <h2>18 AUG</h2>
@@ -32,11 +32,6 @@ const BookingOptions = () => {
           <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-08-18" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner
-            </Button>
-          </a>
-          <a href="http://sevn.ly/xIQyNdpI" target="_blank">
-            <Button classes="events-button ticket" btnType="hollow">
-              Buy Tickets
             </Button>
           </a>
           <a href="http://sevn.ly/xIQyNdpI" target="_blank">
