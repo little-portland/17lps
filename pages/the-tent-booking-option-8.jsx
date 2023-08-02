fe2a0a2f3d9c1919c9888001ec95b3e3730bbfb5
little-portland <110906749+little-portland@users.noreply.github.com>
2023-08-02 12:47:03 +0100

    Update the-tent-booking-option-8.jsx

--- pages/the-tent-booking-option-8.jsx
+++ pages/the-tent-booking-option-8.jsx
@@ -18,19 +18,25 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions" class="tent">
-        <h1 class="event-name">DINNER & TUNES</h1>
+        <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
         <div class="event-info">
             <div class="info date">
               <h2>18 AUG</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>TBA </h3>
+              <h3><span>STUDIO <b>//</b> </span>DR BANANA & TOBY</h3>
+              <h3><span>TENT <b>//</b> </span>OLI SILVA </h3>
             </div>
         </div>
         <div class="button-wrapper">
           <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-08-18" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner
+            </Button>
+          </a>
+          <a href="http://sevn.ly/xIQyNdpI" target="_blank">
+            <Button classes="events-button ticket" btnType="hollow">
+              Buy Tickets
             </Button>
           </a>
           <a href="http://sevn.ly/xIQyNdpI" target="_blank">
