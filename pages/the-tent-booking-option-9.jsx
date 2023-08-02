c27c9d40f4a4b4afb299128f7b5b9ea81126629f
little-portland <110906749+little-portland@users.noreply.github.com>
2023-08-02 14:13:22 +0100

    Update the-tent-booking-option-9.jsx

--- pages/the-tent-booking-option-9.jsx
+++ pages/the-tent-booking-option-9.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>19 AUG</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>PASH KA </h3>
+              <h3><span>TENT <b>//</b> </span>PASH</h3>
             </div>
         </div>
         <div class="button-wrapper">
