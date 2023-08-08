eb960afbe5d1df1303348cb3e51d797f352b9f5b
little-portland <110906749+little-portland@users.noreply.github.com>
2023-08-08 14:06:09 +0100

    Update the-tent-booking-option-7.jsx

--- pages/the-tent-booking-option-7.jsx
+++ pages/the-tent-booking-option-7.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>17 AUG</h2>
             </div>
             <div class="info artists">
-               <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+               <h3><span>STUDIO <b>//</b> </span>MID-SUMMER SPECIAL PART II</h3>
               <h3><span>TENT <b>//</b> </span>GREGORIO SOAVE B2B RHYTHMS OF PRESCOTT</h3>
             </div>
         </div>
