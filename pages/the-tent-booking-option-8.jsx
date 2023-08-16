968e6f8acb8155da6ce1300840319bde2594991b
little-portland <110906749+little-portland@users.noreply.github.com>
2023-08-16 15:57:57 +0100

    Update the-tent-booking-option-8.jsx

--- pages/the-tent-booking-option-8.jsx
+++ pages/the-tent-booking-option-8.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name">FRIDAY UNDERGROUND</h1>
+        <h1 class="event-name">MID-SUMMER SPECIAL PART II</h1>
         <div class="event-info">
             <div class="info date">
               <h2>18 AUG</h2>
