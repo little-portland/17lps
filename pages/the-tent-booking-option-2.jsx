e8743ea1f22a179fadc1874fe92293a402187518
little-portland <110906749+little-portland@users.noreply.github.com>
2023-08-21 13:10:52 +0100

    Update the-tent-booking-option-2.jsx

--- pages/the-tent-booking-option-2.jsx
+++ pages/the-tent-booking-option-2.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>08 Sep</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>OHM HOURANI</h3>
               <h3><span>TENT <b>//</b> </span>GILLES SMITH, MAYBE LAURA</h3>
             </div>
         </div>
