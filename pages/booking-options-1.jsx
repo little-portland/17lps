5d0828d6a780489f4dc943774d76c37fa54376c7
little-portland <110906749+little-portland@users.noreply.github.com>
2023-08-29 13:10:18 +0100

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions" class="tent">
-        <h1 class="event-name">Thursday Underground</h1>
+        <h1 class="event-name focal-point-presents">Focal Point Presents</h1>
         <div class="event-info">
             <div class="info date">
               <h2>14 SEP</h2>
