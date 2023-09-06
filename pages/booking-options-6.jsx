449a532df8bce4e8d58387481a9b844b8357c887
little-portland <110906749+little-portland@users.noreply.github.com>
2023-09-06 11:15:47 +0100

    Update booking-options-6.jsx

--- pages/booking-options-6.jsx
+++ pages/booking-options-6.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>PHENOM AND FRIENDS</h3>
-              <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & ROBERTO MANZIN[LIVE JAZZ]</h3>
+              <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & ROBERTO MANZIN [LIVE JAZZ]</h3>
             </div>
         </div>
         <div class="button-wrapper">
