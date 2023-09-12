8a98a917e07390101a9236aa4bac9c3d648f063b
little-portland <110906749+little-portland@users.noreply.github.com>
2023-09-12 16:18:21 +0100

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>29 SEP</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>Bas Ibellini</h3>
               <h3><span>TENT <b>//</b> </span>DAVID AGRELLA, ROBERTA CUTOLO</h3>
             </div>
         </div>
