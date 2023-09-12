2996ea560fe64d9e86de2232a3d427e9d24bf57a
little-portland <110906749+little-portland@users.noreply.github.com>
2023-09-12 12:55:15 +0100

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>B.LOVE, TRULY MADLY</h3>
-              <h3><span>TENT <b>//</b> </span>2STEPBEC & ZEUS</h3>
+              <h3><span>TENT <b>//</b> </span>2STEPBEC & ZEUS + PAUL DAY</h3>
             </div>
         </div>
         <div class="button-wrapper">
