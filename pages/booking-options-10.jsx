eda75797ee260385706482be0f7776a4ed77c877
little-portland <110906749+little-portland@users.noreply.github.com>
2023-09-19 10:22:37 +0100

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -18,25 +18,25 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
+        <h1 class="event-name urban-future">Public image</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>15 SEP</h2>
+              <h2>04 OCT</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & LVCA</h3>
-              <h3><span>TENT <b>//</b> </span>HARRI PEPPER, JUNGLE JARVIN</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & ROBSON BARROS [LIVE JAZZ]</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-09-15" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-10-04" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="http://sevn.ly/xGRPd0nU" target="_blank">
+          <a href="#" target="_blank">
             <Button classes="events-button ticket" btnType="hollow">
-               GET ON CLUB <span>GUESTLIST</span>
+              GET ON CLUB <span>GUESTLIST</span>
             </Button>
           </a>
         </div>
