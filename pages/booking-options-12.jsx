fa3655774038ee4edb55865bc601c62bd3ca8a9e
little-portland <110906749+little-portland@users.noreply.github.com>
2023-09-20 10:08:42 +0100

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>28 Sep</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>SPACED RECORDS</h3>
+              <h3><span>STUDIO <b>//</b> </span>SPACED RECORDS WITH THO & LOREN</h3>
               <h3><span>TENT <b>//</b> </span>RHYTHMS OF PRESCOTT & DEMI</h3>
             </div>
         </div>
