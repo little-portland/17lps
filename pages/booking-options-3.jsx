6d438dafba69f4c104da3b1fb29d338dcfa693c2
little-portland <110906749+little-portland@users.noreply.github.com>
2023-09-26 11:50:23 +0100

    Update booking-options-3.jsx

--- pages/booking-options-3.jsx
+++ pages/booking-options-3.jsx
@@ -18,25 +18,25 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
+        <h1 class="event-name disco-saturdays">DISCO SATURDAYS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>22 SEP</h2>
+              <h2>14 Oct</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI ALL NIGHT LONG</h3>
-              <h3><span>TENT <b>//</b> </span>DR J, PAUL DAY</h3>
+              <h3><span>STUDIO <b>//</b> </span>JONNY ROCK</h3>
+              <h3><span>TENT <b>//</b> </span>DR J, JAIME</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2023-09-22" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-10-14" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="http://sevn.ly/xtLj7kdZ" target="_blank">
-           <Button classes="events-button ticket" btnType="hollow">
-               GET ON CLUB <span>GUESTLIST</span>
+          <a href="http://sevn.ly/x1PkRTYK" target="_blank">
+            <Button classes="events-button ticket" btnType="hollow">
+              GET ON CLUB <span>GUESTLIST</span>
             </Button>
           </a>
         </div>
