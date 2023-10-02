ee2ead5c05affaac478d73571cd7015ee9889106
little-portland <110906749+little-portland@users.noreply.github.com>
2023-10-02 19:29:35 +0100

    Update booking-options-5.jsx

--- pages/booking-options-5.jsx
+++ pages/booking-options-5.jsx
@@ -21,20 +21,20 @@ const BookingOptions = () => {
         <h1 class="event-name disco-saturdays">DISCO SATURDAYS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>23 SEP</h2>
+              <h2>21 OCT</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>JONNY ROCK</h3>
-              <h3><span>TENT <b>//</b> </span>Frankie Valentine, Gabriel Rai</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>TENT <b>//</b> </span>FUNKTONIO, REMI MAZET</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-09-23" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-10-21" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="http://sevn.ly/xZjWO0O4" target="_blank">
+          <a href="#" target="_blank">
             <Button classes="events-button ticket" btnType="hollow">
               GET ON CLUB <span>GUESTLIST</span>
             </Button>
