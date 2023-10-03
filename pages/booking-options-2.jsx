3b119d6ea2df188c2ee7473f0dcb3cc5ab48643d
little-portland <110906749+little-portland@users.noreply.github.com>
2023-10-03 17:58:00 +0100

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -24,8 +24,8 @@ const BookingOptions = () => {
               <h2>13 OCT</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, JONNY ROCK</h3>
-              <h3><span>TENT <b>//</b> </span>DAVID TRIANA, MAX VAN DE PUT</h3>
+              <h3><span>STUDIO <b>//</b> </span>DAVID TRIANA, JONNY ROCK</h3>
+              <h3><span>TENT <b>//</b> </span>JAIME, MAX VAN DE PUT</h3>
             </div>
         </div>
         <div class="button-wrapper">
