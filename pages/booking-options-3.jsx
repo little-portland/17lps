a7953b05f9b6330b2c0cc22d224bec4fbaeb6767
little-portland <110906749+little-portland@users.noreply.github.com>
2023-10-10 16:27:21 +0100

    Update booking-options-3.jsx

--- pages/booking-options-3.jsx
+++ pages/booking-options-3.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>20 OCT</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, ISAAC CARTER</h3>
               <h3><span>TENT <b>//</b> </span>DAVID AGRELLA, MR SHIVER </h3>
             </div>
         </div>
