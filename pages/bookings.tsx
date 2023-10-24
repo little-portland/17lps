a5eb76a3112bef382bd849fd96b211aee870df3d
little-portland <110906749+little-portland@users.noreply.github.com>
2023-10-24 10:28:19 +0100

    Update bookings.tsx

--- pages/bookings.tsx
+++ pages/bookings.tsx
@@ -52,25 +52,54 @@ const Bookings = () => {
           style={style}
         />
         <div className="bookings-body-text">
-         <h1 className="bookings-heading-1">Special Events</h1>
+        <h1 className="bookings-heading-1">LARGE GROUPS</h1>
+          <ul>
+            <li>For group reservations for 10 persons or more, please email us at <a href="mailto:eat@little-portland.com">eat@little-portland.com</a>.</li>
+            <li>We can cater for groups of up to 34 in The Tent, or 12 in the Chef’s Studio.</li>
+            <li>Large groups can choose from 2 set menu options. <a href="https://www.little-portland.com/large-group-bookings-menu">Click here</a> to see sample menus.</li>
+            <li>To confirm a reservation, we require a deposit equal to 50% of the set menu price, per person.</li>
+            <li>Cancellations within 10 days of your dinner reservation or no-shows will be subject to a late cancellation fee equal to the deposit paid.</li>
+            <li>Reductions in group sizes within 10 days of your reservation time will also result in late cancellation fees being charged.</li>
+          </ul>
+          
+          <h1 className="bookings-heading-1">CHEF’S STUDIO</h1>
+          <ul>
+            <li>The Chef’s Studio follows a set menu format and is £100pp. <a href="https://www.little-portland.com/chefs-menu">Click here</a> to see a sample menu.</li>
+            <li>To confirm a reservation, we require a deposit equal to £100pp.</li>
+            <li>Cancellations within 10 days of your dinner reservation or no-shows will be subject to a £100pp late cancellation fee.</li>
+            <li>Reductions in group sizes within 10 days of your reservation time will also result in late cancellation fees being charged.</li>
+            <li>Reservations can be made for a minimum group size of 8 for Wednesdays and Thursdays, and 4 for Fridays and Saturdays.</li>
+            <li>Reservations must be made at least 7 days in advance.</li>
+            <li>Unfortunately we cannot cater for allergies or dietary requirements in the Chef’s Studio. No changes can be made to the set menu. Menu is subject to change depending on availability of produce.</li>
+            <li>For more information, to make a reservation or modify an existing one, contact <a href="mailto:eat@little-portland.com">eat@little-portland.com</a></li>
+          </ul>
+
+          <h1 className="bookings-heading-1">PRIVATE HIRE + PRIVATE DINING</h1>
+          <ul>
+            <li>Both The Tent and the Chef’s Studio are available for private hire. Contact <a href="mailto:yo@little-portland.com">yo@little-portland.com</a> for more information.</li>
+            <li>Both the large group and Chef’s Studio menus are available for private dinner events (see above for sample menu links).</li>
+            <li>We also offer canapés for private events. <a href="https://www.little-portland.com/canape-menu">Click here</a> to see the sample canapé menu.</li>
+            <li>We are able to cater for Private Dining requests for groups of up to 12 in the Chef’s Studio (see details above).</li>
+          </ul>
+
+         <h1 className="bookings-heading-1">CANCELLATION POLICY FOR GROUP SIZES <10</h1>
+          <ul>
+            <li>To confirm a reservation, we require a credit card guarantee.</li>
+            <li>Cancellations within 48 hours of your dinner reservation or no-shows will be subject to a £30pp late cancellation fee.</li>
+            <li>For any changes to reservations, or for further information, contact <a href="mailto:eat@little-portland.com">eat@little-portland.com</a></li>
+            <li>Reductions in group sizes within 48 hours of your reservation time will also result in late cancellation fees.</li>
+          </ul>
+          
+          <h1 className="bookings-heading-2">MISCELLANEOUS</h1>
           <ul>
             <li>All menus are subject to change depending on availability.</li>
             <li>Unfortunately we cannot cater for allergies or dietary requirements during special events. No changes can be made to special set menus.</li>
-          </ul>
-        <h1 className="bookings-heading-1">Large Groups</h1>
-          <ul>
-            <li>For larger group bookings of 10 or more, please email us at <a href="mailto:eat@little-portland.com">eat@little-portland.com</a>.</li>
-            <li>Large groups can choose from 2 set menu options. <a href="https://www.little-portland.com/menu-options">Click here</a> to see the menus.</li>
-            <li>To confirm the booking, we will require 50% deposit of the chosen set menu (total price per head). If cancelling within 10 days of your dinner reservation, we will retain the 50% deposit made.</li>
-          </ul>
-          <h1 className="bookings-heading-2">Bookings &amp; Cancellations</h1>
-          <ul>
-            <li>We ask that any changes to your reservation be made at least 48 hours before the reservation time in order to avoid a late cancellation fee.</li>
-            <li>Changes to reservations can be made by email at <a href="mailto:eat@little-portland.com">eat@little-portland.com</a>.</li>
-            <li>Late cancellations or no-shows will be subject to a £30pp late cancellation fee.</li>
-            <li>Last minute reductions in group sizes may also be charged a cancellation fee.</li>
-            <li>Children under the age of 16 are not permitted to enter the venue.</li>
-            <li>Reservations for the following month will become available online on the 1st of every month.</li>
+            <li>Anyone under the age of 18 is not permitted to enter the venue.</li>
+            <li>Dogs are not permitted to enter the venue.</li>
+            <li>No photos or videos are permitted to be taken inside the venue.</li>
+            <li>Reservations can be made two months in advance.</li>
+            <li>All menus are subject to change depending on availability.</li>
+            <li> For any changes to reservations, or for further information, contact <a href="mailto:eat@little-portland.com">eat@little-portland.com</a>.</li>
           </ul>
         </div>
 
