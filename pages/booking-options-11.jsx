68a9667b7bc4abe9702271c3db78cad502c62f7e
little-portland <110906749+little-portland@users.noreply.github.com>
2023-10-24 09:54:42 +0100

    Update booking-options-11.jsx

--- pages/booking-options-11.jsx
+++ pages/booking-options-11.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>01 NOV</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO<b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO<b>//</b> </span>KARL PHENOM + FRIENDS</h3>
               <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & ROBERTO MANZIN</h3>
             </div>
         </div>
