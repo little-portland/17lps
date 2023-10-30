6b178da2aa8b65a93b2be9dcd484fa12f7993215
little-portland <110906749+little-portland@users.noreply.github.com>
2023-10-30 13:52:54 +0000

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>GABBS, SMALL WORLD</h3>
-              <h3><span>TENT <b>//</b> </span>B. LOVE, OLIVE.R</h3>
+              <h3><span>TENT <b>//</b> </span>B.LOVE, OLIVE.R</h3>
             </div>
         </div>
         <div class="button-wrapper">
