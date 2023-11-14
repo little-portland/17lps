0d506b2bd3fb847717d86e4005a1c6fca80ac6dc
little-portland <110906749+little-portland@users.noreply.github.com>
2023-11-14 12:44:27 +0000

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>BAS IBELINI, ZEINA</h3>
-              <h3><span>TENT <b>//</b> </span>MARC JARVIN,  NICK WILLIAMS</h3>
+              <h3><span>TENT <b>//</b> </span>JUNGLE JARVIN,  NICK WILLIAMS</h3>
             </div>
         </div>
         <div class="button-wrapper">
