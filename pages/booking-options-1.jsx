a4d6db99aca3f86f1d5fd6511f591593a8ace760
little-portland <110906749+little-portland@users.noreply.github.com>
2023-11-23 10:31:32 +0000

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -21,7 +21,7 @@ const BookingOptions = () => {
         <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>22 Nov</h2>
+              <h2>23 Nov</h2>
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>LOVE CHILD</h3>
