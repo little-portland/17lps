b6a22596a4ab1a477137ca2e12cc18fadff32c17
little-portland <110906749+little-portland@users.noreply.github.com>
2023-11-28 08:48:33 +0000

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>16 DEC</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>BILL BREWSTER </h3>
               <h3><span>TENT <b>//</b> </span>DR J, GEORGIA</h3>
             </div>
         </div>
