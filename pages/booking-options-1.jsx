50d151bbf92358439e14799c33dec7a40c06de35
little-portland <110906749+little-portland@users.noreply.github.com>
2023-11-29 14:00:34 +0000

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
               Book Dinner <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="http://sevn.ly/xxlrMLSP" target="_blank">
+          <a class=""not-allowed href="" target="_blank">
             <Button classes="events-button ticket" btnType="hollow">
               Book Club <span>Ticket</span>
             </Button>
