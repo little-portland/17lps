8d996fbf594f62aa870187ab62aba71dc62c9c09
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-05 12:23:11 +0000

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>22 DEC</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>JOSH LUDLOW, RAKIM UNDER</h3>
               <h3><span>TENT <b>//</b> </span>MAR*O, Roberta Cutolo</h3>
             </div>
         </div>
