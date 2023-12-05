07c65b549ff377f4812d7600d3e3ab3519da867b
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-05 13:22:17 +0000

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>CLOSED</h3>
-              <h3><span>TENT <b>//</b> </span>KEM, MAYBE LAURA, REMI</h3>
+              <h3><span>TENT <b>//</b> </span>KEM, MAYBE LAURA, REMI MAZET</h3>
             </div>
         </div>
         <div class="button-wrapper">
