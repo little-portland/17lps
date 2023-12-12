1def850f55108a04fc7331386b8056aafb4f0bfd
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-12 17:13:19 +0000

    Update booking-options-5.jsx

--- pages/booking-options-5.jsx
+++ pages/booking-options-5.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>23 DEC</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>CLOSED</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
               <h3><span>TENT <b>//</b> </span>NICK WILLIAMS, PASH</h3>
             </div>
         </div>
