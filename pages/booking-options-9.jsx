789f6f94341f0940adae012e0894136fea2e7178
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-19 11:55:31 +0000

    Update booking-options-9.jsx

--- pages/booking-options-9.jsx
+++ pages/booking-options-9.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>29 DEC</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>A GUY CALLED GERALD</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
               <h3><span>TENT <b>//</b> </span>JOS, NICK WILLIAMS</h3>
             </div>
         </div>
