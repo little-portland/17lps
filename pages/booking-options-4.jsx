d0d6dbe36a45bf3f86b915ac6e1074c108a559c8
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-20 12:48:06 +0000

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>TBA</h3>
-              <h3><span>TENT <b>//</b> </span>BENZ, LUDI</h3>
+              <h3><span>TENT <b>//</b> </span>BALEARIC LONDON (ARIANE V, BEN GOMORI & SARAHTONIN)</h3>
             </div>
         </div>
         <div class="button-wrapper">
