375d0886b61b81622f6d4f9045ff9c3adfecba46
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-20 12:41:41 +0000

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>TBA</h3>
-              <h3><span>TENT <b>//</b> </span>FUNKTONIO, NICK WILLIAMS</h3>
+              <h3><span>TENT <b>//</b> </span>DAVID AGRELLA, NICK WILLIAMS </h3>
             </div>
         </div>
         <div class="button-wrapper">
