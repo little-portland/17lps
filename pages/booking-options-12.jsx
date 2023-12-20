a2ae6c153ad0d5ba69c77bb0d2888adbc24cc941
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-20 12:26:51 +0000

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>A GUY CALLED GERALD</h3>
-              <h3><span>TENT <b>//</b> </span>KEM, TRIXIS</h3>
+              <h3><span>TENT <b>//</b> </span>KEM, TRIXIE</h3>
             </div>
         </div>
         <div class="button-wrapper">
