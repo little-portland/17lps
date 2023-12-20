572398b3d48d506ec9c324ae36f6abdd4999ac42
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-20 15:55:11 +0000

    Update booking-options-8.jsx

--- pages/booking-options-8.jsx
+++ pages/booking-options-8.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>CLOSED</h3>
-              <h3><span>TENT <b>//</b> </span>Oli SILVA, n-gynn</h3>
+              <h3><span>TENT <b>//</b> </span>n-gynn</h3>
             </div>
         </div>
         <div class="button-wrapper">
