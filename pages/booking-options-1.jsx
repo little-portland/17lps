758570a12aa254d327d9368861e58f09a3864c66
little-portland <110906749+little-portland@users.noreply.github.com>
2023-12-21 14:19:53 +0000

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -29,7 +29,7 @@ const BookingOptions = () => {
             </div>
         </div>
         <div class="button-wrapper">
-          <a class="not-allowed" href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-01-01" target="_blank">
+          <a class="not-allowed" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
