f04ab0760362e67613e9ea14d32f63507a047430
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-03 11:32:48 +0000

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>11 JAN</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>SUBB-AN</h3>
               <h3><span>TENT <b>//</b> </span>MR SHIVER, PAUL DAY</h3>
             </div>
         </div>
