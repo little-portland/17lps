c20ed076e32685e05631a0569c907f9edb565dcd
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-09 09:02:40 +0000

    Update booking-options-14.jsx

--- pages/booking-options-14.jsx
+++ pages/booking-options-14.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>13 JAN</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>JACOB HUSLEY</h3>
               <h3><span>TENT <b>//</b> </span>DAVIDE DEL VECCHIO, DR J</h3>
             </div>
         </div>
