e3efe5746e392e6aaa30cd05fb3d95b757e35fb3
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-09 09:00:35 +0000

    Update booking-options-3.jsx

--- pages/booking-options-3.jsx
+++ pages/booking-options-3.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>25 Jan</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Bobby.</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
               <h3><span>TENT <b>//</b> </span>GABRIEL RAI & OLI SILVA </h3>
             </div>
         </div>
