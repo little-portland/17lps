f18d448fe972b87f17916095c2124c22bf459283
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-23 13:27:12 +0000

    Update booking-options-9.jsx

--- pages/booking-options-9.jsx
+++ pages/booking-options-9.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>02 FEB</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>GILES SMITH</h3>
               <h3><span>TENT <b>//</b> </span>/MARI*, OLI SILVA </h3>
             </div>
         </div>
