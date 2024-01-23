a83ae061260db01b73c232e7e335d20145022a8f
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-23 11:48:09 +0000

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -21,20 +21,20 @@ const BookingOptions = () => {
         <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>18 Jan</h2>
+              <h2>08 Feb</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>HAMISH & TOBY, JOSH TWEEK, TIMUR BASHA</h3>
-              <h3><span>TENT <b>//</b> </span>DEMI, HARRI PEPPER</h3>
+              <h3><span>STUDIO <b>//</b> </span>SPECIAL GUEST</h3>
+              <h3><span>TENT <b>//</b> </span>MR SHIVER, PAUL DAY</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-01-18" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-02-08" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="https://sevn.ly/xbK4jSqN" target="_blank">
+          <a href="https://sevn.ly/xyk7Zw3o" target="_blank">
             <Button classes="events-button ticket" btnType="hollow">
               Book Club <span>Ticket</span>
             </Button>
