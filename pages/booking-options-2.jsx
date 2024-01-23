3880f9e73421a59292d9f70e15defeee4a873c16
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-23 14:52:13 +0000

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>SEAN OD & FRIENDS</h3>
-              <h3><span>TENT <b>//</b> </span>MAR*O, ROBERTA CUTOLO</h3>
+              <h3><span>TENT <b>//</b> </span>MARI*, ROBERTA CUTOLO</h3>
             </div>
         </div>
         <div class="button-wrapper">
