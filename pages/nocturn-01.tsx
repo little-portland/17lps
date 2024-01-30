041eddb87fe99914d12bd8156d8d694d4849b400
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-30 11:55:40 +0000

    Update nocturn-01.tsx

--- pages/nocturn-01.tsx
+++ pages/nocturn-01.tsx
@@ -20,24 +20,14 @@ const Menu = ({ menuImage }) => {
       </Head>
 
       <CenterContainer>
-          <h1 className="nocturn-header">
-            NOCTURN <br />
-            <span>
-              an AFFECT: a MOOD: a STATE OF CONTACT
-            </span>
-          </h1>
-        <h2>
-        Nocturn [01] feat. Gray Wielebinski
-        </h2>
-        <h3>ONE MORE HOUR OF THE MILLENIUM [2024]</h3>
         {menuImage && (
           <Image
-            src={"/images/gary-wielebinski.jpg"} 
+            src={"/images/nocturn-01.jpg"} 
             alt={menuImage.title}
             className={"image"}
             width={menuImage.width} //automatically provided
             height={menuImage.height} //automatically provided
-            blurDataURL={"/images/gary-wielebinski.jpg"} //automatically provided
+            blurDataURL={"/images/nocturn-01.jpg"} //automatically provided
             placeholder="blur" // Optional blur-up while loading
           />
         )}
