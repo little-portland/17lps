f662081b45547878d5a7dc4a035128d175341197
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-30 18:27:24 +0000

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>FELIX DICKINSON, ROB MELLO</h3>
-              <h3><span>TENT <b>//</b> </span>DAVID <b>B2B</b> RAKIM UNDER, ECE DUZGÏT</h3>
+              <h3><span>TENT <b>//</b> </span>DAVID TRIANA <b>B2B</b> ECE DÜZGIT</h3>
             </div>
         </div>
         <div class="button-wrapper">
