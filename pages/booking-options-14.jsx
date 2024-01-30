0d2f604dd9be5dbb8f5de39b4158c426cf55e852
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-30 13:39:33 +0000

    Update booking-options-14.jsx

--- pages/booking-options-14.jsx
+++ pages/booking-options-14.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name openlab-presents">DISCO SATURDAYS</h1>
+        <h1 class="event-name openlab-presents">OPENLAB PRESENTS</h1>
           <div class="event-info">
             <div class="info date">
               <h2>03 FEB</h2>
