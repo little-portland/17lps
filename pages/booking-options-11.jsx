a588832f941184254cd25e0b80427cb8711ce5c9
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-30 14:09:10 +0000

    Update booking-options-11.jsx

--- pages/booking-options-11.jsx
+++ pages/booking-options-11.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>07 FEB</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO<b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO<b>//</b> </span>Icykof, Max Menaged</h3>
               <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & ROBERTO MANZIN</h3>
             </div>
         </div>
