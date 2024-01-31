1798730660e40f9a19d5ad7c206640e764a5ee52
little-portland <110906749+little-portland@users.noreply.github.com>
2024-01-31 10:31:36 +0000

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>09 FEB</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS </h3>
               <h3><span>TENT <b>//</b> </span>NICK WILLIAMS, N-GYNN </h3>
             </div>
         </div>
