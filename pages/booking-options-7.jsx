b891a15762a3b454cba514b1240dc8a9327f7e4d
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-06 15:46:16 +0000

    Update booking-options-7.jsx

--- pages/booking-options-7.jsx
+++ pages/booking-options-7.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>TBA</h3>
-              <h3><span>TENT <b>//</b> </span>TIM LANG</h3>
+              <h3><span>TENT <b>//</b> </span>TIM LANG, GEORGIA </h3>
             </div>
         </div>
         <div class="button-wrapper">
