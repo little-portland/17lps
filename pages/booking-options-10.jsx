a246e6731f6d12a11651732b4640ede7448cd010
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-13 13:39:24 +0000

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, OHM HOURANI</h3>
-              <h3><span>TENT <b>//</b> </span>PAUL DAY, ROBERTA CUTOLO</h3>
+              <h3><span>TENT <b>//</b> </span>Frankie Valentine & Francesco Mami, PAUL DAY</h3>
             </div>
         </div>
         <div class="button-wrapper">
