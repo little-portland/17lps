1ba9db24abd8c7f8f221eaa068da325fad220a03
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-13 10:59:53 +0000

    Create booking-options-15.jsx

---
+++ pages/booking-options-15.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Script from "next/script";
+
+//hooks
+import useDeviceDetect from "@utils/useDeviceDetect";
+
+import CenterContainer from "@components/UX/CenterContainer/CenterContainer";
+
+import Button from "@components/UX/Button";
+
+const BookingOptions = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Bookings </title>
+      </Head>
+
+      <div id="bookingOptions">
+        <h1 class="event-name">Eat at 17 Little Portland Street, London</h1>
+          <div class="event-info">
+            <div class="info date">
+              <h2>03 FEB</h2>
+            </div>
+            <div class="info artists">
+              <h3><span>STUDIO <b>//</b> </span>PBR STREETGANG, RALPH LAWSON</h3>
+              <h3><span>TENT <b>//</b> </span>SADEEDO, TINTIN</h3>
+            </div>
+        </div>
+        <div class="button-wrapper">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-02-03" target="_blank">
+            <Button classes="events-button"  btnType="hollow">
+              Dinner+Tunes in The Tent<span>(Middle Eastern under)</span>
+            </Button>
+          </a>
+          <a href="https://sevn.ly/xT7QEuYG" target="_blank">
+            <Button classes="events-button" btnType="hollow">
+                 Chef's Studio<span>(Chinese)</span>
+            </Button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookingOptions;
