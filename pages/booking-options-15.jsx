4c3b2699e5230035afd76e8c07970b3637444526
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-13 12:16:36 +0000

    Update booking-options-15.jsx

--- pages/booking-options-15.jsx
+++ pages/booking-options-15.jsx
@@ -23,7 +23,7 @@ const BookingOptions = () => {
               <h1>Eat at 17 Little Portland Street, London</h1>
             </div>
         </div>
-        <div class="button-wrapper">
+        <div class="button-wrapper eat-body">
           <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-02-03" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               <div>Dinner+Tunes</div> in The Tent<span>(Middle Eastern)</span>
