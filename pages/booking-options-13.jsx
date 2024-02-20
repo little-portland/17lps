93619ae70def49b39ebd8bd43c0be7712d43676e
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-20 10:43:44 +0000

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name cny">Chinese New Year</h1>
+        <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
               <h2>22 FEB</h2>
