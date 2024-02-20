00750ac97bfa3efa46ee0a4a7ed6ef11695fb6c1
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-20 10:22:24 +0000

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -21,20 +21,20 @@ const BookingOptions = () => {
         <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>16 FEB</h2>
+              <h2>08 MAR</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, MATHEW JONSON[live], REMI MAZET</h3>
-              <h3><span>TENT <b>//</b> </span>DAVID TRIANA <b>B2B</b> ECE DÜZGIT, ROBERTA CUTOLO </h3>
+              <h3><span>STUDIO <b>//</b> </span>CASSY, DAVID TRIANA</h3>
+              <h3><span>TENT <b>//</b> </span>MR SHIVER,  ROBERTA CUTOLO</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-02-16" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-08" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="https://sevn.ly/xM7dfUvh" target="_blank">
+          <a href="https://sevn.ly/x2crfDce" target="_blank">
             <Button classes="events-button ticket" btnType="hollow">
                GET ON CLUB <span>GUESTLIST</span>
             </Button>
