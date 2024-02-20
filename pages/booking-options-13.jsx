00b71e7a37207d3c21511518d2aba84bf855d875
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-20 10:51:07 +0000

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -27,9 +27,9 @@ const BookingOptions = () => {
               <h3><span>STUDIO <b>//</b> </span>SPACED WITH JACK WRENN, LOREN, THO</h3>
               <h3><span>TENT <b>//</b> </span>Demi, FUNKTONIO</h3>
             </div>
-            <div class="info">
-              <h3>CHINESE NEW YEAR - MASTER SPECIAL</h3>
-            </div>
+        </div>
+        <div class="event-info">
+            <h3>CHINESE NEW YEAR - MASTER SPECIAL</h3>
         </div>
         <div class="button-wrapper">
           <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-02-22" target="_blank">
