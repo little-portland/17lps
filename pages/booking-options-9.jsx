45ff2119008ade6b8373b11e7fc3af24307623c3
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-20 11:03:00 +0000

    Update booking-options-9.jsx

--- pages/booking-options-9.jsx
+++ pages/booking-options-9.jsx
@@ -28,6 +28,9 @@ const BookingOptions = () => {
               <h3><span>TENT <b>//</b> </span>MR SHIVER, RHYTHMS OF PRESCOTT</h3>
             </div>
         </div>
+       <div class="cny">
+            <h3>CHINESE NEW YEAR - MASTER SPECIAL</h3>
+        </div>
         <div class="button-wrapper">
           <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-02-23" target="_blank">
             <Button classes="events-button"  btnType="hollow">
