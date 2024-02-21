8d7aece457ccca3a8c26e21f2c912aef452ae4fb
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-21 17:40:25 +0000

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>09 MAR</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>David Triana & Friends</h3>
               <h3><span>TENT <b>//</b> </span>BEN GOMORI, NICK WILLIAMS</h3>
             </div>
         </div>
