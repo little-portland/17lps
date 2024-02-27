e39cd2caeb60333526cc30945e4d9610285bda94
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-27 14:56:39 +0000

    Update booking-options-9.jsx

--- pages/booking-options-9.jsx
+++ pages/booking-options-9.jsx
@@ -32,7 +32,6 @@ const BookingOptions = () => {
           <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-15" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner 
-              <p class="chinese-ny">CNY - MASTER WEEK</p>
               <span>(includes free club entry)</span>
             </Button>
           </a>
