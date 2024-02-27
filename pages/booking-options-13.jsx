5bf2580b1672c209d9d50865119ed9589013a985
little-portland <110906749+little-portland@users.noreply.github.com>
2024-02-27 14:18:35 +0000

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -21,22 +21,22 @@ const BookingOptions = () => {
         <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>22 FEB</h2>
+              <h2>14 MAr</h2>
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>SPACED WITH JACK WRENN, LOREN, THO</h3>
-              <h3><span>TENT <b>//</b> </span>Demi, FUNKTONIO</h3>
+              <h3><span>TENT <b>//</b> </span> DAVID AGRELLA B2B TRIXIE</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-02-22" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-14" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner 
               <p class="chinese-ny">CNY - MASTER WEEK</p>
               <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="https://sevn.ly/xsIhScqE" target="_blank">
+          <a href="" target="_blank">
             <Button classes="events-button ticket" btnType="hollow">
               Book Club <span>Ticket</span>
             </Button>
