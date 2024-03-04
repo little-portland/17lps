53679595cdbb0a0a9608f6327726204d850b1125
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-04 17:33:00 +0000

    Update booking-options-5.jsx

--- pages/booking-options-5.jsx
+++ pages/booking-options-5.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>23 MAR</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>Private Events</h3>
               <h3><span>TENT <b>//</b> </span>MR SHIVER</h3>
             </div>
         </div>
