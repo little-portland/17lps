d98b5c92675d2b265c346a1cac8cceb4606c5159
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-04 17:31:48 +0000

    Update booking-options-3.jsx

--- pages/booking-options-3.jsx
+++ pages/booking-options-3.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>21 Mar</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>ADMNTi & KEARUN</h3>
               <h3><span>TENT <b>//</b> </span> ECE DUZGÏT, LUDI</h3>
             </div>
         </div>
