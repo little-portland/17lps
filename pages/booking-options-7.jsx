fa56dfa34c3100b57a835989b9ca47a73be0ce7d
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-11 16:42:21 +0000

    Update booking-options-7.jsx

--- pages/booking-options-7.jsx
+++ pages/booking-options-7.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>16 Mar</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Razor 'N' Tape</h3>
+              <h3><span>STUDIO <b>//</b> </span>Razor-N-Tape</h3>
               <h3><span>TENT <b>//</b> </span>Luther Vine B2B Soho</h3>
             </div>
         </div>
