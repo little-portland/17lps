18aa5ee8e24048681267384e84a1d538be404028
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-11 16:30:26 +0000

    Update booking-options-11.jsx

--- pages/booking-options-11.jsx
+++ pages/booking-options-11.jsx
@@ -21,20 +21,20 @@ const BookingOptions = () => {
         <h1 class="event-name urban-future">Public image</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>07 FEB</h2>
+              <h2>27 Mar</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO<b>//</b> </span>Icykof, Max Menaged</h3>
-              <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & ROBERTO MANZIN</h3>
+              <h3><span>STUDIO<b>//</b> </span>Icykof/h3>
+              <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & MAFALDA RAMOS</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-02-07" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-27" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
           </a>
-          <a href="https://sevn.ly/xqrZhY6O" target="_blank">
+          <a href="" target="_blank">
             <Button classes="events-button ticket" btnType="hollow">
               GET ON CLUB <span>GUESTLIST</span>
             </Button>
