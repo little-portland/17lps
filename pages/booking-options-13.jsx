cbabdd34badf759ea3b79f3beb9c308bbb3a9a2c
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-12 13:25:17 +0000

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>14 MAr</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>DAN ANDREI </h3>
+              <h3><span>STUDIO <b>//</b> </span>DAN ANDREI, Morgan</h3>
               <h3><span>TENT <b>//</b> </span> DAVID AGRELLA B2B TRIXIE</h3>
             </div>
         </div>
