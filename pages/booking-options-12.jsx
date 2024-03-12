8e2ec6dc7ca01efb18fc7145578a20a251ba169a
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-12 11:34:38 +0000

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>28 Mar</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Truly Madly Private Birthday Party</h3>
+              <h3><span>STUDIO <b>//</b> </span>Closed</h3>
               <h3><span>TENT <b>//</b> </span>MARCELINA WICK B2B B.AI</h3>
             </div>
         </div>
