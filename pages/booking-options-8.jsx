9cae3e707e587cffdb6da4456eaa31bb47544ee1
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-13 10:32:27 +0000

    Update booking-options-8.jsx

--- pages/booking-options-8.jsx
+++ pages/booking-options-8.jsx
@@ -21,15 +21,15 @@ const BookingOptions = () => {
         <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>04 JAN</h2>
+              <h2>14 MAR</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>CLOSED</h3>
-              <h3><span>TENT <b>//</b> </span>MAYBE LAURA, n-gynn</h3>
+              <h3><span>STUDIO <b>//</b> </span>Dan Andrei, Morgan</h3>
+              <h3><span>TENT <b>//</b> </span>David Agrella B2B Trixie</h3>
             </div>
         </div>
         <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-01-01" target="_blank">
+          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-14" target="_blank">
             <Button classes="events-button"  btnType="hollow">
               Book Dinner <span>(includes free club entry)</span>
             </Button>
