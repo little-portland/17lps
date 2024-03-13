d7813f0821341264c8b005b445794a9ecb7f5e7c
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-13 11:50:23 +0000

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name dinner-tunes">Dinner & Tunes</h1>
+        <h1 class="event-name dinner-tunes">Dinner + Tunes</h1>
         <div class="event-info">
             <div class="info date">
               <h2>28 Mar</h2>
