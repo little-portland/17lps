c358fd7f54d5f69822e209346a44c16e70a71708
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-13 10:38:04 +0000

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -28,37 +28,6 @@ const BookingOptions = () => {
               <h3><span>TENT <b>//</b> </span> DAVID AGRELLA B2B TRIXIE</h3> 
             </div>
         </div>
-        <div class="button-wrapper">
-            <Button classes="events-button"  btnType="hollow">
-             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-14" target="_blank">
-              Dinner+Tunes <br/>in the Tent
-              <p>(Middle Eastern)</p>
-              <p>7PM - 9:30PM</p>
-             </a>
-             <a href="https://www.little-portland.com/menu" target="_blank">
-              [MENU]
-             </a>
-            </Button>
-
-            <Button classes="events-button ticket"  btnType="hollow">
-              <a href="https://www.little-portland.com/bookings" target="_blank">
-              CHEF’S STUDIO
-              <p>(Chinese)</p>
-              <p></p>Minimum 8 people now</p>
-              <p>8PM</p>
-             </a>
-             <a href="https://www.little-portland.com/chefs-menu" target="_blank">
-              [MENU]
-             </a>
-            </Button>
-        </div>
-        <div class="button-wrapper">
-            <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xE8RAEDy" target="_blank">
-                Book Club <p>Ticket</p>
-              </a>
-            </Button>
-        </div>
       </div>
     </>
   );
