c1e4ab68deb0fefbf3fdf4a924e3f9ecbb4a7f6a
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-18 14:40:54 +0000

    Update booking-options-11.jsx

--- pages/booking-options-11.jsx
+++ pages/booking-options-11.jsx
@@ -28,17 +28,36 @@ const BookingOptions = () => {
               <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & MAFALDA RAMOS</h3>
             </div>
         </div>
-        <div class="button-wrapper">
+        <div class="button-wrapper new-button-wrapper">
+            <Button classes="events-button new-events-button"  btnType="hollow">
           <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-27" target="_blank">
-            <Button classes="events-button"  btnType="hollow">
-              Book Dinner <span>(includes free club entry)</span>
+              <span>Dinner+Tunes <br/>in the Tent</span>
+              <p class="cusine">(Middle Eastern)</p>
+              <p class="time">7PM - 9:30PM</p>
+             </a>
+             <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
+              [MENU]
+             </a>
             </Button>
-          </a>
-          <a href="" target="_blank">
-            <Button classes="events-button ticket" btnType="hollow">
-              GET ON CLUB <span>GUESTLIST</span>
+
+            <Button classes="events-button ticket"  btnType="hollow">
+              <a href="https://www.little-portland.com/bookings" target="_blank">
+              <span>CHEF’S STUDIO</span>
+              <p class="cusine">(Chinese)</p>
+              <p class="cusine">Minimum 8 people</p>
+              <p class="time">8PM</p>
+             </a>
+             <a class="menu-btn" href="https://www.little-portland.com/chefs-menu" target="_blank">
+              [MENU]
+             </a>
             </Button>
-          </a>
+        </div>
+        <div class="button-wrapper new-button-wrapper">
+            <Button classes="events-button ticketNew" btnType="hollow">
+              <a href="" target="_blank">
+                Book Club Ticket
+              </a>
+            </Button>
         </div>
       </div>
     </>
