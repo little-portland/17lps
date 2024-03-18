21bdd813c322b953c9353bb402e6f32d6d39efb9
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-18 15:04:24 +0000

    Update booking-options-7.jsx

--- pages/booking-options-7.jsx
+++ pages/booking-options-7.jsx
@@ -18,28 +18,34 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name disco-saturdays">DISCO SATURDAYS</h1>
-        <div class="event-info">
-            <div class="info date"> 
-              <h2>16 Mar</h2>
-            </div>
-            <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Razor-N-Tape</h3>
-              <h3><span>TENT <b>//</b> </span>Luther Vine B2B Soho</h3>
+          <div class="event-info">
+            <div class="info artists eat-heading">
+              <h1>Eat at 17 Little Portland Street, London</h1>
             </div>
         </div>
-        <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-16" target="_blank">
-            <Button classes="events-button"  btnType="hollow">
-              Book Dinner 
-              <span>(includes free club entry)</span>
+        <div class="button-wrapper new-button-wrapper">
+            <Button classes="events-button new-events-button"  btnType="hollow">
+            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-20" target="_blank">
+              <span>Dinner+Tunes <br/>in the Tent</span>
+              <p class="cusine">(Middle Eastern)</p>
+              <p class="time">7PM - 9:30PM</p>
+             </a>
+             <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
+              [MENU]
+             </a>
             </Button>
-          </a>
-          <a href="https://sevn.ly/xm0wA2ti" target="_blank">
-            <Button classes="events-button ticket" btnType="hollow">
-               GET ON CLUB <span>GUESTLIST</span>
+
+            <Button classes="events-button ticket"  btnType="hollow">
+              <a href="https://www.little-portland.com/bookings" target="_blank">
+              <span>CHEF’S STUDIO</span>
+              <p class="cusine">(Chinese)</p>
+              <p class="cusine">Minimum 8 people</p>
+              <p class="time">8PM</p>
+             </a>
+             <a class="menu-btn" href="https://www.little-portland.com/chefs-menu" target="_blank">
+              [MENU]
+             </a>
             </Button>
-          </a>
         </div>
       </div>
     </>
