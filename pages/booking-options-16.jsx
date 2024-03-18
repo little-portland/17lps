e7b271410baa8df1280f9e66cfee153b1e031f63
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-18 12:58:32 +0000

    Create booking-options-16.jsx

---
+++ pages/booking-options-16.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Script from "next/script";
+
+//hooks
+import useDeviceDetect from "@utils/useDeviceDetect";
+
+import CenterContainer from "@components/UX/CenterContainer/CenterContainer";
+
+import Button from "@components/UX/Button";
+
+const BookingOptions = () => { 
+
+  return (
+    <>
+      <Head>
+        <title>Bookings </title> 
+      </Head>
+
+      <div id="bookingOptions">
+        <h1 class="event-name">Thursday Underground</h1>
+        <div class="event-info">
+            <div class="info date">
+              <h2>14 MAr</h2>
+            </div>
+            <div class="info artists">
+              <h3><span>STUDIO <b>//</b> </span>DAN ANDREI, Morgan</h3>
+              <h3><span>TENT <b>//</b> </span> DAVID AGRELLA B2B TRIXIE</h3> 
+            </div>
+        </div>
+        <div class="button-wrapper">
+            <Button classes="events-button"  btnType="hollow">
+             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-14" target="_blank">
+              Dinner+Tunes <br/>in the Tent
+              <p>(Middle Eastern)</p>
+              <p>7PM - 9:30PM</p>
+             </a>
+             <a href="https://www.little-portland.com/menu" target="_blank">
+              [MENU]
+             </a>
+            </Button>
+
+            <Button classes="events-button ticket"  btnType="hollow">
+              <a href="https://www.little-portland.com/bookings" target="_blank">
+              CHEF’S STUDIO
+              <p>(Chinese)</p>
+              <p>Minimum 8 people now</p>
+              <p>8PM</p>
+             </a>
+             <a href="https://www.little-portland.com/chefs-menu" target="_blank">
+              [MENU]
+             </a>
+            </Button>
+        </div>
+        <div class="button-wrapper">
+            <Button classes="events-button ticketNew" btnType="hollow">
+              <a href="https://sevn.ly/xE8RAEDy" target="_blank">
+                Book Club <p>Ticket</p>
+              </a>
+            </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookingOptions;
