e223fd919ce10486a4c6f98bb78712a4792e89c8
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-18 18:05:27 +0000

    Update booking-options-9.jsx

--- pages/booking-options-9.jsx
+++ pages/booking-options-9.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2>05 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>OHM HOURANI</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS </h3>
               <h3><span>TENT <b>//</b> </span>ROBERTA CUTOLO, TIM LANG</h3>
             </div>
         </div>
