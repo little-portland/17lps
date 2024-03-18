d8e83e5400ea083a70a35e0927b4debafec55b45
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-18 16:18:44 +0000

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>TBA</h3>
-              <h3><span>TENT <b>//</b> </span>GILLES SMITH, HARRI PEPPER</h3>
+              <h3><span>TENT <b>//</b> </span>GILES SMITH, HARRI PEPPER</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
