e2002b4a9444063497c17301186c16bd906dca91
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-20 20:15:42 +0000

    Update booking-options-3.jsx

--- pages/booking-options-3.jsx
+++ pages/booking-options-3.jsx
@@ -21,7 +21,7 @@ const BookingOptions = () => {
         <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2>21 Mar</h2>
+              <h2><span>THU</span>21 Mar</h2>
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>ADMNTI, HOLLY WHY, KEARUN</h3>
@@ -30,7 +30,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-21" target="_blank">
+            <a class="book-link" href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-21" target="_blank">
               <span>BOOK DINNER</span>
               <p class="time">Includes Free Club Entry</p>
              </a>
@@ -40,7 +40,7 @@ const BookingOptions = () => {
             </Button>
 
             <Button classes="events-button ticket"  btnType="hollow">
-              <a href="https://www.little-portland.com/bookings" target="_blank">
+              <a class="book-link" href="https://www.little-portland.com/bookings" target="_blank">
               <span>BOOK CHEF’S STUDIO</span>
               <p class="time">Includes Free Club Entry</p>
              </a>
