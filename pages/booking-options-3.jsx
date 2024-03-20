89f5e6529817043e3fc45e2e2cf079c6c8dc5ed7
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-20 19:33:50 +0000

    Update booking-options-3.jsx

--- pages/booking-options-3.jsx
+++ pages/booking-options-3.jsx
@@ -33,10 +33,9 @@ const BookingOptions = () => {
             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-21" target="_blank">
               <span>BOOK DINNER</span>
               <p class="time">Includes Free Club Entry</p>
-              <p class="cusine">(Middle Eastern)</p>
              </a>
              <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
-              [MENU]
+              [Middle Eastern MENU]
              </a>
             </Button>
 
@@ -44,10 +43,9 @@ const BookingOptions = () => {
               <a href="https://www.little-portland.com/bookings" target="_blank">
               <span>BOOK CHEF’S STUDIO</span>
               <p class="time">Includes Free Club Entry</p>
-              <p class="cusine">(Chinese)</p>
              </a>
              <a class="menu-btn" href="https://www.little-portland.com/chefs-menu" target="_blank">
-              [MENU]
+              [Chinese MENU]
              </a>
             </Button>
         </div>
