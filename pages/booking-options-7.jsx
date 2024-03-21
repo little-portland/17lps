8b4025d1f928b5d6da8d9e644902a940d20fda6d
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-21 14:59:40 +0000

    Update booking-options-7.jsx

--- pages/booking-options-7.jsx
+++ pages/booking-options-7.jsx
@@ -24,27 +24,29 @@ const BookingOptions = () => {
               <h2>17 Little Portland Street, London</h2> 
             </div>
         </div>
-        <div class="button-wrapper new-button-wrapper">
+       <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-20" target="_blank">
-              <span>Dinner+Tunes <br/>in the Tent</span>
-              <p class="cusine">(Middle Eastern)</p>
-              <p class="time">7PM - 9:30PM</p>
+            <a class="book-link" href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-22" target="_blank">
+              <span>BOOK DINNER</span>
+              <span class="space">IN THE TENT</span>
+              <p class="time">Includes Free Club Entry</p>
              </a>
+             <p class="dotted-divider first-divider"></p>
              <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
-              [MENU]
+              <span>[Middle Eastern MENU]</span>
              </a>
             </Button>
 
             <Button classes="events-button ticket"  btnType="hollow">
-              <a href="https://www.little-portland.com/bookings" target="_blank">
-              <span>CHEF’S STUDIO</span>
-              <p class="cusine">(Chinese)</p>
-              <p class="cusine">Minimum 8 people</p>
-              <p class="time">8PM</p>
+              <a class="book-link" href="https://www.little-portland.com/bookings" target="_blank">
+              <span>BOOK DINNER</span>
+              <span class="space">IN CHEF’S STUDIO</span>
+              <p class="time">Includes Free Club Entry</p>
+              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>£100PP</span></p>
              </a>
+             <p class="dotted-divider"></p>
              <a class="menu-btn" href="https://www.little-portland.com/chefs-menu" target="_blank">
-              [MENU]
+              <span>[Chinese MENU]</span>
              </a>
             </Button>
         </div>
