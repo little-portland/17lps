e4f64e21b65fd5a9b850324ecfb0b55406cf755f
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-25 14:14:33 +0000

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -28,7 +28,7 @@ const BookingOptions = () => {
               <h3><span>TENT <b>//</b> </span>MARCELINA WICK B2B B.AI</h3>
             </div>
         </div>
-        <div class="button-wrapper new-button-wrapper">
+        <div class="button-wrapper new-button-wrapper full-width-btn">
             <Button classes="events-button new-events-button"  btnType="hollow">
               <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-28" target="_blank">
               <span>BOOK DINNER</span>
@@ -38,19 +38,6 @@ const BookingOptions = () => {
              <p class="dotted-divider first-divider"></p>
              <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
               <span>[Middle Eastern MENU]</span>
-             </a>
-            </Button>
-
-            <Button classes="events-button ticket"  btnType="hollow">
-              <a class="book-link" href="https://www.little-portland.com/bookings" target="_blank">
-              <span>BOOK DINNER</span>
-              <span class="space">IN CHEF’S STUDIO</span>
-              <p class="time">Includes Free Club Entry</p>
-              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>£100PP</span></p>
-             </a>
-             <p class="dotted-divider"></p>
-             <a class="menu-btn" href="https://www.little-portland.com/chefs-menu" target="_blank">
-              <span>[Chinese MENU]</span>
              </a>
             </Button>
         </div>
