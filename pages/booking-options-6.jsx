3c94a3668329411151745d92492aa6086cff4b63
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-25 13:47:30 +0000

    Update booking-options-6.jsx

--- pages/booking-options-6.jsx
+++ pages/booking-options-6.jsx
@@ -18,27 +18,48 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name urban-future">Public image</h1>
+        <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
         <div class="event-info">
-            <div class="info date">
-              <h2>20 SEP</h2>
+           <div class="info date">
+              <h2><span>FRI</span>29 MAR</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>DJ NOMAD, iBO3Y, TAROIMAI, ZEZE, DJ PHENOM</h3>
-              <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & ROBERTO MANZIN [LIVE JAZZ]</h3>
+              <h3><span>STUDIO <b>//</b> </span>Silverlining</h3>
+              <h3><span>TENT <b>//</b> </span>Giles Smith, Harri Pepper</h3>
             </div>
         </div>
-        <div class="button-wrapper">
-          <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2023-09-20" target="_blank">
-            <Button classes="events-button"  btnType="hollow">
-              Book Dinner <span>(includes free club entry)</span>
+        <div class="button-wrapper new-button-wrapper">
+            <Button classes="events-button new-events-button"  btnType="hollow">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-29" target="_blank">
+              <span>BOOK DINNER</span>
+              <span class="space">IN THE TENT</span>
+              <p class="time">Includes Free Club Entry</p>
+             </a>
+             <p class="dotted-divider first-divider"></p>
+             <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
+              <span>[Middle Eastern MENU]</span>
+             </a>
             </Button>
-          </a>
-          <a href="http://sevn.ly/xIYmVJED" target="_blank">
-            <Button classes="events-button ticket" btnType="hollow">
-              GET ON CLUB <span>GUESTLIST</span>
+
+            <Button classes="events-button ticket"  btnType="hollow">
+              <a class="book-link" href="https://www.little-portland.com/bookings" target="_blank">
+              <span>BOOK DINNER</span>
+              <span class="space">IN CHEF’S STUDIO</span>
+              <p class="time">Includes Free Club Entry</p>
+              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>£100PP</span></p>
+             </a>
+             <p class="dotted-divider"></p>
+             <a class="menu-btn" href="https://www.little-portland.com/chefs-menu" target="_blank">
+              <span>[Chinese MENU]</span>
+             </a>
             </Button>
-          </a>
+        </div>
+        <div class="button-wrapper new-button-wrapper">
+            <Button classes="events-button ticketNew" btnType="hollow">
+              <a href="https://sevn.ly/xJqr8f7q" target="_blank">
+                CLUB GUESTLIST ONLY
+              </a>
+            </Button>
         </div>
       </div>
     </>
