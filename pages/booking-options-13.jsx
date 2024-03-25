6227b5fb4d243dfd09eb733637c7299295c990a1
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-25 14:15:03 +0000

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -56,7 +56,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="" target="_blank">
+            <a href="https://sevn.ly/xLDScVMK" target="_blank">
                 CLUB TICKETS ONLY
               </a>
             </Button>
