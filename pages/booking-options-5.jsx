0c2b5cb05b1666f7dff770b4c793c2e64d78f6b6
little-portland <110906749+little-portland@users.noreply.github.com>
2024-03-25 16:29:46 +0000

    Update booking-options-5.jsx

--- pages/booking-options-5.jsx
+++ pages/booking-options-5.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>13 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>GARRET DAVID, KENNY JONES, VITZH</h3>
+              <h3><span>STUDIO <b>//</b> </span>KENNY JONES, VITZH, Special Guest</h3>
               <h3><span>TENT <b>//</b> </span>CASSIUS & HARRI (TELTALE), SHARIF</h3>
             </div>
         </div>
