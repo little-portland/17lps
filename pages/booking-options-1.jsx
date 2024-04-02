d3a2916be776019cb7950cef53fecfe189ca369c
little-portland <110906749+little-portland@users.noreply.github.com>
2024-04-02 14:57:11 +0100

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>DJ PIERRE</h3>
-              <h3><span>TENT <b>//</b> </span>MAYBE LAURA & REMI MAZET , MILLIE MCKEE</h3>
+              <h3><span>TENT <b>//</b> </span>MAYBE LAURA, REMI MAZET, MILLIE MCKEE</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
