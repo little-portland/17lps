ebfcb02348c48220b3ccdb53cd274a1e4dbda385
little-portland <110906749+little-portland@users.noreply.github.com>
2024-04-02 13:16:33 +0100

    Update booking-options-12.jsx

--- pages/booking-options-12.jsx
+++ pages/booking-options-12.jsx
@@ -18,27 +18,21 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name dinner-tunes">Dinner+Tunes</h1>
+        <h1 class="event-name dinner-tunes">After Party</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>28 Mar</h2>
+              <h2><span>Sun</span>07 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Closed</h3>
-              <h3><span>TENT <b>//</b> </span>MARCELINA WICK B2B B.AI</h3>
+              <h3><span>STUDIO <b>//</b> </span>Hermanos Gutiérrez</h3>
+              <h3><span>TENT <b>//</b> </span>Closed</h3>
             </div>
         </div>
-        <div class="button-wrapper new-button-wrapper full-width-btn">
-            <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-28" target="_blank">
-              <span>BOOK DINNER</span>
-              <span class="space">IN THE TENT</span>
-              <p class="time">Includes Free Club Entry</p>
-             </a>
-             <p class="dotted-divider first-divider"></p>
-             <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
-              <span>[Middle Eastern MENU]</span>
-             </a>
+        <div class="button-wrapper new-button-wrapper">
+            <Button classes="events-button ticketNew" btnType="hollow">
+              <a href="https://sevn.ly/xUSQptYT" target="_blank">
+                Book Club Ticket
+              </a>
             </Button>
         </div>
       </div>
