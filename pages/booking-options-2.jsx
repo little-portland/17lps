3f877fc26ae860e82274b2ba5a4631f61dbd8fc2
little-portland <110906749+little-portland@users.noreply.github.com>
2024-04-08 15:31:18 +0100

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>FRI</span>29 Mar</h2>
+              <h2><span>FRI</span>26 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>SILVERLINING</h3>
-              <h3><span>TENT <b>//</b> </span>GILES SMITH, HARRI PEPPER</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>TENT <b>//</b> </span>FRANCESCO MAMI, ROBERTA CUTOLO </h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-29" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-04-26" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -56,7 +56,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xJqr8f7q" target="_blank">
+              <a href="" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
