c65fe229b7fc030eff016c75d05de00908fa6aff
little-portland <110906749+little-portland@users.noreply.github.com>
2024-04-09 16:47:40 +0100

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -24,8 +24,8 @@ const BookingOptions = () => {
               <h2><span>SAT</span>27 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
-              <h3><span>TENT <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>ROB MELLO</h3>
+              <h3><span>TENT <b>//</b> </span>GREGORIO SOAVE, KEM</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
