8dff814c2e7e30047f95b3fd1ec1548874d24529
little-portland <110906749+little-portland@users.noreply.github.com>
2024-04-09 12:28:35 +0100

    Update booking-options-5.jsx

--- pages/booking-options-5.jsx
+++ pages/booking-options-5.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>ALEX FROM UTOPIA & FRIENDS</h3>
-              <h3><span>TENT <b>//</b> </span>NICK WILLIAMS, IPATO</h3>
+              <h3><span>TENT <b>//</b> </span>NICK WILLIAMS, PATO</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
