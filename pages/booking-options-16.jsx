b7b60f7a5c4002039b0e8d13edb17c2b4b0147ad
little-portland <110906749+little-portland@users.noreply.github.com>
2024-04-16 14:06:56 +0100

    Update booking-options-16.jsx

--- pages/booking-options-16.jsx
+++ pages/booking-options-16.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>BUSHWACKA, LAURENCE</h3>
-              <h3><span>TENT <b>//</b> </span> FUNKTONIO, MR SHIVER</h3> 
+              <h3><span>TENT <b>//</b> </span>Mr Shiver, Rhythms of Prescott</h3> 
             </div>
         </div>
          <div class="button-wrapper new-button-wrapper">
