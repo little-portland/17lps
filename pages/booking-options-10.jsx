635b9ce7d2f9ece24cabc05d01a44d1275dc6fef
little-portland <110906749+little-portland@users.noreply.github.com>
2024-04-16 11:53:26 +0100

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
-              <h3><span>TENT <b>//</b> </span>GILES SMITH</h3>
+              <h3><span>TENT <b>//</b> </span>GILES SMITH, TIM LANG</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
