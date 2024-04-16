cf9794fda23070a01f548f4e4e8d3c02b52a73c5
little-portland <110906749+little-portland@users.noreply.github.com>
2024-04-16 14:07:19 +0100

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>19 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, ISAAC CARTER</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, Funktonio</h3>
               <h3><span>TENT <b>//</b> </span>MAYBE LAURA & REMI MAZET, MILLIE MCKEE</h3>
             </div>
         </div>
