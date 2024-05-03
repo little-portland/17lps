22ba6c544f019479b45c5b25862d944599f230f2
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-03 17:08:08 +0100

    Update booking-options-5.jsx

--- pages/booking-options-5.jsx
+++ pages/booking-options-5.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name disco-saturdays">Phonica Presents</h1>
+        <h1 class="event-name phonica">Phonica Presents</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>SAT</span>25 May</h2>
