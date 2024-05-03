7eb755c4702588e7c68b3c8ded811a017c310798
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-03 17:03:59 +0100

    Update booking-options-5.jsx

--- pages/booking-options-5.jsx
+++ pages/booking-options-5.jsx
@@ -18,19 +18,19 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
+        <h1 class="event-name disco-saturdays">Phonica Presents</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>SAT</span>13 Apr</h2>
+              <h2><span>SAT</span>25 May</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>ALEX FROM UTOPIA & FRIENDS</h3>
-              <h3><span>TENT <b>//</b> </span>NICK WILLIAMS, PATO</h3>
+              <h3><span>STUDIO <b>//</b> </span>KIERAN JANDU, ROB MELLO</h3>
+              <h3><span>TENT <b>//</b> </span>AVSLUTA, LUTHER VINE</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-04-13" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-05-25" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -56,7 +56,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/x51EAMqz" target="_blank">
+              <a href="" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
