4a8dd057ee26923682761d1f2968c4220135c2d3
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-03 17:00:13 +0100

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -14,23 +14,23 @@ const BookingOptions = () => {
   return (
     <>
       <Head>
-        <title>Bookings</title>
+        <title>Bookings </title>
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name focal-point-presents">Focal Point Presents</h1>
+        <h1 class="event-name urban-future">Public image</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>02 May</h2>
+              <h2><span>WED</span>22 May</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>DAVE HARVEY, WILLOW</h3>
-              <h3><span>TENT <b>//</b> </span>B.LOVE, JOSH TWEEK</h3>
+              <h3><span>STUDIO<b>//</b> </span>TBA</h3>
+              <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & MAFALDA RAMOS</h3>
             </div>
         </div>
-        <div class="button-wrapper new-button-wrapper">
+         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-            <a class="book-link" href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-05-02" target="_blank">
+             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-05-22" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,8 +59,8 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xKnGmztg" target="_blank">
-                CLUB TICKETS ONLY
+            <a href="" target="_blank">
+                CLUB GUESTLIST ONLY
               </a>
             </Button>
         </div>
