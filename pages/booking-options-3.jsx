027ed0bf886f02880d9ce851a040dad581303c21
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-07 10:48:30 +0100

    Update booking-options-3.jsx

--- pages/booking-options-3.jsx
+++ pages/booking-options-3.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>THU</span>23 MAY</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Vera  & Bobby </h3>
+              <h3><span>STUDIO <b>//</b> </span>BOBBY., VERA</h3>
               <h3><span>TENT <b>//</b> </span>LUCA ANDREOZZI & TONY SERBAN</h3>
             </div>
         </div>
