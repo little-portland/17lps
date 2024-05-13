e6d64c230953dbab92a8a319084dfa51f5fe336b
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-13 15:14:56 +0100

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>17 May</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, CASSY</h3>
               <h3><span>TENT <b>//</b> </span>FREDERIKA, JUNGLE JARVIN </h3>
             </div>
         </div>
