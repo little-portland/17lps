b3395a7ade49e822d8f36133f253b46f986f7b56
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-13 15:19:19 +0100

    Update booking-options-6.jsx

--- pages/booking-options-6.jsx
+++ pages/booking-options-6.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
         <div class="event-info">
            <div class="info date">
-              <h2><span>FRI</span>29 MAR</h2>
+              <h2><span>FRI</span>31 May</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Silverlining</h3>
-              <h3><span>TENT <b>//</b> </span>Giles Smith, Harri Pepper B2B Oliverio Segura</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
+              <h3><span>TENT <b>//</b> </span>ECE DUZGIT, HARRI PEPPER</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-03-29" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-05-31" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -46,17 +46,20 @@ const BookingOptions = () => {
               <span>BOOK DINNER</span>
               <span class="space">IN CHEF’S STUDIO</span>
               <p class="time">Includes Free Club Entry</p>
-              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>£100PP</span></p>
+              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>FROM £80PP</span></p>
              </a>
              <p class="dotted-divider"></p>
+             <a class="menu-btn" href="https://www.little-portland.com/set-menu" target="_blank">
+              <span>[MIDDLE EASTERN]</span>
+             </a>
              <a class="menu-btn" href="https://www.little-portland.com/chefs-menu" target="_blank">
-              <span>[Chinese MENU]</span>
+              <span>[CHINESE]</span>
              </a>
             </Button>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xJqr8f7q" target="_blank">
+              <a href="" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
