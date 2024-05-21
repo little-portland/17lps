1af22690534d1d7cb7168c1123bed289b4f800e5
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-21 15:16:58 +0100

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, RENATO RATIER</h3>
-              <h3><span>TENT <b>//</b> </span>HARRI PEPPER, KEM</h3>
+              <h3><span>TENT <b>//</b> </span>KEM, TIM LANG</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
