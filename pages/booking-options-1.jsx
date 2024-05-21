b6131c71f86b1f86691fb523d26131a0a946b366
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-21 13:49:37 +0100

    Update booking-options-1.jsx

--- pages/booking-options-1.jsx
+++ pages/booking-options-1.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name urban-future">Public image</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>WED</span>15 May</h2>
+              <h2><span>WED</span>05 June</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO<b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO<b>//</b> </span>DJ HARSH, JHERI MAYNE, LILO GALERIA, LILO, TLADSS, VILLABEATZ</h3>
               <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & MAFALDA RAMOS</h3>
             </div>
         </div>
          <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-05-15" target="_blank">
+             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-06-01" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xii3wXFW" target="_blank">
+            <a href="" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
