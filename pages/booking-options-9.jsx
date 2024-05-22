ceaf05c440b6ccf9c6775e550b278355373ac934
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-22 11:21:11 +0100

    Update booking-options-9.jsx

--- pages/booking-options-9.jsx
+++ pages/booking-options-9.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>08 JUN</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>RALPH LAWSON, RUFF DUG</h3>
+              <h3><span>STUDIO <b>//</b> </span>RALPH LAWSON, RUF DUG</h3>
               <h3><span>TENT <b>//</b> </span>TINTIN</h3>
             </div>
         </div>
