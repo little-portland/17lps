0225574b77e1523d5e3185300f5d2b41021ac336
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-22 11:20:39 +0100

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>07 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, RENATO RATIER</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
               <h3><span>TENT <b>//</b> </span>KEM, TIM LANG</h3>
             </div>
         </div>
