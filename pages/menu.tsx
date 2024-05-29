76ef6f398b7da2491f2f5dda7538f4a06159b0fa
little-portland <110906749+little-portland@users.noreply.github.com>
2024-05-29 13:05:22 +0100

    Update menu.tsx

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -18,12 +18,12 @@ const Menu = ({ menuImage }) => {
       <CenterContainer>
         {menuImage && (
           <Image
-            src={"/images/17LPS_MenU_220823.jpg"}
+            src={"/images/17LPS_sample_menu.jpg"}
             alt={menuImage.title}
             className={"image"}
             width={menuImage.width} //automatically provided
             height={menuImage.height} //automatically provided
-            blurDataURL={"/images/17LPS_MenU_220823.jpg"} //automatically provided
+            blurDataURL={"/images/17LPS_sample_menu.jpg"} //automatically provided
             placeholder="blur" // Optional blur-up while loading
           />
         )}
