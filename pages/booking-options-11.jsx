acec79073be6a2c6f86510201a76a9ffcc81fb13
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-03 16:59:19 +0100

    Update booking-options-11.jsx

--- pages/booking-options-11.jsx
+++ pages/booking-options-11.jsx
@@ -18,26 +18,26 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name urban-future">Public image</h1>
+        <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>WED</span>08 May</h2>
+              <h2><span>SAT</span>22 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO<b>//</b> </span>TBA</h3>
-              <h3><span>TENT <b>//</b> </span>MARIO BAKUNA & MAFALDA RAMOS</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>TENT <b>//</b> </span>NICK WILLIAMS, REMI MAZET & MAYBE LAURA</h3>
             </div>
         </div>
-         <div class="button-wrapper new-button-wrapper">
+        <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-05-08" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-06-22" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
              </a>
              <p class="dotted-divider first-divider"></p>
              <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
-              <span>[Middle Eastern MENU]</span>
+              <span>[MENU]</span>
              </a>
             </Button>
 
@@ -46,20 +46,20 @@ const BookingOptions = () => {
               <span>BOOK DINNER</span>
               <span class="space">IN CHEF’S STUDIO</span>
               <p class="time">Includes Free Club Entry</p>
-              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>FROM £80PP</span></p>
+              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>£100PP</span></p>
              </a>
              <p class="dotted-divider"></p>
-             <a class="menu-btn" href="https://www.little-portland.com/set-menu" target="_blank">
-              <span>[MIDDLE EASTERN]</span>
+             <a class="menu-btn" href="https://www.little-portland.com/regular-set-menu" target="_blank">
+              <span>[REGULAR MENU]</span>
              </a>
-             <a class="menu-btn" href="https://www.little-portland.com/chefs-menu" target="_blank">
-              <span>[CHINESE]</span>
+             <a class="menu-btn premium-menu-btn" href="https://www.little-portland.com/premium-set-menu" target="_blank">
+              <span>[PREMIUM MENU]</span>
              </a>
             </Button>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xeUYDqGa" target="_blank">
+              <a href="" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
