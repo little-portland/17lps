15d8eea3fe4c284cf862533537c852ad59f6292c
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-05 12:29:38 +0100

    Update booking-options-6.jsx

--- pages/booking-options-6.jsx
+++ pages/booking-options-6.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, OHM HOURANI</h3>
-              <h3><span>TENT <b>//</b> </span>HARRI PEPPER, MR SHIVER</h3>
+              <h3><span>TENT <b>//</b> </span>KEM, MR SHIVER</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
