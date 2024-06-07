65a98cd74011f4fe2a0bc4ef8987aad99182371d
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-07 17:05:16 +0100

    Update nocturn-02.tsx

--- pages/nocturn-02.tsx
+++ pages/nocturn-02.tsx
@@ -25,41 +25,12 @@ const Menu = ({ menuImage }) => {
            <img src="/images/nocturn-web-page.png" alt="Nocturn" width="100%" height="100%" />
 
            <div class="row">
-  <div class="column">
-    <img src="wedding.jpg">
-    <img src="rocks.jpg">
-    <img src="falls2.jpg">
-    <img src="paris.jpg">
-    <img src="nature.jpg">
-    <img src="mist.jpg">
-    <img src="paris.jpg">
-  </div>
-  <div class="column">
-    <img src="underwater.jpg">
-    <img src="ocean.jpg">
-    <img src="wedding.jpg">
-    <img src="mountainskies.jpg">
-    <img src="rocks.jpg">
-    <img src="underwater.jpg">
-  </div>
-  <div class="column">
-    <img src="wedding.jpg">
-    <img src="rocks.jpg">
-    <img src="falls2.jpg">
-    <img src="paris.jpg">
-    <img src="nature.jpg">
-    <img src="mist.jpg">
-    <img src="paris.jpg">
-  </div>
-  <div class="column">
-    <img src="underwater.jpg">
-    <img src="ocean.jpg">
-    <img src="wedding.jpg">
-    <img src="mountainskies.jpg">
-    <img src="rocks.jpg">
-    <img src="underwater.jpg">
-  </div>
-</div>
+            <div class="column">
+              <img src="/images/nocturn-web-page.png">
+              <img src="/images/nocturn-web-page.png">
+              <img src="/images/nocturn-web-page.png">
+            </div>
+           </div>
          </div>
     </>
   );
