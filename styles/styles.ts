15b7384e86f947848ed465a2ac8b434ad6b38f32
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-10 17:04:38 +0100

    Update styles.ts

--- styles/styles.ts
+++ styles/styles.ts
@@ -11,7 +11,7 @@ export const breakpoints = {
 export const GlobalStyle = createGlobalStyle` 
 
 @import url("https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap");
-@import url("https://fonts.cdnfonts.com/css/helvetica-neue-5");
+@import url("https://fonts.cdnfonts.com/css/helvetica-neue-55");
 
 @font-face {
   font-family: "Space Mono";
