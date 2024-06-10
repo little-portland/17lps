631f2b154b8e1effb24887e5ad65231eaf20292f
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-10 20:02:30 +0100

    Update nocturn-02.tsx

--- pages/nocturn-02.tsx
+++ pages/nocturn-02.tsx
@@ -26,7 +26,7 @@ const Menu = ({ menuImage }) => {
             {'.nocturn-text-wrapper{ margin-top: 30px!important;padding: 0 30px;}'}
             {'.nocturn-text-wrapper img{ max-width: 25%;margin-bottom: 30px!important;}'}
             {'.nocturn-text{ font-family: Helvetica!important;font-weight: bold!important;text-decoration: none!important;color: #0068ff!important;padding-bottom: 30px;line-height: 1.1;}'}
-            {'.nocturn-text{ font-family: padding-bottom: 20px;}'}
+            {'.nocturn-text{ padding-bottom: 20px;}'}
             {'@media (max-width: 768px) { .nocturn{width: 90%;}.nocturn-text-wrapper{padding: 0 15px;}.top{ font-size: 20px;}.bottom{ font-size: 15px;}.row{padding: 0!important;}.column{ flex: 50%;max-width: 50%;}}'}
         </style>
         <title>Nocturn - Zoë Marden</title>
