d54a2aa8684d034edba346d5014f6725b9352a01
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-21 13:17:20 +0100

    Update nocturn.tsx

--- pages/nocturn.tsx
+++ pages/nocturn.tsx
@@ -35,112 +35,24 @@ const Menu = ({ menuImage }) => {
          <div className="nocturn">
            <img src="/images/nocturn-main-web-page-header.png" alt="Nocturn" width="100%" />
 
-           <div className="nocturn-text-wrapper top"> 
-              <p className="nocturn-text">crave rave creatures of the deep this is our home slip slip 
-slip into another skin within down under daddy ten 
-thousand leagues below the surface descend into the 
-hadal zone where life has come undone every breath 
-sacred in this watery grave sing your death song and be 
-born again shadow dancing the night sea journey turn the 
-key touch tentacles far reaching seething teething a rip in 
-the space time continuum the boundaries sizzle and melt 
-felt another portal crack open the walls they will not hold 
-this is a story not yet told
-</p>
-<p className="nocturn-text second">fold into my luscious skin swim meet the desires 
-dormant within we creatures emerge red hot geyser slip 
-slip slip leave your inhibitions on land we demand plunge 
-soak me up like a sponge and survive dive no need to hide 
-carve open your insides out shout time and space bend 
-there is no end light travels not in the farthest trench of the 
-den slip slip slip into the mud slip slip slip slip slip slip in
-within
-</p>
-<p> 
-  <img src="/images/nocturn/17LPS_Nocturne2_Whole_email_bianca-quote.png" />
-  </p>
-            </div>
-
            <div className="row">
               <div className="column">
-                <img src="/images/nocturn/nocturn02_21.png" />
-                <img src="/images/nocturn/nocturn02_01.png" />
-                <img src="/images/nocturn/nocturn02_02.png" />
-                <img src="/images/nocturn/nocturn02_03.png" />
-                <img src="/images/nocturn/nocturn02_04.png" />
-                <img src="/images/nocturn/nocturn02_05.png" />
-                <img src="/images/nocturn/nocturn02_13.png" />
-                <img src="/images/nocturn/nocturn02_06.png" />
-                <img src="/images/nocturn/nocturn02_07.png" />
-                <img src="/images/nocturn/nocturn02_08.png" />
-                <a href="https://www.youtube.com/watch?v=BWKKJwBXir4" target="_blank">
-                  <img src="/images/nocturn/nocturn02_video_01.png" />
+                <a href="https://www.little-portland.com/nocturn-01" target="_blank">
+                  <img src="/images/nocturn/17LPS_Flyer_Nocturn_1.jpg" />
                 </a>
-                <img src="/images/nocturn/nocturn02_09.png" />
-                <img src="/images/nocturn/nocturn02_41.png" />
-                <img src="/images/nocturn/nocturn02_42.png" />
-                <img src="/images/nocturn/nocturn02_10.png" />
-                <img src="/images/nocturn/nocturn02_14.png" />
-                <img src="/images/nocturn/nocturn02_11.png" />
-                <a href="https://www.youtube.com/watch?v=KtFFmDGIsa4" target="_blank">
-                  <img src="/images/nocturn/nocturn02_video_04.png" />
-                </a>
-                <img src="/images/nocturn/nocturn02_12.gif" />
-                <img src="/images/nocturn/nocturn02_15.png" />
-                <img src="/images/nocturn/nocturn02_18.png" />
-                <img src="/images/nocturn/nocturn02_19.png" />
-                <img src="/images/nocturn/nocturn02_20.png" />
-                <img src="/images/nocturn/nocturn02_43.png" />
-                <img src="/images/nocturn/nocturn02_44.png" />
-                <img src="/images/nocturn/nocturn02_45.png" />
-                <a href="https://www.smithsonianmag.com/science-nature/creatures-of-the-deep-163679120/" target="_blank">
-                  <img src="/images/nocturn/nocturn02_video_03.png" />
-                </a>
+                <div className="nocturn-text-wrapper top"> 
+                  <p className="nocturn-text">crave rave creatures of the deep this is our home slip slip </p>
+                </div>
               </div>
               <div className="column">
-                <img src="/images/nocturn/nocturn02_23.png" />
-                <a href="https://medium.com/@rachellee9087/the-titan-submarine-disaster-what-really-happened-c40a1acde9aa" target="_blank">
-                  <img src="/images/nocturn/nocturn02_video_05.png" />
+                <a href="https://www.little-portland.com/nocturn-02" target="_blank">
+                  <img src="/images/nocturn/17LPS_Flyer_Nocturn_2.jpg" />
                 </a>
-                <img src="/images/nocturn/nocturn02_16.png" />
-                <img src="/images/nocturn/nocturn02_24.png" />
-                <img src="/images/nocturn/nocturn02_25.png" />
-                <a href="https://www.youtube.com/watch?v=8oL6u9eujSU" target="_blank">
-                  <img src="/images/nocturn/nocturn02_video_06.png" />
-                </a>
-                <img src="/images/nocturn/nocturn02_26.png" />
-                <img src="/images/nocturn/nocturn02_27.png" />
-                <img src="/images/nocturn/nocturn02_17.png" />
-                <img src="/images/nocturn/nocturn02_28.png" />
-                <img src="/images/nocturn/nocturn02_29.png" />
-                <a href="https://www.youtube.com/watch?v=fDwHwWuUuk4" target="_blank">
-                  <img src="/images/nocturn/nocturn02_video_07.png" />
-                </a>
-                 <img src="/images/nocturn/nocturn02_22.png" />
-                <img src="/images/nocturn/nocturn02_30.png" />
-                <img src="/images/nocturn/nocturn02_46.png" />
-                <img src="/images/nocturn/nocturn02_31.png" />
-                <a href="https://www.e-flux.com/journal/75/67125/tentacular-thinking-anthropocene-capitalocene-chthulucene/" target="_blank">
-                  <img src="/images/nocturn/nocturn02_video_02.png" />
-                </a>
-                <img src="/images/nocturn/nocturn02_32.png" />
-                <img src="/images/nocturn/nocturn02_33.png" />
-                <audio controls src="/images/nocturn/nocturn02_34.mp3"></audio>
-                <img src="/images/nocturn/nocturn02_36.png" />
-                <img src="/images/nocturn/nocturn02_37.png" />
-                <img src="/images/nocturn/nocturn02_38.png" />
-                <img src="/images/nocturn/nocturn02_39.png" />
-                <img src="/images/nocturn/nocturn02_40.png" />
-                <img src="/images/nocturn/nocturn02_47.png" />
-                <img src="/images/nocturn/nocturn02_48.png" />
+                <div className="nocturn-text-wrapper top"> 
+                  <p className="nocturn-text">crave rave creatures of the deep this is our home slip slip </p>
+                </div>
               </div>
            </div>
-           
-            <div className="nocturn-text-wrapper bottom"> 
-              <p className="nocturn-text">Zoë Marden is an artist, filmmaker, and writer living between her home town of Hong Kong and London. She graduated from the Royal College of Art’s Moving Image MA, Marden works with performance, video, text, sound, sculpture and installation to create alternate worlds and speculative futures. Her research-focused works are concerned with Post-human feminisms and where they overlap with the postcolonial. Her intimate performances play with the voice, activating soundscapes of desire and vulnerability. She is part of the CAMPerVAN collective, active since 2016, a nomadic queer performance platform that can be deployed anywhere in the world to bring performance art, film screening, panel discussion & workshop into the public realm.</p>
-              <p><a className="nocturn-text" href="https://www.instagram.com/zoemarden/" target="_blank">@zoemarden</a></p>
-              <p><a className="nocturn-text" href="https://www.zoemarden.com/" target="_blank">www.zoemarden.com</a></p>
-            </div>
            
          </div>      
     </>
