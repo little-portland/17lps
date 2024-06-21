0ca762c299e40a7abb34ca5885d017756cd20d54
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-21 14:25:52 +0100

    Update nocturn.tsx

--- pages/nocturn.tsx
+++ pages/nocturn.tsx
@@ -28,6 +28,7 @@ const Menu = ({ menuImage }) => {
             {'.nocturn .flyer:hover{border: 1px solid #bfff10;filter: grayscale(50%);}'}
             {'.nocturn-text{ font-family: Helvetica!important;font-weight: bold!important;text-decoration: none!important;color: #dddac3!important;padding-bottom: 30px;line-height: 1.1;}'}
             {'.nocturn-text{ padding-bottom: 20px;}'}
+            {'.nocturn a:hover{color: #ff00ff;}'}
             {'@media (max-width: 768px) { .nocturn{width: 90%;}.nocturn-text-wrapper{padding: 0 15px;}.top{ font-size: 20px;}.bottom{ font-size: 15px;}.row{padding: 0!important;}.column{ flex: 50%;max-width: 50%;}}'}
         </style>
         <title>Nocturn</title>
