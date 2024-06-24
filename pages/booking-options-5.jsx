e0d5b50ca4bfee18ce3ec388f218399851e2f71c
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-24 17:02:49 +0100

    Update booking-options-5.jsx

--- pages/booking-options-5.jsx
+++ pages/booking-options-5.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>SAT</span>15 Jun</h2>
+              <h2><span>SAT</span>13 Jul</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>alex bradley, Jaime Fiorito</h3>
-              <h3><span>TENT <b>//</b> </span>RHYTHMS OF PRESCOTT, SAM KARAM</h3>
+              <h3><span>STUDIO <b>//</b> </span>Mr Shiver, Nancy Noise</h3>
+              <h3><span>TENT <b>//</b> </span>MILLIE MCKEE, SHERIF</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-06-15" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-07-13" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xVdpSMHG" target="_blank">
+              <a href="https://sevn.ly/xAEXC24j" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
