926d5802f7fa6b024328d3b81499186ab4e6375f
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-25 09:43:45 +0100

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -24,8 +24,8 @@ const BookingOptions = () => {
               <h2><span>FRI</span>05 JUL</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
-              <h3><span>TENT <b>//</b> </span>ROBERTA CUTOLO</h3>
+              <h3><span>STUDIO <b>//</b> </span>DAVID TRIANA, KYPER YOO</h3>
+              <h3><span>TENT <b>//</b> </span>PAUL DAY, ROBERTA CUTOLO</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
