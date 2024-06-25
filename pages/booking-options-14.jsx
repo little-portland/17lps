67962f0878b923227b9dd8cfee519e44226dd027
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-25 09:42:52 +0100

    Update booking-options-14.jsx

--- pages/booking-options-14.jsx
+++ pages/booking-options-14.jsx
@@ -60,7 +60,7 @@ const BookingOptions = () => {
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
               <a href="https://sevn.ly/xv1tOKeT" target="_blank">
-                CLUB GUESTLIST ONLY
+                TENT GUESTLIST ONLY
               </a>
             </Button>
         </div>
