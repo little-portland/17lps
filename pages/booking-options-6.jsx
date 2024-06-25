97f53bca690f5878fd9f015f5d4a0b09ccb53268
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-25 09:45:30 +0100

    Update booking-options-6.jsx

--- pages/booking-options-6.jsx
+++ pages/booking-options-6.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>12 JUL</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>ISAAC CARTER</h3>
               <h3><span>TENT <b>//</b> </span>FREDERIKA, SOHO</h3>
             </div>
         </div>
