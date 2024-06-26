442aecc94536446eee42d4bc3735bbe7a77bf77b
little-portland <110906749+little-portland@users.noreply.github.com>
2024-06-26 12:08:56 +0100

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>29 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Greg Wilson</h3>
+              <h3><span>STUDIO <b>//</b> </span>Davide Del Vecchio, Greg Wilson</h3>
               <h3><span>TENT <b>//</b> </span>ECE DUZGÏT, Tim lang</h3>
             </div>
         </div>
