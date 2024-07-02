5c4938d5fc878841a2691acb1a6756949498a285
little-portland <110906749+little-portland@users.noreply.github.com>
2024-07-02 12:18:26 +0100

    Update booking-options-4.jsx

--- pages/booking-options-4.jsx
+++ pages/booking-options-4.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>27 JUN</h2>
+              <h2><span>THU</span>18 JUL</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>SILVERLINING B2B TRIXIE</h3>
-              <h3><span>TENT <b>//</b> </span>DEMI, OLI SILVA</h3>
+              <h3><span>STUDIO <b>//</b> </span>Chavez, max, vinyl speed adjust</h3>
+              <h3><span>TENT <b>//</b> </span>TONY SERBAN B2B LUCAS ANDREOZZI</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-06-27" target="_blank">
+            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-07-18" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xkLMRXcu" target="_blank">
+            <a href="" target="_blank">
                 CLUB TICKETS ONLY
               </a>
             </Button>
