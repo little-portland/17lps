9d647e14b2524ba34bfd28f33fdd847c90cc80aa
little-portland <110906749+little-portland@users.noreply.github.com>
2024-07-09 10:56:49 +0100

    Update booking-options-16.jsx

--- pages/booking-options-16.jsx
+++ pages/booking-options-16.jsx
@@ -1,4 +1,4 @@
- import React, { useEffect } from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import Script from "next/script";
 
@@ -14,23 +14,23 @@ const BookingOptions = () => {
   return (
     <>
       <Head>
-        <title>Bookings</title>
+        <title>Bookings </title>
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name openlab-presents">UN_MUTE PRESENTS</h1>
+        <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>06 Jun</h2>
+              <h2><span>SAT</span>27 Jul</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>ALEC FALCONER, DAVID AGRELLA, MARIO LIBERTI</h3>
-              <h3><span>TENT <b>//</b> </span>DAVIDE DEL VECCHIO, Harri Pepper & KT</h3>
+              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>TENT <b>//</b> </span>Funktonio, Pash</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-            <a class="book-link" href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-06-06" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-07-27" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -40,12 +40,13 @@ const BookingOptions = () => {
               <span>[MENU]</span>
              </a>
             </Button>
+
             <Button classes="events-button ticket"  btnType="hollow">
               <a class="book-link" href="mailto:eat@little-portland.com?subject=Chef’s Studio" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN CHEF’S STUDIO</span>
               <p class="time">Includes Free Club Entry</p>
-              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>FROM £80PP</span></p>
+              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>£100PP</span></p>
              </a>
              <p class="dotted-divider"></p>
              <a class="menu-btn" href="https://www.little-portland.com/regular-set-menu" target="_blank">
@@ -58,8 +59,8 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/x4ONsDlW" target="_blank">
-                CLUB TICKETS ONLY
+              <a href="https://sevn.ly/xAvr1dmu" target="_blank">
+                CLUB GUESTLIST ONLY
               </a>
             </Button>
         </div>
