e0874419ee8ca9be822779c414c951ef744b163c
little-portland <110906749+little-portland@users.noreply.github.com>
2024-07-16 10:20:51 +0100

    Update booking-options-16.jsx

--- pages/booking-options-16.jsx
+++ pages/booking-options-16.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>27 Jul</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>TBA</h3>
+              <h3><span>STUDIO <b>//</b> </span>JACOB HUSLEY & FRIENDS</h3>
               <h3><span>TENT <b>//</b> </span>Funktonio, Pash</h3>
             </div>
         </div>
