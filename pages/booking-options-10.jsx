43d8c7bdfd3311c76af36b7a23a07caa82b203e0
little-portland <110906749+little-portland@users.noreply.github.com>
2024-07-23 14:29:03 +0100

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>26 JUL</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
               <h3><span>TENT <b>//</b> </span>MARI*, Tim Lang</h3>
             </div>
         </div>
