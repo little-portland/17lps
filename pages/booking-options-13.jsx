96272cc07798d848f6a07fbc02f6ef1d1b9c0d6f
little-portland <110906749+little-portland@users.noreply.github.com>
2024-07-30 11:04:20 +0100

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>SAT</span>20 Jul</h2>
+              <h2><span>SAT</span>17 Aug</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>Heels & Souls, Sean OD</h3>
-              <h3><span>TENT <b>//</b> </span>BABA ALI, LUTHER VINE</h3>
+              <h3><span>STUDIO <b>//</b> </span>Ferny, David Triana</h3>
+              <h3><span>TENT <b>//</b> </span>STCKMAN, TONY SERBAN</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-07-20" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-08-17" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/x1ziTRPH" target="_blank">
+              <a href="https://sevn.ly/xDoiCdgA" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
