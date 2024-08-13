3f085db835c9cc7a66cd6d933553b2c006761627
little-portland <110906749+little-portland@users.noreply.github.com>
2024-08-13 12:04:37 +0100

    Update booking-options-13.jsx

--- pages/booking-options-13.jsx
+++ pages/booking-options-13.jsx
@@ -24,8 +24,8 @@ const BookingOptions = () => {
               <h2><span>SAT</span>17 Aug</h2>
             </div>
             <div class="info artists">
-              <h3><span>STUDIO <b>//</b> </span>David Triana, Ferny</h3>
               <h3><span>TENT <b>//</b> </span>STCKMAN, TONY SERBAN</h3>
+              <h3><span>STUDIO <b>//</b> </span>David Triana, Ferny</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
