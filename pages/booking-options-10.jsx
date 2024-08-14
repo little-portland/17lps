02c6de68277c8cc96ef94eff56045f602343d6b8
little-portland <110906749+little-portland@users.noreply.github.com>
2024-08-14 12:24:58 +0100

    Update booking-options-10.jsx

--- pages/booking-options-10.jsx
+++ pages/booking-options-10.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>HARRI PEPPER, Marcelina</h3>
-              <h3><span>STUDIO <b>//</b> </span>ISAAC CARTER</h3>
+              <h3><span>STUDIO <b>//</b> </span>ISAAC CARTER, Nat Wendell</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
