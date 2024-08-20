482cb0d30e18e998546449e94b8b43c18b4c8c92
little-portland <110906749+little-portland@users.noreply.github.com>
2024-08-20 13:05:42 +0100

    Update booking-options-6.jsx

--- pages/booking-options-6.jsx
+++ pages/booking-options-6.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>JUNGLE JARVIN, ROBERTA CUTOLO</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, JONNY ROCK</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
