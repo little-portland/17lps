df33ccfcd23c67e5b34eca1350058d163adcff0a
little-portland <110906749+little-portland@users.noreply.github.com>
2024-08-27 12:23:55 +0100

    Update booking-options-8.jsx

--- pages/booking-options-8.jsx
+++ pages/booking-options-8.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>THU</span>12 SEP</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>KEM, MARCELINA WICK</h3>
+              <h3><span>TENT <b>//</b> </span>KEM, MARCELINA</h3>
               <h3><span>STUDIO <b>//</b> </span>Nicky Macha & kensa</h3>
             </div>
         </div>
