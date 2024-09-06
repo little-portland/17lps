4eeab880b7e01757c2aed3d9e559672e7092916f
little-portland <110906749+little-portland@users.noreply.github.com>
2024-09-06 13:32:31 +0100

    Update booking-options-14.jsx

--- pages/booking-options-14.jsx
+++ pages/booking-options-14.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>05 Sep</h2>
+              <h2><span>THU</span>26 Sep</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span> I.LIV.EYOR, NICOLAU, WAX MATERIAL</h3>
-              <h3><span>STUDIO <b>//</b> </span>Charlie, Miller</h3>
+              <h3><span>TENT <b>//</b> </span>Maybe Laura & Remi Mazet, Frederika</h3>
+              <h3><span>STUDIO <b>//</b> </span>Sammy Dee</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-09-05" target="_blank">
+            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-09-26" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xGMYwwTu" target="_blank">
+            <a href="" target="_blank">
                 CLUB TICKETS ONLY
               </a>
             </Button>
