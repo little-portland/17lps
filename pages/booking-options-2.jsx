0a85ddb1adbcc71d351342e86fe01a87f9a94767
little-portland <110906749+little-portland@users.noreply.github.com>
2024-09-10 10:16:44 +0100

    Update booking-options-2.jsx

--- pages/booking-options-2.jsx
+++ pages/booking-options-2.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>BABA ALI, Ece Düzgit</h3>
-              <h3><span>STUDIO <b>//</b> </span>SECRET GUEST</h3>
+              <h3><span>STUDIO <b>//</b> </span>BRAWTHER</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
