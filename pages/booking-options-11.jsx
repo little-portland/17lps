ac737b74a2f778a3807314cae158689d71a98944
little-portland <110906749+little-portland@users.noreply.github.com>
2024-09-17 12:46:13 +0100

    Update booking-options-11.jsx

--- pages/booking-options-11.jsx
+++ pages/booking-options-11.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name dinner-tunes">LIVE JAZZ</h1>
+        <h1 class="event-name friday-residents">Digital Soundboy</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>SAT</span>18 SEP</h2>
