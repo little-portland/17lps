e0d0c68695ad0d0442c7647cb523651fc93fb194
little-portland <110906749+little-portland@users.noreply.github.com>
2024-09-23 11:45:21 +0100

    Update booking-options-6.jsx

--- pages/booking-options-6.jsx
+++ pages/booking-options-6.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>DEMI, MR SHIVER</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
+              <h3><span>STUDIO <b>//</b> </span>ADAM SHELTON, SUBB-AN</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
