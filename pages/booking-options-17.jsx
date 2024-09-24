075b3239247f46ca990006545f2f56a4f8c39b18
little-portland <110906749+little-portland@users.noreply.github.com>
2024-09-24 15:40:52 +0100

    Update booking-options-17.jsx

--- pages/booking-options-17.jsx
+++ pages/booking-options-17.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name disco-saturdays">WARMER PEOPLE</h1>
+        <h1 class="event-name warmer-people">WARMER PEOPLE</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>SAT</span>28 SEP</h2>
