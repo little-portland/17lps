11c2663b32c12fcb4f26fd9e18d0df693f7f0881
little-portland <110906749+little-portland@users.noreply.github.com>
2024-10-01 13:06:47 +0100

    Update booking-options-19.jsx

--- pages/booking-options-19.jsx
+++ pages/booking-options-19.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>11 Oct</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>CLOSED</h3>
+              <h3><span>TENT <b>//</b> </span>DAVIDE DEL VECCHIO</h3>
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
