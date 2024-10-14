7e08aeaab0e248158823b82bc9decdd0a000dea5
little-portland <110906749+little-portland@users.noreply.github.com>
2024-10-14 18:34:44 +0100

    Update booking-options-19.jsx

--- pages/booking-options-19.jsx
+++ pages/booking-options-19.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>FRI</span>11 Oct</h2>
+              <h2><span>FRI</span>01 Nov</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>CLOSED</h3>
-              <h3><span>STUDIO <b>//</b> </span> FUNKTONIO, OHM HOURANI</h3>
+              <h3><span>TENT <b>//</b> </span>BABA ALI, MR SHIVER</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-10-11" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-11-01" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xEA1Q6WC" target="_blank">
+              <a href="https://sevn.ly/xW82Mu7P" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
