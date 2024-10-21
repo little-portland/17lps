ccab5acd1b68cee9083596d4a4d04a2fc47d6216
little-portland <110906749+little-portland@users.noreply.github.com>
2024-10-21 12:23:32 +0100

    Update override.jsx

--- pages/override.jsx
+++ pages/override.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+ideimport React, { useEffect } from "react";
 import Head from "next/head";
 import Script from "next/script";
 
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
-            <Button classes="events-button ticketNew" btnType="hollow">
+            <Button classes="events-button ticketNew override-btn" btnType="hollow">
               <a href="https://sevn.ly/xeLHgzuf" target="_blank">
                 Gabriel Pryce (Rita’s)
                 <p class="dotted-divider first-divider"></p>
@@ -35,21 +35,21 @@ const BookingOptions = () => {
         </div>
 
         <div class="button-wrapper new-button-wrapper">
-            <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xeLHgzuf" target="_blank">
-                Gabriel Pryce (Rita’s)
+            <Button classes="events-button ticketNew override-btn" btnType="hollow">
+              <a href="https://sevn.ly/x67GScdC" target="_blank">
+                Sirichai “Siri” Kularbwong (Singburi)
                 <p class="dotted-divider first-divider"></p>
-                <span>28 November - From 7PM</span>
+                <span>11 December - From 7PM</span>
               </a>
             </Button>
         </div>
 
        <div class="button-wrapper new-button-wrapper">
-            <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xeLHgzuf" target="_blank">
-                Gabriel Pryce (Rita’s)
+            <Button classes="events-button ticketNew override-btn" btnType="hollow">
+              <a href="https://sevn.ly/xMWA7DA6" target="_blank">
+                SEB MEYERS (PLanque)
                 <p class="dotted-divider first-divider"></p>
-                <span>28 November - From 7PM</span>
+                <span>29 January - From 7PM</span>
               </a>
             </Button>
         </div>
