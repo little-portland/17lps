a4a21db7d5e9982bb96677922fc9aff4eb39f2a0
little-portland <110906749+little-portland@users.noreply.github.com>
2024-10-21 11:52:30 +0100

    Update override.jsx

--- pages/override.jsx
+++ pages/override.jsx
@@ -25,38 +25,11 @@ const BookingOptions = () => {
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
-            <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-11-02" target="_blank">
-              <span>BOOK DINNER</span>
-              <span class="space">IN THE TENT</span>
-              <p class="time">Includes Free Club Entry</p>
-             </a>
-             <p class="dotted-divider first-divider"></p>
-             <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
-              <span>[MENU]</span>
-             </a>
-            </Button>
-
-            <Button classes="events-button ticket"  btnType="hollow">
-              <a class="book-link" href="mailto:eat@little-portland.com?subject=Chef’s Studio" target="_blank">
-              <span>BOOK DINNER</span>
-              <span class="space">IN CHEF’S STUDIO</span>
-              <p class="time">Includes Free Club Entry</p>
-              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>£100PP</span></p>
-             </a>
-             <p class="dotted-divider"></p>
-             <a class="menu-btn" href="https://www.little-portland.com/regular-set-menu" target="_blank">
-              <span>[REGULAR MENU]</span>
-             </a>
-             <a class="menu-btn premium-menu-btn" href="https://www.little-portland.com/premium-set-menu" target="_blank">
-              <span>[PREMIUM MENU]</span>
-             </a>
-            </Button>
-        </div>
-        <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xl75rcNq" target="_blank">
-                CLUB GUESTLIST ONLY
+              <a href="https://sevn.ly/xeLHgzuf" target="_blank">
+                Gabriel Pryce (Rita’s)
+               <hr />
+                <span></>28 November - From 7PM</span>
               </a>
             </Button>
         </div>
