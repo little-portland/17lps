55cf2a4627506c95d323c26a8aa766f40db45db0
little-portland <110906749+little-portland@users.noreply.github.com>
2024-10-22 14:06:48 +0100

    Update booking-options-qkrs.jsx

--- pages/booking-options-qkrs.jsx
+++ pages/booking-options-qkrs.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name">Thursday Underground</h1>
+        <h1 class="event-name">All possible futures</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>THU</span>07 Nov</h2>
