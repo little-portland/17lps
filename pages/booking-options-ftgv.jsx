4599e458c09642b041bd2c4124ac2ee404ad7f6f
little-portland <110906749+little-portland@users.noreply.github.com>
2024-10-28 16:14:20 +0000

    Update booking-options-ftgv.jsx

--- pages/booking-options-ftgv.jsx
+++ pages/booking-options-ftgv.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>SAT</span>26 Oct</h2>
+              <h2><span>SAT</span>16 Nov</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>LUCA ANDREOZZI & TONY SERBAN</h3>
-              <h3><span>STUDIO <b>//</b> </span>DAVID TRIANA & RAKIM UNDER, DER & ONIRIK</h3>
+              <h3><span>TENT <b>//</b> </span>ENZO LEEP, FERDINAND MONK, MOOULIN</h3>
+              <h3><span>STUDIO <b>//</b> </span>ALINE BROOKLYN, ANGELESS, ENZO LEEP, FERDINAND MONK</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-10-26" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-11-16" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xein5Srz" target="_blank">
+              <a href="" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
