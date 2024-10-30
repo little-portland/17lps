05425ae9a7e3d5adc5d7726c82984dc58807f313
little-portland <110906749+little-portland@users.noreply.github.com>
2024-10-30 23:26:56 +0000

    Update nocturn-03.tsx

--- pages/nocturn-03.tsx
+++ pages/nocturn-03.tsx
@@ -14,6 +14,8 @@ const Menu = ({ menuImage }) => {
     <>
       <Head>
         <style>
+            {'html{overflow: auto!important;}'}
+            {'body{background-color: #FA2E2E!important;overflow: auto!important;overflow-x: initial!important;}'}
             {'.nocturn{width: 50%;margin: 0 auto;}'}
             {'.nocturn-wider-section{width: 80%;margin: 0 auto;}'}
             {'.row{ display: flex;flex-wrap: wrap;padding: 0 4px;}'}
@@ -35,7 +37,6 @@ const Menu = ({ menuImage }) => {
             {'.preloader{ margin:30px 0 10px 0!important;}'}
             {'.spacing{ margin-top:30px!important;}'}
             {'.live{ border-top: 5px solid #000;padding-top: 20px;border-bottom: 5px solid #000;margin-bottom: 40px;text-align: center;}'}
-            {'@media (max-width: 768px) { .nocturn{width: 90%;}.spacing{ margin-top:15px!important;}.preloader{ margin:15px 0 -15px 0!important;}.loading{ font-size: 35px;}.live{ border-top: 3px solid #000;padding-top: 20px;border-bottom: 3px solid #000;margin-bottom: 30px}.nocturn-wider-section{width: 90%;}.nocturn-text-wrapper{padding: 0 15px;}.top{ font-size: 20px;}.bottom{ font-size: 15px;}.nocturn-text-wrapper small{ font-size: 12px!important;margin-left: 5px!important;}.row{padding: 0!important;}.column{ flex: 50%;max-width: 50%;}}'}
         </style>
 
         <title>NOCTURN [03]: it’s your turn to be - Abbas Zahedi</title>
