58afec5aed32f6448de5538a8768bb06dfa98424
little-portland <110906749+little-portland@users.noreply.github.com>
2024-11-05 10:49:37 +0000

    Update booking-options-utfs.jsx

--- pages/booking-options-utfs.jsx
+++ pages/booking-options-utfs.jsx
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="" target="_blank">
+              <a href="https://sevn.ly/xz5xziYd" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
