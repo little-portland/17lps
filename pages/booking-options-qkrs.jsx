6399f1990adec42644b98c32b308e781f33b78c4
little-portland <110906749+little-portland@users.noreply.github.com>
2024-11-06 13:54:25 +0000

    Update booking-options-qkrs.jsx

--- pages/booking-options-qkrs.jsx
+++ pages/booking-options-qkrs.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>THU</span>07 Nov</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>BAGSY, CORNACCHIA</h3>
+              <h3><span>TENT <b>//</b> </span>BUGSY, CORNACCHIA</h3>
               <h3><span>STUDIO <b>//</b> </span>ANDREW JAMES GUSTAV, JOS, MODERNO</h3>
             </div>
         </div>
