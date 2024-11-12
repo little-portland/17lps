e295f2bff951daf55bc48161410c1ef9dbe58cd4
little-portland <110906749+little-portland@users.noreply.github.com>
2024-11-12 11:22:21 +0000

    Update booking-options-utfs.jsx

--- pages/booking-options-utfs.jsx
+++ pages/booking-options-utfs.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>22 Nov</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>MARI*, TIM LANG</h3>
+              <h3><span>TENT <b>//</b> </span>Gaby D’Annunzio, TIM LANG</h3>
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
