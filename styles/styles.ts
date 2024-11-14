313f654f54ac352ecfacd9aacc0b3c93c03ad3cc
little-portland <110906749+little-portland@users.noreply.github.com>
2024-11-14 13:32:49 +0000

    Update styles.ts

--- styles/styles.ts
+++ styles/styles.ts
@@ -813,6 +813,10 @@ html, body {
  margin-bottom: 0!important;
 }
 
+.override-box .override-dietary {
+  margin-bottom: 0!important;
+}
+
 /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
 @media screen and (max-width: 600px) {
   .column {
