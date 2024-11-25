9fea29cf328e791760800b64d11f00c4ebb5dd8c
little-portland <110906749+little-portland@users.noreply.github.com>
2024-11-25 14:45:30 +0000

    Update booking-options-tshk.jsx

--- pages/booking-options-tshk.jsx
+++ pages/booking-options-tshk.jsx
@@ -18,19 +18,19 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
+        <h1 class="event-name openlab-presents">OPENLAB PRESENTS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>SAT</span>23 Nov</h2>
+              <h2><span>SAT</span>14 Dec</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>Carly Foxx, Gaucho, GÖO!</h3>
-              <h3><span>STUDIO <b>//</b> </span>Ben Gomori, Josh Ludlow, Tigerbalm</h3>
+              <h3><span>TENT <b>//</b> </span>Alex Wolf, Ralph Lawson</h3>
+              <h3><span>STUDIO <b>//</b> </span>Antonio Prosper, Dylan Blue, TinTin</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-11-23" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-12-14" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xXZCFUfg" target="_blank">
+              <a href="" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
