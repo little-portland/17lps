8faeb1c67e958c0943597dd421853da6622ff190
little-portland <110906749+little-portland@users.noreply.github.com>
2024-11-27 17:19:36 +0000

    Update booking-options-wktr.jsx

--- pages/booking-options-wktr.jsx
+++ pages/booking-options-wktr.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Isherwood</h3>
-              <h3><span>STUDIO <b>//</b> </span>Georgi㋛ooOoOOoO, Kristina May (live) Oli Silva</h3>
+              <h3><span>STUDIO <b>//</b> </span>Georgi㋛ooOoOOoO, Kristina May (live), Oli Silva</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
