4d75047be730870ec5c7daf9ea257c9f02f59fa1
little-portland <110906749+little-portland@users.noreply.github.com>
2024-11-27 12:23:27 +0000

    Update booking-options-tshk.jsx

--- pages/booking-options-tshk.jsx
+++ pages/booking-options-tshk.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Antonio Prosper, Dylan Blue, TinTin</h3>
-              <h3><span>STUDIO <b>//</b> </span>Alex Wolf, Ralph Lawson</h3>
+              <h3><span>STUDIO <b>//</b> </span>Lex Wolf, Ralph Lawson</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
