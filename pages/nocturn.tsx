93f8e40946ff19b0c623ceef7c5ad2003d5e6d79
little-portland <110906749+little-portland@users.noreply.github.com>
2024-12-10 11:49:43 +0000

    Update nocturn.tsx

--- pages/nocturn.tsx
+++ pages/nocturn.tsx
@@ -33,7 +33,7 @@ const Menu = ({ menuImage }) => {
             {'body{background-color: #1c043d!important;overflow:hidden!important;-webkit-overflow-scrolling:touch!important;}'}
             {'.nocturn{width: 50%;margin: 0 auto 30px auto;}'}
             {'.nocturn-wider-section{width: 80%;margin: 0 auto;}'}
-            {'.nocturn-wider-section{width: 100%;margin: 0 auto;}'}
+            {'.subscribe{width: 100%;margin: 0 auto;}'}
             {'.row{ display: flex;flex-wrap: wrap;padding: 0 4px;margin-bottom: 30px;}'}
             {'.column{flex: 33%;max-width: 33%;padding: 0 4px;}'}
             {'.subscribe .column{flex: 100%;max-width: 100%;padding: 0 4px;}'}
