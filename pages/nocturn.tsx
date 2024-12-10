f87eaa19532c2f34a94ebfbe70a8699de49f06a1
little-portland <110906749+little-portland@users.noreply.github.com>
2024-12-10 15:58:40 +0000

    Update nocturn.tsx

--- pages/nocturn.tsx
+++ pages/nocturn.tsx
@@ -97,6 +97,10 @@ const Menu = ({ menuImage }) => {
                       }}
                       style={style}
                     />
+               <div className="nocturn-text-wrapper bottom"> 
+                   <p><a className="nocturn-text" href="mailto:bianca@little-portland.com" target="_blank">bianca@little-portland.com</a></p>
+                   <p><a className="nocturn-text" href="https://bianca-chu.com/" target="_blank">www.bianca-chu.com</a></p>
+                </div>
               </div>
            </div>
           </div>
