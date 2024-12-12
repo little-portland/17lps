8813a0a3011b26e3bf22817e150172c7d74339c5
little-portland <110906749+little-portland@users.noreply.github.com>
2024-12-12 17:26:34 +0000

    Update override.jsx

--- pages/override.jsx
+++ pages/override.jsx
@@ -33,15 +33,6 @@ const BookingOptions = () => {
             </div>
         </div>
 
-        <div class="button-wrapper new-button-wrapper">
-            <Button classes="events-button ticketNew override-btn" btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-12-11&default_party_size=8" target="_blank">
-                Sirichai “Siri” Kularbwong <span class="rest-name">(Singburi) </span>
-                <span class="takeover-date">11 Dec<time>From 7PM</time></span>
-              </a>
-            </Button>
-        </div>
-
        <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew override-btn" btnType="hollow">
               <a href="https://sevn.ly/xMWA7DA6" target="_blank">
