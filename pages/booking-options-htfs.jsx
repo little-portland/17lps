8b417325853bdf2e704b605768f79d2b590b9125
little-portland <110906749+little-portland@users.noreply.github.com>
2024-12-24 10:41:46 +0000

    Update booking-options-htfs.jsx

--- pages/booking-options-htfs.jsx
+++ pages/booking-options-htfs.jsx
@@ -21,16 +21,16 @@ const BookingOptions = () => {
         <h1 class="event-name">Thursday Underground</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>12 Dec</h2>
+              <h2><span>THU</span>09 Jan</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>RHYTHMS OF PRESCOTT</h3>
-              <h3><span>STUDIO <b>//</b> </span>CAP, Cem Ozden</h3>
+              <h3><span>TENT <b>//</b> </span>Annx, AOB</h3>
+              <h3><span>STUDIO <b>//</b> </span>Nirvan b2b elioss, Olga Korol</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2024-12-12" target="_blank">
+            <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2025-01-09" target="_blank">
               <span>BOOK DINNER</span>
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
@@ -59,7 +59,7 @@ const BookingOptions = () => {
         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xUBCyjuY" target="_blank">
+            <a href="" target="_blank">
                 CLUB TICKETS ONLY
               </a>
             </Button>
