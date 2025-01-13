a46fcce73155c5e6a4e5b5ff803a3b505ce1772a
little-portland <110906749+little-portland@users.noreply.github.com>
2025-01-13 17:55:59 +0000

    Update booking-options-wktr.jsx

--- pages/booking-options-wktr.jsx
+++ pages/booking-options-wktr.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>BIFA AND THE JUICE, MAYBE LAURA & REMI MAZET</h3>
-              <h3><span>STUDIO <b>//</b> </span>Eli Verveine, K-led</h3>
+              <h3><span>STUDIO <b>//</b> </span>Eli Verveine, K.LED</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
