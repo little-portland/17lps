148cb243dcf7388f791fed9fc1220c0b9cba5a37
little-portland <110906749+little-portland@users.noreply.github.com>
2025-01-21 10:07:10 +0000

    Update booking-options-erts.jsx

--- pages/booking-options-erts.jsx
+++ pages/booking-options-erts.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>07 Feb</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>NATHAN COLINET, ROBERTA CUTOLO</h3>
+              <h3><span>TENT <b>//</b> </span>Nick Williams, ROBERTA CUTOLO</h3>
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, Ohm Hourani</h3>
             </div>
         </div>
