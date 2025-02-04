6056d407f19837081ca89710340be79e40bcd3ee
little-portland <110906749+little-portland@users.noreply.github.com>
2025-02-04 15:20:20 +0000

    Update booking-options-sxct.jsx

--- pages/booking-options-sxct.jsx
+++ pages/booking-options-sxct.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Gaby d’annunzio, LABEACH</h3>
-              <h3><span>STUDIO <b>//</b> </span>Z@P, Paddy Lee</h3>
+              <h3><span>STUDIO <b>//</b> </span>Paddy Lee, Z@P</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
