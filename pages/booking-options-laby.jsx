a618dae4baaefb7688f6554b112c6355ca8892f4
little-portland <110906749+little-portland@users.noreply.github.com>
2025-02-11 10:49:43 +0000

    Update booking-options-laby.jsx

--- pages/booking-options-laby.jsx
+++ pages/booking-options-laby.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>HARRI PEPPER B2B Rosie Ama</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI & FRIENDS</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, DAVID TRIANA, ISAAC CARTER</h3>
             </div>
         </div>
         <div class="button-wrapper new-button-wrapper">
