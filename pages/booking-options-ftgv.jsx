012c28f57a75feba42b24d3e95f3833a2fa1ee3e
little-portland <110906749+little-portland@users.noreply.github.com>
2025-02-14 05:18:48 +0000

    Update booking-options-ftgv.jsx

--- pages/booking-options-ftgv.jsx
+++ pages/booking-options-ftgv.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
+        <h1 class="event-name balance">Balance</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>SAT</span>08 Mar</h2>
