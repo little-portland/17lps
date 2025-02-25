c27ffaf0bf6ceb6d147c87b3cea5cdd1bdabcec6
little-portland <110906749+little-portland@users.noreply.github.com>
2025-02-25 13:29:24 +0000

    Update booking-options-actv.jsx

--- pages/booking-options-actv.jsx
+++ pages/booking-options-actv.jsx
@@ -28,9 +28,9 @@ const BookingOptions = () => {
               <h3><span>STUDIO <b>//</b> </span>Sweely, B.Love</h3>
             </div>
         </div>
+        <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
         <div class="button-wrapper new-button-wrapper">
-            <span>BOOK OVERRIDE DINNER</span>
-            <Button classes="events-button new-events-button"  btnType="hollow">
+          <Button classes="events-button new-events-button"  btnType="hollow">
             <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2025-03-06" target="_blank">
               <span class="space">IN THE TENT</span>
               <p class="time">Includes Free Club Entry</p>
