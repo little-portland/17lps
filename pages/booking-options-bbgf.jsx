195d92d7767207c66d2917fa75c4735842c598f1
little-portland <110906749+little-portland@users.noreply.github.com>
2025-02-25 11:56:39 +0000

    Update booking-options-bbgf.jsx

--- pages/booking-options-bbgf.jsx
+++ pages/booking-options-bbgf.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>07 Mar</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>FEDERICO MARTON, CARLY FOX </h3>
+              <h3><span>TENT <b>//</b> </span>CARLY FOX, FEDERICO MARTON</h3>
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
