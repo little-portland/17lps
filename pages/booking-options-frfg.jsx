68db697f2653d48f1d161ac9a1b20f3937c7d642
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-03 16:02:54 +0000

    Update booking-options-frfg.jsx

--- pages/booking-options-frfg.jsx
+++ pages/booking-options-frfg.jsx
@@ -58,7 +58,7 @@ const BookingOptions = () => {
          </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="" target="_blank">
+            <a href="https://sevn.ly/xKMyUvKp" target="_blank">
                 CLUB TICKETS ONLY
               </a>
             </Button>
