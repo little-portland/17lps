d67fccbd05de8e5a09641ac554149693eb612712
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-04 18:56:24 +0000

    Update bookings.tsx

--- pages/bookings.tsx
+++ pages/bookings.tsx
@@ -52,34 +52,35 @@ const Bookings = () => {
           style={style}
         />
         <div className="bookings-body-text"> 
-        <h1 className="bookings-heading-0">LARGE GROUPS</h1>
+         <h1 className="bookings-heading-0">DINNER RESERVATIONS</h1>
           <ul>
-            <li>For group reservations for 10 persons or more, please email us at <a href="mailto:eat@little-portland.com">eat@little-portland.com</a>.</li>
-            <li>We can cater for groups of up to 34 in The Tent, or 12 in the Chef’s Studio.</li>
+            <li>For all dinner reservations in the Tent, we have a specially designed set menu curated by John Javier and Zen Ong. Available until the end of June. </li>
+            <li>Please note, we cannot cater to any dietary requirements for this special menu.</li>
+            <li>Cancellations within 48 hours prior to your dinner reservation or no-shows will be subject to a £30 per person cancellation fee.</li>
+          </ul>
+          
+        <h1 className="bookings-heading-1">LARGE GROUPS</h1>
+          <ul>
+            <li>For large groups of 6 or more persons, please email us at <a href="mailto:eat@little-portland.com">eat@little-portland.com</a>.</li>
             <li>Large groups can choose from 2 set menu options. <a href="https://www.little-portland.com/large-group-bookings-menu" target="_blank">Click here</a> to see sample menus.</li>
             <li>To confirm a reservation, we require a deposit equal to 50% of the set menu price, per person.</li>
-            <li>Cancellations within 10 days of your dinner reservation or no-shows will be subject to a late cancellation fee equal to the deposit paid.</li>
+            <li>Cancellations within 10 days of your dinner reservation or no-shows, will be subject to a late cancellation fee of £30 per person.</li>
             <li>Reductions in group sizes within 10 days of your reservation time will also result in late cancellation fees being charged.</li>
           </ul>
           
           <h1 className="bookings-heading-1">CHEF’S STUDIO</h1>
           <ul>
-            <li>The Chef’s Studio follows a set menu format with a choice between our <a href="https://www.little-portland.com/regular-set-menu" target="_blank">standard</a> and <a href="https://www.little-portland.com/premium-set-menu" target="_blank">premium</a> menus.</li>
-            <li>To confirm a reservation, we require a £40 per person deposit redeemable towards your final bill.</li>
-            <li>Cancellations within 10 days of your dinner reservation or no-shows will be subject to a £40 per person cancellation fee.</li>
-            <li>Reductions in group sizes within 10 days of your reservation time will also result in late cancellation fees being charged.</li>
-            <li>Reservations can be made for a minimum group size of 8 and maximum 12 between Wednesdays – Saturdays.</li>
-            <li>Reservations must be made at least 7 days in advance.</li>
-            <li>Please let us know in advance for any specific dietary requirements within the group.</li>
-            <li>For more information, to make a reservation or modify an existing one, contact <a href="mailto:eat@little-portland.com">eat@little-portland.com</a></li>
+            <li>The Chef’s Studio is a private dining space located beneath the Tent and can cater between 6 - 12 guests seated at 8pm.</li>
+            <li>To confirm a reservation, we will require a pre-authorised payment of £30 per person. This will only be chargeable in the event of a cancellation, late reductions in group size (within 7 days), or no-show on the day of the reservation.  </li>
+            <li>Reservations can be made for a minimum group size of 6 and maximum of 12 between Thursdays – Saturdays.</li>
+            <li>We unfortunately cannot cater to any dietary requirements with our special set menus. </li>
+            <li>To book or modify an existing reservation, please contact <a href="mailto:eat@little-portland.com">eat@little-portland.com</a></li>
           </ul>
 
           <h1 className="bookings-heading-1">PRIVATE HIRE + PRIVATE DINING</h1>
           <ul>
             <li>Both The Tent and Studio are available for private hire. Contact <a href="mailto:yo@little-portland.com">yo@little-portland.com</a> for more information.</li>
-            <li>Large groups dining with us, must be on a chosen set menu per person.</li>
-            <li>We also offer canapés for private events. <a href="https://www.little-portland.com/canape-menu" target="_blank">Click here</a> to see the sample canapé menu.</li>
-            <li>We are able to cater for Private Dining requests for groups of up to 12 in the Chef’s Studio (see details above).</li>
+            <li>We also offer canapés for Tent or Full venue exclusive events.</li>
           </ul>
 
          <h1 className="bookings-heading-1">CANCELLATION POLICY FOR GROUP SIZES OF LESS THAN 10</h1>
@@ -92,13 +93,12 @@ const Bookings = () => {
           
           <h1 className="bookings-heading-2">MISCELLANEOUS</h1>
           <ul>
-            <li>All menus are subject to change depending on availability.</li>
+            <li>All menus are subject to change per each override special guest chef curated menu.</li>
             <li>Anyone under the age of 18 is not permitted to enter the venue.</li>
             <li>Dogs are not permitted to enter the venue.</li>
             <li>No photos or videos are permitted to be taken inside the venue.</li>
             <li>Reservations can be made two months in advance.</li>
-            <li>All menus are subject to change depending on availability.</li>
-            <li> For any changes to reservations, or for further information, contact <a href="mailto:eat@little-portland.com">eat@little-portland.com</a>.</li>
+            <li>We have a relaxed casual dress code.</li>
           </ul>
         </div>
 
