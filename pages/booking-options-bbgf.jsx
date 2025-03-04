b917b0267ff7d634b8eabdb0648e358da747a019
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-04 09:24:46 +0000

    Update booking-options-bbgf.jsx

--- pages/booking-options-bbgf.jsx
+++ pages/booking-options-bbgf.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>CARLY FOX, FEDERICO MARTON</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, Bill Patrick</h3>
             </div>
         </div>
         <div class="book-wrapper">
