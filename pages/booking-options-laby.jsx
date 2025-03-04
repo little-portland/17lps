7013bd8088efc1d0f7a8fb427bdebb0b6efff7f6
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-04 12:52:00 +0000

    Update booking-options-laby.jsx

--- pages/booking-options-laby.jsx
+++ pages/booking-options-laby.jsx
@@ -24,8 +24,8 @@ const BookingOptions = () => {
               <h2><span>SAT</span>22 Mar</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>Wasted Space & Grace Raw Silk</h3>
-              <h3><span>STUDIO <b>//</b> </span>AEGY, DEMI REQUISMO, NIKS</h3>
+              <h3><span>TENT <b>//</b> </span>secret guest</h3>
+              <h3><span>STUDIO <b>//</b> </span>secret guest</h3>
             </div>
         </div>
        <div class="book-wrapper">
