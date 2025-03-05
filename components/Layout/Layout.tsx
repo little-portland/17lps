7a7e9bca6940bc1cbd62a5e3a4bb43a69b522245
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-05 16:53:42 +0000

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -143,9 +143,10 @@ const Layout: React.FC<IProps> = ({ main, eatItem, hireItem }) => {
         <h2>CHEF’S STUDIO</h2>
         <p>An intimate 12-seater dining experience in our invite-only club hidden beneath the Tent (at the End of the Universe). This is how those in the know kick off their night.</p> 
         <br/>  
+          <p>6-12pax</p>
           <p>£65pp</p>
-          <p>Override Menu</p>
-          <p>6-12 pax</p>
+          <p>8pm start</p>
+          <p><small></>*Unfortunately, we cannot cater to any dietary requirements.</small></p>
         <br/> 
         <p className="links"><a target="_blank" href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-03-06&default_time=21:00&default_party_size=5">[BOOK]</a> <a href="https://www.little-portland.com/override-menu" target="_blank">[MENU]</a> <a href="https://www.little-portland.com/override-03" target="_blank">[EXPLORE ZEN]</a></p>
         </div>}
