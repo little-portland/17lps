8839125f814ff2a551a517a749461d4e6614f8f7
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-06 20:41:49 +0000

    Update booking-options-sxct.jsx

--- pages/booking-options-sxct.jsx
+++ pages/booking-options-sxct.jsx
@@ -21,7 +21,7 @@ const BookingOptions = () => {
         <h1 class="event-name openlab-presents">Openlab</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>05 Apr</h2>
+              <h2><span>SAT</span>05 Apr</h2>
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Awkward Moments, TinTin</h3>
