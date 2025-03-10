3f56924b916a7402fe8e84fe4c569ed5d7fb1faa
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-10 16:53:36 +0000

    Update booking-options-actv.jsx

--- pages/booking-options-actv.jsx
+++ pages/booking-options-actv.jsx
@@ -14,25 +14,25 @@ const BookingOptions = () => {
   return (
     <>
       <Head>
-        <title>Bookings</title>
+        <title>Bookings </title>
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name focal-point-presents">FOCAL POINT</h1>
+        <h1 class="event-name override">JEZEBEL</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>06 Mar</h2>
+              <h2><span>SAT</span>26 Mar</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>Harrison Guyett, Marc Jarvin</h3>
-              <h3><span>STUDIO <b>//</b> </span>B.Love, Sweely</h3>
+              <h3><span>TENT <b>//</b> </span>CLOSED</h3>
+              <h3><span>STUDIO <b>//</b> </span>GEORGE PLANT, MLR, JAGUAR SKILLS, JUST JJ</h3>
             </div>
         </div>
-        <div class="book-wrapper">
+       <div class="book-wrapper">
           <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
           <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2025-03-06" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2025-03-26" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Free Club Entry</p>
                 <p class="details"><span>9PM</span></p>
@@ -58,8 +58,8 @@ const BookingOptions = () => {
          </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/x2ELP7Pn" target="_blank">
-                CLUB TICKETS ONLY
+              <a href="https://sevn.ly/xGVGedR6" target="_blank">
+                CLUB GUESTLIST ONLY
               </a>
             </Button>
         </div>
