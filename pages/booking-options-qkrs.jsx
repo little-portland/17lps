b7ba21590ce935e7f2ea67241cbd8d18239728f3
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-18 10:32:19 +0000

    Update booking-options-qkrs.jsx

--- pages/booking-options-qkrs.jsx
+++ pages/booking-options-qkrs.jsx
@@ -32,7 +32,7 @@ const BookingOptions = () => {
           <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
           <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2025-04-03" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-04-03&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Free Club Entry</p>
                 <p class="details"><span>9PM</span></p>
