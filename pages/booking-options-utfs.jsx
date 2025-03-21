bb15d4cd377d7070900635fa7b0beb2804303904
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-21 11:48:41 +0000

    Update booking-options-utfs.jsx

--- pages/booking-options-utfs.jsx
+++ pages/booking-options-utfs.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>DAWIDU, MARIELLA</h3>
-              <h3><span>STUDIO <b>//</b> </span>JAMIE 3:26, KENNY JONES</h3>
+              <h3><span>STUDIO <b>//</b> </span>JAMIE 3:26, Kenia</h3>
             </div>
         </div>
         <div class="book-wrapper">
