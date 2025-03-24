eb57b7699fc84e3c4e0a031ccd6e0e265665f13d
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-24 14:41:18 +0000

    Update booking-options-qkrs.jsx

--- pages/booking-options-qkrs.jsx
+++ pages/booking-options-qkrs.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>DAVID AGRELLA, James puentess</h3>
-              <h3><span>STUDIO <b>//</b> </span>B.love,Jord Kuning (live), SECRET GUEST</h3>
+              <h3><span>STUDIO <b>//</b> </span>Jord Kuning (live), SECRET GUEST, TIP TOP</h3>
             </div>
         </div>
         <div class="book-wrapper">
