f084d6c2de08d21ad8e111a04c5122a675ffcea8
little-portland <110906749+little-portland@users.noreply.github.com>
2025-03-24 12:41:30 +0000

    Update booking-options-frfg.jsx

--- pages/booking-options-frfg.jsx
+++ pages/booking-options-frfg.jsx
@@ -18,21 +18,21 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name focal-point-presents">LOCAL MOVES</h1>
+        <h1 class="event-name focal-point-presents">Pablo Roots</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>20 Mar</h2>
+              <h2><span>THU</span>26 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>Gabriel Rai, ROLO</h3>
-              <h3><span>STUDIO <b>//</b> </span>Stckman, Ste Robert’s, Yuta suzuki</h3>
+              <h3><span>TENT <b>//</b> </span>DER & NESTA</h3>
+              <h3><span>STUDIO <b>//</b> </span>AGELESS, NATALIA ROTH, NESTA, TIBI DABO</h3>
             </div>
         </div>
         <div class="book-wrapper">
           <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
           <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-03-20&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-04-26&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Free Club Entry</p>
                 <p class="details"><span>9PM</span></p>
@@ -58,7 +58,7 @@ const BookingOptions = () => {
          </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xKMyUvKp" target="_blank">
+            <a href="https://sevn.ly/xpzlNqZ9" target="_blank">
                 CLUB TICKETS ONLY
               </a>
             </Button>
