1d4474842ea1dd0ed8e39e5bc99b12baf133f1de
little-portland <110906749+little-portland@users.noreply.github.com>
2025-04-01 11:22:54 +0100

    Update booking-options-clnh.jsx

--- pages/booking-options-clnh.jsx
+++ pages/booking-options-clnh.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name">Thursday Underground</h1>
+        <h1 class="event-name focal-point-presents">MOZE</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>THU</span>10 Apr</h2>
