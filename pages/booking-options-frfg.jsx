c3a0428b163d9a28e6b9c76700bfe7f8105dd5cf
little-portland <110906749+little-portland@users.noreply.github.com>
2025-04-08 11:57:12 +0100

    Update booking-options-frfg.jsx

--- pages/booking-options-frfg.jsx
+++ pages/booking-options-frfg.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name focal-point-presents">Pablo Roots</h1>
+        <h1 class="event-name balance">Roots</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>THU</span>26 Apr</h2>
