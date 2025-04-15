35ee1c8d098dfb3295b6577a5dd6a24e9cdf5e67
little-portland <110906749+little-portland@users.noreply.github.com>
2025-04-15 12:37:30 +0100

    Update booking-options-laby.jsx

--- pages/booking-options-laby.jsx
+++ pages/booking-options-laby.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
+        <h1 class="event-name balance">UTOPIA</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>SAT</span>03 May</h2>
