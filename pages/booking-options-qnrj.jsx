9a84173dc111116d0fafd42b8de97b777501fee3
little-portland <110906749+little-portland@users.noreply.github.com>
2025-04-17 08:50:42 +0100

    Update booking-options-qnrj.jsx

--- pages/booking-options-qnrj.jsx
+++ pages/booking-options-qnrj.jsx
@@ -18,21 +18,21 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
+        <h1 class="event-name halucinate">ALIEN COM</h1>
         <div class="event-info">
-           <div class="info date">
-              <h2><span>FRI</span>21 Mar</h2>
+            <div class="info date">
+              <h2><span>THU</span>22 MAY</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>MR SHIVER, Tim Lang</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
+              <h3><span>TENT <b>//</b> </span>LUTHER VINE, Trixie</h3>
+              <h3><span>STUDIO <b>//</b> </span>RADIOACTIVE MAN B2B STE ROBERTS, ALIEN COMMUNICATIONS B2B DOMENIC CAPPELLO</h3>
             </div>
         </div>
-        <div class="book-wrapper">
+       <div class="book-wrapper">
           <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
           <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-03-21&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-05-22&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Free Club Entry</p>
                 <p class="details"><span>9PM</span></p>
@@ -58,7 +58,7 @@ const BookingOptions = () => {
          </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xkfKPpv3" target="_blank">
+              <a href="https://sevn.ly/xSjT4IVn" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
