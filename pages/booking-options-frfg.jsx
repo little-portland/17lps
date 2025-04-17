3e1282ab8dd84755f22d66a3970216abb3b524ba
little-portland <110906749+little-portland@users.noreply.github.com>
2025-04-17 08:28:42 +0100

    Update booking-options-frfg.jsx

--- pages/booking-options-frfg.jsx
+++ pages/booking-options-frfg.jsx
@@ -21,7 +21,7 @@ const BookingOptions = () => {
         <h1 class="event-name balance">Roots</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>THU</span>26 Apr</h2>
+              <h2><span>SAT</span>26 Apr</h2>
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>DER & NESTA, SEMOA</h3>
