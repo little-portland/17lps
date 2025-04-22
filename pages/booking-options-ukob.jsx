170ea480124fcc5a6287155c4b1c095d78ce4bcc
little-portland <110906749+little-portland@users.noreply.github.com>
2025-04-22 11:18:03 +0100

    Update booking-options-ukob.jsx

--- pages/booking-options-ukob.jsx
+++ pages/booking-options-ukob.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>MR SHIVER, THOM PARRIS</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, Mr Joshooa</h3>
             </div>
         </div>
         <div class="book-wrapper">
