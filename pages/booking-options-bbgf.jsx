be52193259461ae4467c2606c242c1a5cffa470f
little-portland <110906749+little-portland@users.noreply.github.com>
2025-04-22 15:08:41 +0100

    Update booking-options-bbgf.jsx

--- pages/booking-options-bbgf.jsx
+++ pages/booking-options-bbgf.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name openlab-presents">Openlab</h1>
+        <h1 class="event-name openlab-presents">20/20 vision</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>SAT</span>10 May</h2>
