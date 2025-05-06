f69fadfc71f4ea3266e1fa35c6765b26121fdaa2
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-06 12:02:30 +0100

    Update booking-options-qnrj.jsx

--- pages/booking-options-qnrj.jsx
+++ pages/booking-options-qnrj.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name halucinate">ALIEN COM</h1>
+        <h1 class="event-name">Alien Communications</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>THU</span>22 MAY</h2>
