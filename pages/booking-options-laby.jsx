c3ddcb322cccc731ae273780d1dfe38dee96e85c
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-07 08:04:54 +0100

    Update booking-options-laby.jsx

--- pages/booking-options-laby.jsx
+++ pages/booking-options-laby.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>DAISYBELLE</h3>
-              <h3><span>STUDIO <b>//</b> </span>BEN GOMORI, GHOSTEN, </br>X-PRESS 2</h3>
+              <h3><span>STUDIO <b>//</b> </span>BEN GOMORI, GHOSTEN, X-PRESS 2</h3>
             </div>
         </div>
        <div class="book-wrapper">
