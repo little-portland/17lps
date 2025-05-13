1b3e877f9543c054649af3dd4d58f15e9e1ac718
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-13 19:25:59 +0100

    Update booking-options-frfg.jsx

--- pages/booking-options-frfg.jsx
+++ pages/booking-options-frfg.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name">Logistic Records</h1>
+        <h1 class="event-name openlab-presents">Logistic Records</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>THU</span>26 Jun</h2>
