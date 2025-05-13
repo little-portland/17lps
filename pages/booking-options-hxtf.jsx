26da76c9974e1dd7f8d108f8c0c67e49c9182e7a
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-13 10:45:17 +0100

    Update booking-options-hxtf.jsx

--- pages/booking-options-hxtf.jsx
+++ pages/booking-options-hxtf.jsx
@@ -21,18 +21,18 @@ const BookingOptions = () => {
         <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>FRI</span>09 May</h2>
+              <h2><span>FRI</span>30 May</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>FREDERIKA, JOS</h3>
-              <h3><span>STUDIO <b>//</b> </span>Harri Pepper</h3>
+              <h3><span>TENT <b>//</b> </span>RHYTHMS OF PRESCOTT, TIM LANG</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, NAGUAL</h3>
             </div>
         </div>
         <div class="book-wrapper">
           <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
           <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-05-09&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-05-30&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Free Club Entry</p>
                 <p class="details"><span>9PM</span></p>
@@ -58,7 +58,7 @@ const BookingOptions = () => {
          </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xOc8RpQo" target="_blank">
+              <a href="" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
