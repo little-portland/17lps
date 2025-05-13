96ac780186e83d8dfbaf7624979b90d7d4404c5f
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-13 11:01:49 +0100

    Update booking-options-actv.jsx

--- pages/booking-options-actv.jsx
+++ pages/booking-options-actv.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name">THURSDAY UNDERGROUND</h1>
+        <h1 class="event-name openlab-presents">KISMET</h1>
         <div class="event-info">
             <div class="info date">
               <h2><span>THU</span>29 May</h2>
