8a1f1939618a7537dacc77eb3257f1192b96677f
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-19 16:53:46 +0100

    Update booking-options-onth.jsx

--- pages/booking-options-onth.jsx
+++ pages/booking-options-onth.jsx
@@ -18,21 +18,21 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name disco-saturdays">Disco Saturdays</h1>
+        <h1 class="event-name disco3000">Disco3000</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>SAT</span>19 Apr</h2>
+              <h2><span>SAT</span>07 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>GABY D'ANNUNZIO, Harri Pepper</h3>
-              <h3><span>STUDIO <b>//</b> </span>DUKE BROOKS, Grace Sands, LUKE SOLOMON</h3>
+              <h3><span>TENT <b>//</b> </span>11AI, MOULIN</h3>
+              <h3><span>STUDIO <b>//</b> </span>MAXIMEB DB, DAVID TRIANA, RAKIM UNDER</h3>
             </div>
         </div>
         <div class="book-wrapper">
           <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
           <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-04-19&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-06-07&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Free Club Entry</p>
                 <p class="details"><span>9PM</span></p>
@@ -58,7 +58,7 @@ const BookingOptions = () => {
          </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/x16L6fzn" target="_blank">
+            <a href="" target="_blank">
                 CLUB TICKETS ONLY
               </a>
             </Button>
