e76d63d7f075bdc0d0a908831a4b58e5ae7573ac
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-19 16:26:41 +0100

    Update booking-options-utfs.jsx

--- pages/booking-options-utfs.jsx
+++ pages/booking-options-utfs.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>24 May</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>DEMI, LEWIS CAROL</h3>
+              <h3><span>TENT <b>//</b> </span>DEMI, LEWIS CARROLL</h3>
               <h3><span>STUDIO <b>//</b> </span>JACOB HUSLEY, JIVE TALK</h3>
             </div>
         </div>
