12cced28a3f45ab5798e762b866066e71f763877
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-21 12:14:45 +0100

    Update booking-options-erts.jsx

--- pages/booking-options-erts.jsx
+++ pages/booking-options-erts.jsx
@@ -25,7 +25,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>MARCELINA, Robi</h3>
-              <h3><span>STUDIO <b>//</b> </span>FUNKTONIO B2B RHYTHMS OF PRESCOTT</h3>
+              <h3><span>STUDIO <b>//</b> </span>FUNKTONIO & RHYTHMS OF PRESCOTT</h3>
             </div>
         </div>
         <div class="book-wrapper">
