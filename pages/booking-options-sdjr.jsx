ffe41d227c0f584bb44b8ff262b435e8e4e80685
little-portland <110906749+little-portland@users.noreply.github.com>
2025-05-23 14:56:18 +0100

    Update booking-options-sdjr.jsx

--- pages/booking-options-sdjr.jsx
+++ pages/booking-options-sdjr.jsx
@@ -18,48 +18,47 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
+        <h1 class="event-name friday-residents">Friday Residentes</h1>
         <div class="event-info">
-           <div class="info date">
-              <h2><span>FRI</span>17 Jan</h2>
+            <div class="info date">
+              <h2><span>FRI</span>27 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>D'MONK, FRANKIE MAMI</h3>
-              <h3><span>STUDIO <b>//</b> </span>LABEACH, RHYTHMS OF PRESCOTT, Rubén Baiz</h3>
+              <h3><span>TENT <b>//</b> </span>MARCELINA, Soho</h3>
+              <h3><span>STUDIO <b>//</b> </span>ERIC DUNCAN</h3>
             </div>
         </div>
-        <div class="button-wrapper new-button-wrapper">
+       <div class="book-wrapper">
+          <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
+          <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa&default_date=2025-01-17" target="_blank">
-              <span>BOOK DINNER</span>
-              <span class="space">IN THE TENT</span>
-              <p class="time">Includes Free Club Entry</p>
-             </a>
-             <p class="dotted-divider first-divider"></p>
-             <a class="menu-btn" href="https://www.little-portland.com/menu" target="_blank">
-              <span>[MENU]</span>
-             </a>
-            </Button>
-
-            <Button classes="events-button ticket"  btnType="hollow">
-              <a class="book-link" href="mailto:eat@little-portland.com?subject=Chef’s Studio" target="_blank">
-              <span>BOOK DINNER</span>
-              <span class="space">IN CHEF’S STUDIO</span>
-              <p class="time">Includes Free Club Entry</p>
-              <p class="details"><span>8-12 PAX</span><span>8PM</span><span>FROM £80PP</span></p>
-             </a>
-             <p class="dotted-divider"></p>
-             <a class="menu-btn" href="https://www.little-portland.com/regular-set-menu" target="_blank">
-              <span>[REGULAR MENU]</span>
-             </a>
-             <a class="menu-btn premium-menu-btn" href="https://www.little-portland.com/premium-set-menu" target="_blank">
-              <span>[PREMIUM MENU]</span>
-             </a>
-            </Button>
-        </div>
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-06-27&default_time=21:00&default_party_size=4" target="_blank">
+                <span class="space">IN THE TENT</span>
+                <p class="time">Includes Free Club Entry</p>
+                <p class="details"><span>9PM</span></p>
+               </a>
+              </Button>
+  
+              <Button classes="events-button ticket"  btnType="hollow">
+                <a class="book-link" href="mailto:eat@little-portland.com?subject=Chef’s Studio" target="_blank">
+                <span class="space">CHEF’S STUDIO</span>
+                <p class="time">Includes Free Club Entry</p>
+                <p class="details chef-studio-details"><span>6-12 PAX</span><span>8PM</span></p>
+               </a>
+              </Button>
+          </div>
+          <div class="button-wrapper new-button-wrapper button-wrapper-bottom">
+            <a class="menu-btn" href="https://www.little-portland.com/override-menu" target="_blank">
+              <span>[OVERRIDE MENU]</span>
+            </a>
+            <a class="menu-btn" href="https://www.little-portland.com/override" target="_blank">
+                <span>[explore override]</span>
+              </a>
+           </div>
+         </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/xIr1nMiK" target="_blank">
+              <a href="https://sevn.ly/xHzefv0a" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
