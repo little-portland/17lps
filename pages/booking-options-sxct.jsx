c53ad83560018ca31d5eddf69a6d1d5fddd3c062
little-portland <110906749+little-portland@users.noreply.github.com>
2025-06-18 11:28:43 +0100

    Update booking-options-sxct.jsx

--- pages/booking-options-sxct.jsx
+++ pages/booking-options-sxct.jsx
@@ -24,7 +24,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>28 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>alia indigo, Gerry Attoe</h3>
+              <h3><span>TENT <b>//</b> </span>alia indigo, Gerrardo</h3>
               <h3><span>STUDIO <b>//</b> </span>JACOB HUSLEY, SEB ZITO</h3>
             </div>
         </div>
