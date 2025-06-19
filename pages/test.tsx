22d2f4b343f2f37c00c8b81c3b7b80ecb8ae7c28
little-portland <110906749+little-portland@users.noreply.github.com>
2025-06-19 11:17:19 +0100

    Update test.tsx

--- pages/test.tsx
+++ pages/test.tsx
@@ -84,8 +84,18 @@ export default function LayoutTestPage() {
             <p>Access to the club is for Friends of the Club only.</p> 
             <p>To apply, email us.</p>
             <p>Or you can access the club by booking a dinner table.</p>
-            <h3>Thursday <span>is</span> Underground</h3>
-            <p><span>Thursday Underground</span> </p>
+            <div className="category">
+              <h3>Thursday <span>is</span> Underground</h3>
+              <p><span>Thursday Underground</span> kicks off the weekend, showcasing cutting-edge electronic artists at the forefront of the underground scene, bringing together a community deeply rooted in its culture.</p>
+            </div>
+            <div className="category">
+              <h3>Friday <span>is</span> Residents</h3>
+              <p><span>Friday Residents</span> bridges Underground and Disco3000, shaping the weekend’s rhythm and flow. Focused on club residents, it brings a sense of familiarity and community—the heartbeat of the weekend.</p>
+            </div>
+            <div className="category">
+              <h3>Saturday <span>is</span> Disco3000</h3>
+              <p><span>Disco3000</span> captures the evolving spirit of the disco era. Embracing a soundscape that truly resonates with the soul, it delivers a timeless and uplifting journey—a cosmic finale to the weekend.</p>
+            </div>
           </div>
         </div>
       </Modal>
