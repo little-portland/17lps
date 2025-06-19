d25caf67fb3b1b18ea1bd15eed1a6533ac237bff
little-portland <110906749+little-portland@users.noreply.github.com>
2025-06-19 10:41:02 +0100

    Update booking-options-htfs.jsx

--- pages/booking-options-htfs.jsx
+++ pages/booking-options-htfs.jsx
@@ -18,7 +18,7 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name override">Balearic</h1>
+        <h1 class="event-name disco-saturdays">Balearic London</h1>
         <div class="event-info">
            <div class="info date">
               <h2><span>SAT</span>19 Jul</h2>
