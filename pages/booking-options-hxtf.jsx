ca67c498aa6c2b39a63d78b1cb128c018dcd4166
little-portland <110906749+little-portland@users.noreply.github.com>
2025-06-23 15:31:08 +0100

    Update booking-options-hxtf.jsx

--- pages/booking-options-hxtf.jsx
+++ pages/booking-options-hxtf.jsx
@@ -18,21 +18,21 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 class="event-name friday-residents">FRIDAY RESIDENTS</h1>
+        <h1 class="event-name balance">Balance</h1>
         <div class="event-info">
             <div class="info date">
-              <h2><span>FRI</span>30 May</h2>
+              <h2><span>SAT</span>12 Jul</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>RHYTHMS OF PRESCOTT, TIM LANG</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, NAGUAL</h3>
+              <h3><span>TENT <b>//</b> </span>Hannah Lamb, Jacob trip</h3>
+              <h3><span>STUDIO <b>//</b> </span>MATTHEW NEWQUAY, SEB ODYSSEY</h3>
             </div>
         </div>
         <div class="book-wrapper">
           <div class="override-heading"><h2>BOOK OVERRIDE DINNER</h2></div>
           <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-05-30&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-07-12&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Free Club Entry</p>
                 <p class="details"><span>9PM</span></p>
@@ -49,7 +49,7 @@ const BookingOptions = () => {
           </div>
           <div class="button-wrapper new-button-wrapper button-wrapper-bottom">
             <a class="menu-btn" href="https://www.little-portland.com/override-menu" target="_blank">
-              <span>[OVERRIDE MENU]</span>
+              <span>[Futurist Asian Menu]</span>
             </a>
             <a class="menu-btn" href="https://www.little-portland.com/override" target="_blank">
                 <span>[explore override]</span>
@@ -58,7 +58,7 @@ const BookingOptions = () => {
          </div>
         <div class="button-wrapper new-button-wrapper">
             <Button classes="events-button ticketNew" btnType="hollow">
-              <a href="https://sevn.ly/x7pZIZjq" target="_blank">
+              <a href="https://sevn.ly/xA8mp7P6" target="_blank">
                 CLUB GUESTLIST ONLY
               </a>
             </Button>
