6c7ceff2f42fa1c3c3040ddd3bcd8a68879b8c32
little-portland <110906749+little-portland@users.noreply.github.com>
2025-07-08 14:04:01 +0100

    Update styles.ts

--- styles/styles.ts
+++ styles/styles.ts
@@ -1990,7 +1990,7 @@ html, body {
 
     .or {
         font-style: italic !important;
-        margin: 20px 0px 15px !important;
+        margin: 10px 0px -8px !important;
         text-align: center;
         color: var(--accent-color);
         font-size: 2rem;
