84118b5e8cd5d3a2ef347f8a9213f339bcc617d8
little-portland <110906749+little-portland@users.noreply.github.com>
2025-07-15 13:48:47 +0100

    Update booking-options-clnh.jsx

--- pages/booking-options-clnh.jsx
+++ pages/booking-options-clnh.jsx
@@ -40,7 +40,7 @@ const BookingOptions = () => {
 
         <div className="button-wrapper new-button-wrapper mobile-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="" target="_blank">
+            <a href="https://sevn.ly/xpG0tNhp" target="_blank">
               CLUB ENTRY ONLY [10PM]
             </a>
           </Button>
@@ -124,7 +124,7 @@ const BookingOptions = () => {
         </div>
         <div className="button-wrapper new-button-wrapper desktop-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="" target="_blank">
+            <a href="https://sevn.ly/xpG0tNhp" target="_blank">
               CLUB ENTRY ONLY [10PM]
             </a>
           </Button>
