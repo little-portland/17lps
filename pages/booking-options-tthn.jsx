91c3e78c3025f6bdf27adfe898ee125f9db585ed
little-portland <110906749+little-portland@users.noreply.github.com>
2025-07-15 16:48:59 +0100

    Update booking-options-tthn.jsx

--- pages/booking-options-tthn.jsx
+++ pages/booking-options-tthn.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Harry Flowers, KC HAPPY, Robert Outch</h3>
-              <h3><span>STUDIO <b>//</b> </span>ALEX FORM UTOPIA, TECH SUPPORT</h3>
+              <h3><span>STUDIO <b>//</b> </span>ALEX From UTOPIA, TECH SUPPORT</h3>
             </div>
         </div>
 
