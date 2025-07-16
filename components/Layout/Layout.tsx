d6367009b01feb18b872b44f5d6bb7c8b2b1247f
little-portland <110906749+little-portland@users.noreply.github.com>
2025-07-16 13:39:07 +0100

    Update Layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -76,14 +76,33 @@ const Layout: React.FC<IProps> = ({ main, eatItem, hireItem, hideNav = false }) 
 
       <MainStyle>{main}</MainStyle>
       <Modal open={displayLineup} close={closeLineup}>
-        <Image
-          src={dancePic} 
-          alt="Dance"
-          // width={500} automatically provided
-          // height={500} automatically provided
-          // blurDataURL="data:..." automatically provided
-          // placeholder="blur" // Optional blur-up while loading
-        />
+        <div className="eat-note dance-popup">
+          <div>
+          <div className="cat-wrapper">
+            <div class="cat-scroll">
+              <h1 className="dance-title">THE CLUB</h1>
+              <h2>Open Thursday to Saturday</h2>
+              <h3 className="open-time">FROM 10PM</h3>
+              <p>Access to the club is for <strong>Friends of the Club</strong> only.</p> 
+              <p>To apply to become a <strong>Friend of the Club</strong>, <a href="mailto:yo@little-portland.com?subject=FOC%20Enquiry">email us</a>.</p>
+              <p>Or you can access the club by <a href="https://www.little-portland.com/bookings" target="_blank">booking</a> a dinner table.</p>
+               <img src="/images/dance-popup-img.jpg" />
+                  <div className="category thu">
+                    <h3>Thursday <span className="italic-word">is</span> <span className="group-item">Underground</span></h3>
+                    <p><span className="group-item">Thursday Underground</span> kicks off the weekend, showcasing cutting-edge electronic artists at the forefront of the underground scene, bringing together a community deeply rooted in its culture.</p>
+                  </div>
+                  <div className="category fri">
+                    <h3>Friday <span className="italic-word">is</span> <span className="group-item">Residents</span></h3>
+                    <p><span className="group-item">Friday Residents</span> bridges Underground and Disco3000, shaping the weekend’s rhythm and flow. Focused on club residents, it brings a sense of familiarity and community. The heartbeat of the weekend.</p>
+                  </div>
+                  <div className="category sat">
+                    <h3>Saturday <span className="italic-word">is</span> <span className="group-item">Disco3000</span></h3>
+                    <p><span className="group-item">Disco3000</span> captures the evolving spirit of the disco era. Embracing a soundscape that truly resonates with the soul, it delivers a timeless and uplifting journey. A cosmic finale to the weekend.</p>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Modal>
       <Modal
         open={displayMenu}
