ceb0737e0b930e0f8af449cc7d71cda9c5c943cc
little-portland <110906749+little-portland@users.noreply.github.com>
2025-07-17 15:04:18 +0100

    Update override.jsx

--- pages/override.jsx
+++ pages/override.jsx
@@ -111,7 +111,7 @@ const Menu = ({ menuImage }) => {
 
            <img className="override-quote override-space-bottom" src="/images/override/myth-slides.gif" alt="Override" width="100%" />
            <img className="override-quote override-space-bottom" src="/images/override/slide07.png" alt="Override" width="100%" />
-            <img className="override-quote override-space-bottom" src="/images/override/slide09.png" alt="Override" width="100%" />
+            <img className="override-quote override-space-top-bottom" src="/images/override/slide09.png" alt="Override" width="100%" />
             <img className="override-quote override-space-bottom" src="/images/override/slide10.png" alt="Override" width="100%" />
          </div>
 
