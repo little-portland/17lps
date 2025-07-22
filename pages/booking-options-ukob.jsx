05f0747cd329abf96edf4cf6cf165f03b5d99e28
little-portland <110906749+little-portland@users.noreply.github.com>
2025-07-22 10:45:02 +0100

    Update booking-options-ukob.jsx

--- pages/booking-options-ukob.jsx
+++ pages/booking-options-ukob.jsx
@@ -33,7 +33,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>02 Aug</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>Richard Marshall, TinTin</h3>
+              <h3><span>TENT <b>//</b> </span>RALPH LAWSON, RICHARD MARSHALL, TINTIN </h3>
               <h3><span>STUDIO <b>//</b> </span>Abby Nurock, Esa, Ivan Turanjanin</h3>
             </div>
         </div>
