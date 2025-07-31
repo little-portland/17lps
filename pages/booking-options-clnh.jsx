c8f5fac8b64c1b97d05636ed36d43332bc8415b2
little-portland <110906749+little-portland@users.noreply.github.com>
2025-07-31 17:19:41 +0100

    Update booking-options-clnh.jsx

--- pages/booking-options-clnh.jsx
+++ pages/booking-options-clnh.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>DENNY KEM, TiM Lang</h3>
-              <h3><span>STUDIO <b>//</b> </span>Isaac Carter</h3>
+              <h3><span>STUDIO <b>//</b> </span>Isaac Carter, Seyer</h3>
             </div>
         </div>
 
