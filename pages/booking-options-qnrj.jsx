5b51b82c5e73b544aeeefe4949bffa65195aa88c
little-portland <110906749+little-portland@users.noreply.github.com>
2025-07-31 17:19:06 +0100

    Update booking-options-qnrj.jsx

--- pages/booking-options-qnrj.jsx
+++ pages/booking-options-qnrj.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>JOS, Theo G</h3>
-              <h3><span>STUDIO <b>//</b> </span>THOMAS MELCHIOR</h3>
+              <h3><span>STUDIO <b>//</b> </span>THOMAS MELCHIOR, Trixie</h3>
             </div>
         </div>
 
