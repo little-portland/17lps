61a2c6c76179de10c0556bfb20a31d7dc078d30b
little-portland <110906749+little-portland@users.noreply.github.com>
2025-08-13 15:21:48 +0100

    Update theclub.jsx

--- pages/theclub.jsx
+++ pages/theclub.jsx
@@ -68,7 +68,7 @@ const Menu = ({ menuImage }) => {
             {`.top{ font-size: 35px;}`}
             {`.bottom{ font-size: 25px;}`}
             {'.image-row{ display: flex;flex-wrap: wrap;padding: 0 4px;}'}
-            {'.image-column{flex: 50%;max-width: 50%;padding: 0 4px;}'}
+            {'.image-column{flex: 33%;max-width: 50%;padding: 0 4px;}'}
             {'.image-column img{ margin-top: 8px;vertical-align: middle;width: 100%;}'}
             {`audio{ margin-top: 8px!important;width:100%!important;}`}
             {`.nocturn-text-wrapper{ margin-top: 30px!important;padding: 0 30px;}`}
