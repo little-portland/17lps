3722e0f7038a5c4e5d3c923d948dc8782be4b5ae
little-portland <110906749+little-portland@users.noreply.github.com>
2025-08-26 12:01:51 +0100

    Update booking-options-tthn.jsx

--- pages/booking-options-tthn.jsx
+++ pages/booking-options-tthn.jsx
@@ -33,7 +33,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>13 Sep</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>In Good Company</h3>
+              <h3><span>TENT <b>//</b> </span>In Good Company </h3>
               <h3><span>STUDIO <b>//</b> </span>In Good Company</h3>
             </div>
         </div>
