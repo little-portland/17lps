f099f7ff7d01e85673358a50b4ed2d87772126eb
little-portland <110906749+little-portland@users.noreply.github.com>
2025-09-05 14:31:55 +0100

    Update thetent.jsx

--- pages/thetent.jsx
+++ pages/thetent.jsx
@@ -117,7 +117,7 @@ const Menu = ({ menuImage }) => {
             <ManualSlideshow />
           </div>
 
-          <div className="nocturn override">
+          <div className="nocturn override override-logo">
             <img className="override-quote" src="/images/override/slide07.png" alt="Override" width="100%" />
             <img className="override-quote override-space-top-bottom" src="/images/override/slide09.png" alt="Override" width="100%" />
             <img className="override-quote override-space-top-bottom" src="/images/override/bits-slides.gif" alt="Override" width="100%" />
