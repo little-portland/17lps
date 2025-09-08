bd7bbb93242194e5ec8df0cf2d44d747bc932620
little-portland <110906749+little-portland@users.noreply.github.com>
2025-09-09 00:46:50 +0100

    Update thetent.jsx

--- pages/thetent.jsx
+++ pages/thetent.jsx
@@ -71,12 +71,13 @@ const Menu = ({ menuImage }) => {
                 margin: 0 20px;
                 z-index: 1;
             }
+
             .events-box .content-btn {
-                position: relative;
-                padding: 0 0 20px 0;
-                margin: 0 20px;
-                z-index: 1;
-            }
+              position: relative;
+              padding: 0 0 20px 0;
+              margin: 10px 0;
+              z-index: 1;
+          }
             .events-box .content-divider {
                 border-bottom: 1px solid #000;
             }
@@ -149,6 +150,7 @@ const Menu = ({ menuImage }) => {
             {`.spacing{ margin-top:30px!important;}`}
             {`.slideshow-wrapper{ max-width: 40%; margin: 0 auto;}`}
             {`.slideshow-wrapper{   object-fit: cover; height: 100%;}`}
+             {`.events-box{font-family: Helvetica !important;}`}
             {`.live{ border-top: 5px solid #000;padding-top: 20px;border-bottom: 5px solid #000;margin-bottom: 40px;text-align: center;}`}
             {`@media (max-width: 768px) { .override-buttons-wrapper {flex-direction: column;gap: 15px;}.slideshow-wrapper{ max-width: 100%;margin: 0 20px;}.override-buttons-wrapper .row {flex-direction: column;gap: 15px;}.nocturn{margin-top:30px;width: 90%;}.spacing{ margin-top:15px!important;}.preloader{ margin:15px 0 -15px 0!important;}.nocturn-text-two{ padding-top: 10px;}.nocturn-wider-section{width: 90%;}.nocturn-text-wrapper{padding: 0 15px;}.top p{ font-size: 20px;}.bottom p{ font-size: 15px;}.nocturn-text-wrapper small{ font-size: 12px!important;margin-left: 5px!important;}
               .explore-zen li a {
