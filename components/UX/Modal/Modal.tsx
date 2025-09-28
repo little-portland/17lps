08afa3357de955562d074c12279cbbc2b7602e75
little-portland <110906749+little-portland@users.noreply.github.com>
2025-09-28 22:52:56 +0100

    Update Modal.tsx

--- components/UX/Modal/Modal.tsx
+++ components/UX/Modal/Modal.tsx
@@ -1,42 +1,4 @@
-import { FC, useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-
-import {
-  Middle,
-  BG,
-  Top,
-  Grid,
-  ButtonWrapper,
-  FirstButtonWrapper,
-} from "./styles";
-import Closer from "./components/CloseIcon";
-import Button from "../Button/Button";
-
-//hooks
-import useDeviceDetect from "@utils/useDeviceDetect";
-
-interface SidebarProps {
-  children: any;
-  open: boolean;
-  close: () => void;
-  email?: string;
-  phone?: string;
-  link?: {
-    target: string;
-    title: string;
-  };
-  link2?: {
-    target: string;
-    title: string;
-  };
-  link3?: {
-    target: string;
-    title: string;
-  };
-  className?: string; 
-}
+// ...unchanged imports
 
 const Modal: FC<SidebarProps> = ({
   children,
@@ -47,114 +9,102 @@ const Modal: FC<SidebarProps> = ({
   link,
   link2,
   link3,
-  className, 
+  className,
 }) => {
-  //Check Device
   const { isMobile } = useDeviceDetect();
   const [copied, setCopied] = useState(false);
+  const closeHandler = () => { close(); setCopied(false); };
 
-  const clickHandler = ({ target: { innerHTML } }) => {
-    console.log(`Clicked on "${innerHTML}"!`); // eslint-disable-line
-  };
-
-  const closeHandler = () => {
-    close();
-    setCopied(false);
-  };
-  
   const eatEmail = "eat@little-portland.com";
 
   return (
     <>
       {open && (
         <>
-          <Grid className={className}> 
+          <Grid className={className}>
             <div />
             <Middle>
               <div style={{ position: "relative" }}>
-                <Top onClick={closeHandler}>
-                  <Closer />
-                </Top>
+                <Top onClick={closeHandler}><Closer /></Top>
                 <div style={{ maxWidth: "80vw" }}>{children}</div>
               </div>
             </Middle>
 
-            <ButtonWrapper style={link && link2 && link3 && { flexWrap: "wrap" }}>
+            {/* Make the buttons area a 2-col grid */}
+            <ButtonWrapper
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(2, 1fr)",
+                gap: "12px",
+              }}
+            >
               {link && (
                 <FirstButtonWrapper>
-                 <div className="sample-menu btn-wrapper" >
-                  <Link href={link.target}>
-                    <a>
-                      <Button btnType="solid">{link.title}</Button>
-                    </a>
-                  </Link>
-                 </div>
+                  <div className="sample-menu btn-wrapper">
+                    <Link href={link.target}>
+                      <a><Button btnType="solid">{link.title}</Button></a>
+                    </Link>
+                  </div>
                 </FirstButtonWrapper>
-              )}              
+              )}
 
               {link2 && (
                 <FirstButtonWrapper>
-                  <div className="sample-menu btn-wrapper" >
-                  <Link href={link2.target}>
-                    <a>
-                      <Button btnType="solid">{link2.title}</Button>
+                  <div className="sample-menu btn-wrapper">
+                    <Link href={link2.target}>
+                      <a><Button btnType="solid">{link2.title}</Button></a>
+                    </Link>
+                    <a
+                      className="insta"
+                      href="https://www.instagram.com/thetentattheendoftheuniverse/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Image src={"/images/instagram.svg"} width={25} height={25} />
                     </a>
-                  </Link>
-                  <a className="insta" href="https://www.instagram.com/thetentattheendoftheuniverse/" target="_blank">
-                <Image
-                  src={"/images/instagram.svg"}
-                  width={25}
-                  height={25} 
-                />
-                </a>
-                </div>
-                  </FirstButtonWrapper>
+                  </div>
+                </FirstButtonWrapper>
               )}
-             {link3 && (
-                <FirstButtonWrapper>
-                  <div className="sample-menu btn-wrapper" >
-                  <Link href={link3.target}>
-                    <a>
-                      <Button btnType="solid">{link3.title}</Button>
-                    </a>
-                  </Link>
-                </div>
-                  </FirstButtonWrapper>
+
+              {/* Make this one full width (spans both columns) */}
+              {link3 && (
+                <FirstButtonWrapper style={{ gridColumn: "1 / -1" }}>
+                  <div className="sample-menu btn-wrapper">
+                    <Link href={link3.target}>
+                      <a><Button btnType="solid">{link3.title}</Button></a>
+                    </Link>
+                  </div>
+                </FirstButtonWrapper>
               )}
+
+              {/* Full-width rows below */}
               {email && (
-              <div className="btn-wrapper-border" >
-                <a href={`mailto:${email}`}>
+                <div className="btn-wrapper-border" style={{ gridColumn: "1 / -1" }}>
+                  <a href={`mailto:${email}`}>
                     <Button btnType={isMobile || email === eatEmail ? "hollow" : "solid"}>
-                    {isMobile ? "email" : email}
-                  </Button>
-                </a>
-               </div>
+                      {isMobile ? "email" : email}
+                    </Button>
+                  </a>
+                </div>
               )}
-              {phone &&
-                (isMobile ? (
-                  <div className="btn-wrapper-border" >
+
+              {phone && (isMobile ? (
+                <div className="btn-wrapper-border" style={{ gridColumn: "1 / -1" }}>
                   <a href={`tel:+${phone.replace(/\s/g, "")}`}>
                     <Button btnType="hollow">call</Button>
                   </a>
-                 </div>
-                ) : (
-                  <CopyToClipboard text={phone} onCopy={() => setCopied(true)}>
-                    <div className="btn-wrapper-border" >
-                    <Button onClick={clickHandler} btnType="hollow">
-                      {phone}
-                    </Button>
+                </div>
+              ) : (
+                <CopyToClipboard text={phone} onCopy={() => setCopied(true)}>
+                  <div className="btn-wrapper-border" style={{ gridColumn: "1 / -1" }}>
+                    <Button btnType="hollow">{phone}</Button>
                   </div>
-                  </CopyToClipboard>
-                ))}
+                </CopyToClipboard>
+              ))}
             </ButtonWrapper>
+
             {copied ? (
-              <span
-                style={{
-                  textAlign: "center",
-                  textTransform: "uppercase",
-                  fontWeight: "400",
-                }}
-              >
+              <span style={{ textAlign: "center", textTransform: "uppercase", fontWeight: 400 }}>
                 Number copied
               </span>
             ) : null}
