a70ca37596d4812f24727aaba7b371214478aef1
little-portland <110906749+little-portland@users.noreply.github.com>
2025-10-17 13:17:26 +0100

    Update studio.jsx

--- pages/studio.jsx
+++ pages/studio.jsx
@@ -283,8 +283,8 @@ const Menu = ({ menuImage }) => {
          <div className="nocturn override">
            <img className="override-logo" src="/images/studio/studio-page-top.gif" alt="Studio" width="100%" />
            <img src="/images/studio/top-tagline.gif" alt="Studio" width="100%" />
-           <img className="override-logo" src="/images/studio/night-schedule.gif" alt="Studio" width="100%" />
            <img className="override-logo" src="/images/studio/logo.gif" alt="Studio" width="100%" />
+           <img className="override-logo" src="/images/studio/night-schedule.gif" alt="Studio" width="100%" />
          </div>
 
         <div className="nocturn hire">
