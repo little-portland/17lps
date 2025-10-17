1f874fdef05b43a60a9c761e6b720dba36e2d582
little-portland <110906749+little-portland@users.noreply.github.com>
2025-10-17 13:21:03 +0100

    Update studio.jsx

--- pages/studio.jsx
+++ pages/studio.jsx
@@ -57,7 +57,7 @@ const Menu = ({ menuImage }) => {
 
             @keyframes flicker {
                 0%, 100% { filter: drop-shadow(0 0 8px rgba(0,100,255,0.9)) drop-shadow(0 0 10px rgba(0,255,120,0.7)); }
-                50%      { filter: drop-shadow(0 0 10px rgba(0,100,255,1)) drop-shadow(0 0 6px rgba(0,255,120,0.9)); }
+                50%      { filter: drop-shadow(0 0 20px rgba(0,100,255,1)) drop-shadow(0 0 20px rgba(0,255,120,0.9)); }
               }
               
               .vhs-img {
