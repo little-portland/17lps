c4d33135797b5b1604b4e7ca2a4bc75e9093afc4
little-portland <110906749+little-portland@users.noreply.github.com>
2025-11-11 12:53:09 +0000

    Update booking-options-actv.jsx

--- pages/booking-options-actv.jsx
+++ pages/booking-options-actv.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Closed</h3>
-              <h3><span>STUDIO <b>//</b> </span>DAVID TRIANA & KARIM UNDER</h3>
+              <h3><span>STUDIO <b>//</b> </span>DAVID TRIANA B2B RAKIM UNDER</h3>
             </div>
         </div>
 
