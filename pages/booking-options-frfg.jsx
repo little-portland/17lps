341e13e43cab187c6bed4676ee092e9783f005d6
little-portland <110906749+little-portland@users.noreply.github.com>
2025-11-12 10:10:40 +0000

    Update booking-options-frfg.jsx

--- pages/booking-options-frfg.jsx
+++ pages/booking-options-frfg.jsx
@@ -24,29 +24,28 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-
-        <h1 className="event-name desktop-only day-category"><span className="day">ALIEN COMMUNICATIONS</span> Presents</h1>
+        <h1 className="event-name desktop-only day-category"><span className="day">Thursday</span> Underground</h1>
         
-        <h1 className="event-name mobile-only cat"><span className="cat-day">ALIEN COMMUNICATIONS</span> Presents</h1>
+        <h1 className="event-name mobile-only cat"><span className="cat-day">Thursday</span> Underground</h1>
 
         <div className="event-info">
-           <div class="info date">
-              <h2><span>THU</span>06 Nov</h2>
+            <div class="info date">
+              <h2><span>THU</span>13 NOV</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>DAN DERHAM, TEE TEE</h3>
-              <h3><span>STUDIO <b>//</b> </span>ALIEN COMMUNICATIONS B2B Ste Roberts</h3>
+              <h3><span>TENT <b>//</b> </span>MARCELINA, NATHAN COLINET</h3>
+              <h3><span>STUDIO <b>//</b> </span>Anna Wall, BECCA OGT, MAYA JANE COLES, Rob Shields</h3>
             </div>
         </div>
 
         <div className="button-wrapper new-button-wrapper mobile-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xleswwOa" target="_blank">
+            <a href="https://sevn.ly/xwQ1hQsh" target="_blank">
               CLUB ENTRY ONLY [10PM]
             </a>
           </Button>
         </div>
-       
+
         <div className="or mobile-only"><h2>Or</h2></div>
 
         <div className="book-wrapper">
@@ -69,7 +68,7 @@ const BookingOptions = () => {
 
           <div className="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button view-menu" btnType="hollow">
-              <a href="https://www.little-portland.com/override-menu" target="_blank">
+              <a href="https://www.little-portland.com/menu" target="_blank">
                 <span>View Menu</span>
               </a>
             </Button>
@@ -77,7 +76,7 @@ const BookingOptions = () => {
 
          <div class="button-wrapper new-button-wrapper desktop-only">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-10-06&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-11-13&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Free Club Entry</p>
                 <p class="details"><span>8:30PM</span></p>
@@ -106,7 +105,7 @@ const BookingOptions = () => {
          
          <div class="button-wrapper new-button-wrapper mobile-only">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-10-06&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2025-11-13&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="details"><span>8:30PM</span></p>
                 <p class="btn-tagline">The Cosmic Experience</p>
@@ -133,10 +132,10 @@ const BookingOptions = () => {
         </div>
 
         <div className="or desktop-only or-desktop"><h2>Or</h2></div>
-        
+
         <div className="button-wrapper new-button-wrapper desktop-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xleswwOa" target="_blank">
+            <a href="https://sevn.ly/xwQ1hQsh" target="_blank">
               CLUB ENTRY ONLY [10PM]
             </a>
           </Button>
