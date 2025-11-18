eda4af06eb40318dbc6acb9cb1d1b06f4ba43711
little-portland <110906749+little-portland@users.noreply.github.com>
2025-11-18 13:27:27 +0000

    Update booking-options-erts.jsx

--- pages/booking-options-erts.jsx
+++ pages/booking-options-erts.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>OLI SILVA, Semoa</h3>
-              <h3><span>STUDIO <b>//</b> HARRI PEPPER B2B LUKAS WIGFLEX</h3>
+              <h3><span>STUDIO <b>//</b> </span>HARRI PEPPER B2B LUKAS WIGFLEX</h3>
             </div>
         </div>
 
