075c6fcfc754af3aeb9cf814740483932436e17c
little-portland <110906749+little-portland@users.noreply.github.com>
2025-12-16 13:23:10 +0000

    Update booking-options-laby.jsx

--- pages/booking-options-laby.jsx
+++ pages/booking-options-laby.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>DEMI, PASH</h3>
-              <h3><span>STUDIO <b>//</b> </span>ANDY CATO, BAS IBELLINI</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
 
