2719ab15b5f69bd28f264a622c7ef10e7552d5c0
little-portland <110906749+little-portland@users.noreply.github.com>
2025-12-18 14:29:21 +0000

    Update booking-options-tthn.jsx

--- pages/booking-options-tthn.jsx
+++ pages/booking-options-tthn.jsx
@@ -24,9 +24,9 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 className="event-name desktop-only day-category disco-saturdays">Roots<span className="day">Presents</span></h1>
+        <h1 className="event-name desktop-only day-category disco-saturdays"><span className="day">Roots</span> Presents</h1>
         
-        <h1 className="event-name mobile-only cat">Roots<span className="cat-day">Presents</span></h1>
+        <h1 className="event-name mobile-only cat"><span className="cat-day">Roots</span> Presents</h1>
 
         <div className="event-info">
            <div class="info date">
