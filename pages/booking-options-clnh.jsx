0c3a5a7482c8ccc1537e69562ec1fbb0e9e652d3
little-portland <110906749+little-portland@users.noreply.github.com>
2026-01-27 10:42:31 +0000

    Update booking-options-clnh.jsx

--- pages/booking-options-clnh.jsx
+++ pages/booking-options-clnh.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>NATALIA ESCOBAR, SAM KARAM</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
+              <h3><span>STUDIO <b>//</b> </span>SEB ODYSEY</h3>
             </div>
         </div>
 
