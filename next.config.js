5ccbd322788133c10d90fe4cd47f21bbe80a8cbe
little-portland <110906749+little-portland@users.noreply.github.com>
2026-01-28 10:05:51 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,9 +9,14 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: '/matrix/api/:path*',
+        destination: 'https://api-matrix.little-portland.com/api/:path*',
+      },
+      {
         source: '/matrix/:path*',
         destination: 'https://matrix.little-portland.com/:path*',
       },
     ]
   },
 }
+
