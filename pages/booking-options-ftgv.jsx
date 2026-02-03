b88d90d275b66ce6340401802db5af93f1c61fdb
little-portland <110906749+little-portland@users.noreply.github.com>
2026-02-03 15:47:26 +0000

    Update booking-options-ftgv.jsx

--- pages/booking-options-ftgv.jsx
+++ pages/booking-options-ftgv.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>JAMES PUENTES, JOSH TWEEK</h3>
-              <h3><span>STUDIO <b>//</b> </span>B. LOVE, SWEELY</h3>
+              <h3><span>STUDIO <b>//</b> </span>B.LOVE, SWEELY</h3>
             </div>
         </div>
 
