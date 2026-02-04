3ee2c41638ef1b0cf81d3f725c31a7d4e1be8ef3
little-portland <110906749+little-portland@users.noreply.github.com>
2026-02-04 11:19:34 +0000

    Update styles.ts

--- styles/styles.ts
+++ styles/styles.ts
@@ -162,14 +162,6 @@ html, body {
   }
 }
 
-@media (hover: none) {
-  .scene-filter svg g {
-    display: block !important;
-    opacity: 1 !important;
-    visibility: visible !important;
-  }
-}
-
  .hollow-btn {
      font-family: "Antique Olive Nord D", system-ui, -apple-system, "Segoe UI", Roboto, Arial, sans-serif!important;
  }
