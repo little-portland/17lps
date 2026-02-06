377d058281577b69d0697a5b8b45a6cb52aec261
little-portland <110906749+little-portland@users.noreply.github.com>
2026-02-06 13:36:17 +0000

    Update styles.ts

--- styles/styles.ts
+++ styles/styles.ts
@@ -101,10 +101,10 @@ html, body {
       grayscale(100%)
       contrast(300%)
 
-    drop-shadow(1px 0 0 #000)
-    drop-shadow(-1px 0 0 #000)
-    drop-shadow(0 1px 0 #000)
-    drop-shadow(0 -1px 0 #000);
+    drop-shadow(0 0 #000)
+    drop-shadow(0 0 0 #000)
+    drop-shadow(0 0 0 #000)
+    drop-shadow(0 0 0 #000);
   }
 }
 
