c0ba69c0faecf7812a2ea958c52ff89ba1d072f6
little-portland <110906749+little-portland@users.noreply.github.com>
2026-02-17 12:44:39 +0000

    Update booking-options-clnh.jsx

--- pages/booking-options-clnh.jsx
+++ pages/booking-options-clnh.jsx
@@ -33,7 +33,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>06 MAR</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>FUNKTONIO, ZEYNEP EBAY</h3>
+              <h3><span>TENT <b>//</b> </span>FUNKTONIO, Zeynep Erbay</h3>
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
