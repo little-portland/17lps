3a322a8c5c1709d897f94c2fcba0b115e2e9e0f5
little-portland <110906749+little-portland@users.noreply.github.com>
2026-02-18 15:57:01 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
                     <link
                         rel="preconnect"
                         href="https://fonts.gstatic.com"
-                        crossOrigin=""
+                        crossOrigin="" 
                     />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
