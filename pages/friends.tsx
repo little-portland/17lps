ec5488b34991cb1c12326970fcd9152556f53562
little-portland <110906749+little-portland@users.noreply.github.com>
2026-03-02 15:51:41 +0000

    Update friends.tsx

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -2,14 +2,13 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 import Script from "next/script";
 
-//hooks
+// hooks
 import useDeviceDetect from "@utils/useDeviceDetect";
 
 import CenterContainer from "@components/UX/CenterContainer/CenterContainer";
 import { IFrameContainerStyle } from "@components/UX/CenterContainer/styles";
 
 const Bookings = () => {
-  //Check Device
   const { isMobile } = useDeviceDetect();
 
   const style = {
@@ -17,26 +16,18 @@ const Bookings = () => {
     height: isMobile ? "70%" : "80%",
     display: "grid",
     placeItems: "center",
-    "@media (minWidth: 500px)": {
-      display: "none",
-    },
-
-    "div.dynamic-height-adjust": {
-      display: "none",
-    },
   };
 
   useEffect(() => {
     let SevenroomsWidget;
 
     if (typeof SevenroomsWidget !== "undefined") {
-      console.log(SevenroomsWidget);
       SevenroomsWidget.init({
         venueId: "littleportland",
-        triggerId: "sr-res-root", // id of the dom element that will trigger this widget
-        type: "reservations", // either 'reservations' or 'waitlist' or 'events'
-        styleButton: true, // true if you are using the SevenRooms button
-        clientToken: "", //(Optional) Pass the api generated clientTokenId here
+        triggerId: "sr-res-root",
+        type: "reservations",
+        styleButton: true,
+        clientToken: "",
       });
     }
   }, []);
@@ -48,64 +39,123 @@ const Bookings = () => {
       </Head>
 
       <CenterContainer>
-        <IFrameContainerStyle className="bookings-iframe"
+        <IFrameContainerStyle
+          className="bookings-iframe"
           dangerouslySetInnerHTML={{
             __html:
               '<iframe src="https://www.sevenrooms.com/reservations/littleportland?client_id=726461756e9725a88ff001a4cae308fd0b5020c074f3e2a8324b11b92c890e1a9154f6d75f5af47a4328f2d005c03c4682b66fe2bcf82664bcbb340e918684fa" width="100%" height="100%"></iframe>',
           }}
           style={style}
         />
-       <h2 className="booking-heading">For bookings of 6 guests or more in The Tent or private dining options, please contact the restaurant directly at         
-         <br/>  
-        <a href="mailto:eat@little-portland.com">eat@little-portland.com</a></h2> 
-        <div className="bookings-body-text"> 
-         <h1 className="bookings-heading-0">DINNER RESERVATIONS</h1>
+
+        <h2 className="booking-heading">
+          For bookings of 6 guests or more in The Tent or private dining
+          options, please contact the restaurant directly at
+          <br />
+          <a href="mailto:eat@little-portland.com">
+            eat@little-portland.com
+          </a>
+        </h2>
+
+        <div className="bookings-body-text">
+          <h1 className="bookings-heading-0">DINNER RESERVATIONS</h1>
           <ul>
-            <li>For all dinner reservations in the Tent, we have a specially designed set menu that is currently inspired by Indonesian cuisine focused around sharing food and eating with your hands. </li>
-            <li>Please note, although we strive to cater for dietary restrictions, our kitchen space is very limited to accommodate every dietary requirement. Unfortunately we cannot guarantee changes outside our set menu. Please notify a member of staff before booking if you suffer from any allergies so we can try to meet your needs.
- </li>
-            <li>Cancellations within 48 hours prior to your dinner reservation, group reduction size or no-shows will be subject to a £30 per person cancellation fee. </li>
-            <li>For large groups of 6 of more guests, or any changes to reservations, please contact <a href="mailto:eat@little-portland.com">eat@little-portland.com</a> </li>
-          </ul>
-          
-          <h1 className="bookings-heading-1">CHEF’S STUDIO</h1>
-          <ul>
-            <li>The Chef’s Studio is a private dining space located beneath the Tent and can cater between 6 - 12 guests at 8pm between Thursdays - Saturdays </li>
-            <li>To confirm a reservation, we will require a credit card guarantee. This will only be chargeable at £30 per person in the event of a cancellation, late reductions in group size (within 7 days), or no-show on the day of the reservation.</li>
-            <li>We unfortunately cannot cater to any dietary requirements.</li>
-            <li>To book or modify an existing reservation, please contact <a href="mailto:eat@little-portland.com">eat@little-portland.com</a></li>
+            <li>
+              For all dinner reservations in the Tent, we have a specially
+              designed set menu that is currently inspired by Indonesian
+              cuisine focused around sharing food and eating with your hands.
+            </li>
+            <li>
+              Please note, although we strive to cater for dietary restrictions,
+              our kitchen space is very limited to accommodate every dietary
+              requirement. Unfortunately we cannot guarantee changes outside our
+              set menu. Please notify a member of staff before booking if you
+              suffer from any allergies so we can try to meet your needs.
+            </li>
+            <li>
+              Cancellations within 48 hours prior to your dinner reservation,
+              group reduction size or no-shows will be subject to a £30 per
+              person cancellation fee.
+            </li>
+            <li>
+              For large groups of 6 of more guests, or any changes to
+              reservations, please contact{" "}
+              <a href="mailto:eat@little-portland.com">
+                eat@little-portland.com
+              </a>
+            </li>
           </ul>
 
-          <h1 className="bookings-heading-1">PRIVATE HIRE + PRIVATE DINING</h1>
+          <h1 className="bookings-heading-1">CHEF’S STUDIO</h1>
           <ul>
-            <li>Both The Tent and Studio are available for private hire. Contact <a href="mailto:yo@little-portland.com">yo@little-portland.com</a> for more information.</li>
-            <li>We also offer canapés for Tent or Full venue exclusive events. </li>
+            <li>
+              The Chef’s Studio is a private dining space located beneath the
+              Tent and can cater between 6 - 12 guests at 8pm between Thursdays
+              - Saturdays
+            </li>
+            <li>
+              To confirm a reservation, we will require a credit card guarantee.
+              This will only be chargeable at £30 per person in the event of a
+              cancellation, late reductions in group size (within 7 days), or
+              no-show on the day of the reservation.
+            </li>
+            <li>We unfortunately cannot cater to any dietary requirements.</li>
+            <li>
+              To book or modify an existing reservation, please contact{" "}
+              <a href="mailto:eat@little-portland.com">
+                eat@little-portland.com
+              </a>
+            </li>
           </ul>
-          
+
+          <h1 className="bookings-heading-1">
+            PRIVATE HIRE + PRIVATE DINING
+          </h1>
+          <ul>
+            <li>
+              Both The Tent and Studio are available for private hire. Contact{" "}
+              <a href="mailto:yo@little-portland.com">
+                yo@little-portland.com
+              </a>{" "}
+              for more information.
+            </li>
+            <li>
+              We also offer canapés for Tent or Full venue exclusive events.
+            </li>
+          </ul>
+
           <h1 className="bookings-heading-2">MISCELLANEOUS</h1>
           <ul>
-            <li>All menus are subject to change per each override special guest chef curated menu. </li>
-            <li>Anyone under the age of 18 is not permitted to enter the venue. </li>
-            <li>Dogs are not permitted to enter the venue. </li>
-            <li>No photos or videos are permitted to be taken inside the venue. </li>
+            <li>All menus are subject to change.</li>
+            <li>
+              Anyone under the age of 18 is not permitted to enter the venue.
+            </li>
+            <li>Dogs are not permitted to enter the venue.</li>
+            <li>
+              No photos or videos are permitted to be taken inside the venue.
+            </li>
             <li>Reservations can be made two months in advance.</li>
             <li>We have a relaxed casual dress code.</li>
           </ul>
         </div>
-
       </CenterContainer>
-          <style jsx global>{`
-        
-        //Bookings iframe specific styles
-         .bookings-container {
+
+      <style jsx global>{`
+        .bookings-container {
           position: absolute;
           padding: 5rem;
-         }
-        
-        .bookings-iframe {
-          margin-top: -2rem!important;
         }
-        
+
+        .bookings-iframe {
+          margin-top: -2rem !important;
+        }
+
+        @media (min-width: 500px) {
+          .bookings-iframe {
+            display: none;
+          }
+        }
+
         .bookings-body-text {
           text-align: left;
           padding: 5px 20px;
@@ -113,15 +163,17 @@ const Bookings = () => {
           margin-bottom: 1.5rem;
           max-width: 80%;
         }
-        
+
         .bookings-body-text a {
           cursor: pointer;
         }
-        
-        .bookings-body-text a, .bookings-body-text a:hover, .bookings-body-text a:focus {
+
+        .bookings-body-text a,
+        .bookings-body-text a:hover,
+        .bookings-body-text a:focus {
           color: rgb(92, 44, 124);
         }
-        
+
         .booking-heading {
           font-size: 20px;
           margin-bottom: 20px;
@@ -130,48 +182,48 @@ const Bookings = () => {
           line-height: 2rem;
           text-transform: uppercase;
         }
-        
-        .booking-heading a, .booking-heading a:hover, .booking-heading a:focus {
+
+        .booking-heading a,
+        .booking-heading a:hover,
+        .booking-heading a:focus {
           color: rgb(92, 44, 124);
         }
-        
+
         .bookings-body-text h1 {
           margin-bottom: 1rem;
         }
-        
-        .bookings-heading-0, .bookings-heading-1, .bookings-heading-2 {
+
+        .bookings-heading-0,
+        .bookings-heading-1,
+        .bookings-heading-2 {
           font-family: "Space Mono" !important;
           font-weight: 900;
           font-style: normal;
           font-size: 1.1rem;
           text-transform: uppercase;
         }
-        
+
         .bookings-heading-0 {
           margin-top: 1rem;
         }
-        
-        .bookings-heading-1 {
-          margin-top: 2rem;
-        }
-        
+
+        .bookings-heading-1,
         .bookings-heading-2 {
           margin-top: 2rem;
         }
-        
+
         .bookings-body-text ul {
-          list-style-type:square;
+          list-style-type: square;
           margin: 10px;
         }
-        
+
         .bookings-body-text ul li {
-          font-size: .8rem;
+          font-size: 0.8rem;
           font-weight: 600;
           margin-bottom: 10px;
           line-height: 15px;
         }
-        
-        `}</style>
+      `}</style>
     </>
   );
 };
