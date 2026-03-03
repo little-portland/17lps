ac63025755b15afc2c437ebbc6c4b843974463de
little-portland <110906749+little-portland@users.noreply.github.com>
2026-03-03 16:08:46 +0000

    Update booking-options-azge.jsx

--- pages/booking-options-azge.jsx
+++ pages/booking-options-azge.jsx
@@ -24,22 +24,22 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 className="event-name desktop-only day-category disco-saturdays"><span className="day">AIR KARNAK</span> Presents</h1>        
-        <h1 className="event-name mobile-only cat"><span className="cat-day">AIR KARNAK</span> Presents</h1>
+        <h1 className="event-name desktop-only day-category disco-saturdays"><span className="day">ND Beaumecker</span> Presents</h1>        
+        <h1 className="event-name mobile-only cat"><span className="cat-day">ND Beaumecker</span> Presents</h1>
 
         <div className="event-info">
            <div class="info date">
-              <h2><span>SAT</span>28 Feb</h2>
+              <h2><span>SAT</span>04 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>KRISTINA MAY, LEX WOLF</h3>
-              <h3><span>STUDIO <b>//</b> </span>DER, MAXIME DB</h3>
+              <h3><span>TENT <b>//</b> </span>Anomalous_DJ, MAYBE LAURA & REMI MAZET</h3>
+              <h3><span>STUDIO <b>//</b> </span>Ben Gomori, ND Beaumecker</h3>
             </div>
         </div>
 
         <div className="button-wrapper new-button-wrapper mobile-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/x69yBVeb" target="_blank">
+            <a href="https://sevn.ly/xU37SQeO" target="_blank">
               CLUB ENTRY ONLY [10PM]
             </a>
           </Button>
@@ -75,7 +75,7 @@ const BookingOptions = () => {
 
          <div class="button-wrapper new-button-wrapper desktop-only">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2026-02-28&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2026-04-04&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Club Entry</p>
                 <p class="details"><span>8:30PM</span></p>
@@ -104,7 +104,7 @@ const BookingOptions = () => {
          
          <div class="button-wrapper new-button-wrapper mobile-only">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2026-02-28&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2026-04-04&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="details"><span>8:30PM</span></p>
                 <p class="btn-tagline">The Cosmic Experience</p>
@@ -134,7 +134,7 @@ const BookingOptions = () => {
         
         <div className="button-wrapper new-button-wrapper desktop-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/x69yBVeb" target="_blank">
+            <a href="https://sevn.ly/xU37SQeO" target="_blank">
               CLUB ENTRY ONLY [10PM]
             </a>
           </Button>
