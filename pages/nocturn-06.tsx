3601b230ae4d47e3251b8f2868c267f7542cabd5
little-portland <110906749+little-portland@users.noreply.github.com>
2026-03-04 20:53:59 +0000

    Update nocturn-06.tsx

--- pages/nocturn-06.tsx
+++ pages/nocturn-06.tsx
@@ -439,7 +439,7 @@ const Menu = ({ menuImage }) => {
         
         @keyframes gradientMoveDesktop {
           0%   { background-position: 25% 50%; }
-          50%  { background-position: 55% 50%; }
+          50%  { background-position: 100% 50%; }
           100% { background-position: 25% 50%; }
         }
         
