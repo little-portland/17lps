3c1c998c030662e5bb79da031d3385db51198665
little-portland <110906749+little-portland@users.noreply.github.com>
2026-03-06 13:37:39 +0000

    Update booking-options-tthn.jsx

--- pages/booking-options-tthn.jsx
+++ pages/booking-options-tthn.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Veronica Lanotte, Vitto</h3>
-              <h3><span>STUDIO <b>//</b> </span>Dandy Jack, David Triana</h3>
+              <h3><span>STUDIO <b>//</b> </span>DANDY JACK & HIS COMETS, DAVID TRIANA</h3>
             </div>
         </div>
 
