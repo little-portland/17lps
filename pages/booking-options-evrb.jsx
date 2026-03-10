b3bf86f46030e37935694fa2e8e13b782672ecbd
little-portland <110906749+little-portland@users.noreply.github.com>
2026-03-10 11:47:51 +0000

    Update booking-options-evrb.jsx

--- pages/booking-options-evrb.jsx
+++ pages/booking-options-evrb.jsx
@@ -24,22 +24,22 @@ const BookingOptions = () => {
       </Head>
 
       <div id="bookingOptions">
-        <h1 className="event-name desktop-only day-category disco-saturdays"><span className="day">AIR KARNAK</span> Presents</h1>        
-        <h1 className="event-name mobile-only cat"><span className="cat-day">AIR KARNAK</span> Presents</h1>
+        <h1 className="event-name desktop-only day-category"><span className="day">Openlab</span> Presents</h1>
+        <h1 className="event-name mobile-only cat"><span className="cat-day">Openlab</span> Presents</h1>
 
         <div className="event-info">
            <div class="info date">
-              <h2><span>SAT</span>28 Feb</h2>
+              <h2><span>SAT</span>28 Mar</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>KRISTINA MAY, LEX WOLF</h3>
-              <h3><span>STUDIO <b>//</b> </span>DER, MAXIME DB</h3>
+              <h3><span>TENT <b>//</b> </span>Sally Rogers, TinTin</h3>
+              <h3><span>STUDIO <b>//</b> </span>Juan Ramos, Wallace</h3>
             </div>
         </div>
 
         <div className="button-wrapper new-button-wrapper mobile-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xL0PATl5" target="_blank">
+            <a href="https://sevn.ly/xrb9JWdn" target="_blank">
               CLUB ENTRY ONLY [10PM]
             </a>
           </Button>
@@ -67,7 +67,7 @@ const BookingOptions = () => {
 
           <div className="button-wrapper new-button-wrapper">
             <Button classes="events-button new-events-button view-menu" btnType="hollow">
-              <a href="https://www.little-portland.com/menu" target="_blank">
+              <a href="https://www.little-portland.com/override-menu" target="_blank">
                 <span>View Menu</span>
               </a>
             </Button>
@@ -75,7 +75,7 @@ const BookingOptions = () => {
 
          <div class="button-wrapper new-button-wrapper desktop-only">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2026-02-28&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2026-03-28&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="time">Includes Club Entry</p>
                 <p class="details"><span>8:30PM</span></p>
@@ -104,7 +104,7 @@ const BookingOptions = () => {
          
          <div class="button-wrapper new-button-wrapper mobile-only">
             <Button classes="events-button new-events-button"  btnType="hollow">
-              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2026-02-28&default_time=21:00&default_party_size=4" target="_blank">
+              <a href="https://www.sevenrooms.com/reservations/littleportland?default_date=2026-03-28&default_time=21:00&default_party_size=4" target="_blank">
                 <span class="space">IN THE TENT</span>
                 <p class="details"><span>8:30PM</span></p>
                 <p class="btn-tagline">The Cosmic Experience</p>
@@ -131,10 +131,9 @@ const BookingOptions = () => {
         </div>
 
         <div className="or desktop-only or-desktop"><h2>Or</h2></div>
-        
         <div className="button-wrapper new-button-wrapper desktop-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xL0PATl5" target="_blank">
+            <a href="https://sevn.ly/xrb9JWdn" target="_blank">
               CLUB ENTRY ONLY [10PM]
             </a>
           </Button>
