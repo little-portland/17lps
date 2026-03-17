cc2fe5a81aebfab8095e2dd0a96d1be7e50fb309
little-portland <110906749+little-portland@users.noreply.github.com>
2026-03-17 17:47:37 +0000

    Update explore-menu.jsx

--- pages/explore-menu.jsx
+++ pages/explore-menu.jsx
@@ -1,15 +1,9 @@
 import dynamic from "next/dynamic";
 
-// Disable SSR (flipbook needs browser)
-const FlipBook = dynamic(
-  () => import("../components/FlipBook"),
-  { ssr: false }
-);
+const FlipBook = dynamic(() => import("../components/FlipBook"), {
+  ssr: false,
+});
 
-export default function EbookPage() {
-  return (
-    <div style={{ padding: 40 }}>
-      <FlipBook />
-    </div>
-  );
+export default function ExploreMenuPage() {
+  return <FlipBook />;
 }
