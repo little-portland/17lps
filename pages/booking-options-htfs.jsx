18fd7dc546746719c6da95aa1465b58d6906f7d2
little-portland <110906749+little-portland@users.noreply.github.com>
2026-03-24 12:38:14 +0000

    Update booking-options-htfs.jsx

--- pages/booking-options-htfs.jsx
+++ pages/booking-options-htfs.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>LUTHER VINE, MARCELINA WICK</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, JONNY ROCK</h3>
             </div>
         </div>
 
