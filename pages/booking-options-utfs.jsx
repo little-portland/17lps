4c8a252039042d286c1d2a92d3e1362f13580fcf
little-portland <110906749+little-portland@users.noreply.github.com>
2026-03-31 11:11:44 +0100

    Update booking-options-utfs.jsx

--- pages/booking-options-utfs.jsx
+++ pages/booking-options-utfs.jsx
@@ -33,7 +33,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>17 APR</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>TONY SERBAN & BABYBASS</h3>
+              <h3><span>TENT <b>//</b> </span>BABYBASS & TONY SERBAN</h3>
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
