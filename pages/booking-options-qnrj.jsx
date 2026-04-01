e656717c44efc2ed3ea328c2588352463b78f9af
little-portland <110906749+little-portland@users.noreply.github.com>
2026-04-01 11:40:41 +0100

    Update booking-options-qnrj.jsx

--- pages/booking-options-qnrj.jsx
+++ pages/booking-options-qnrj.jsx
@@ -33,7 +33,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>03 Apr</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>TIM LANG, ZEYNEP ERBAY</h3>
+              <h3><span>TENT <b>//</b> </span>Roberta Cutolo, ZEYNEP ERBAY</h3>
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
