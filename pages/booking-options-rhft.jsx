d30901d166ef520609aac36b5f6616583d86b3fc
little-portland <110906749+little-portland@users.noreply.github.com>
2026-04-13 14:37:19 +0100

    Update booking-options-rhft.jsx

--- pages/booking-options-rhft.jsx
+++ pages/booking-options-rhft.jsx
@@ -33,7 +33,7 @@ const BookingOptions = () => {
               <h2><span>THU</span>16 APR</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>BELIZ, NATHAN COLINET</h3>
+              <h3><span>TENT <b>//</b> </span>Beliz, Kaan Duzarat, NATHAN COLINET</h3>
               <h3><span>STUDIO <b>//</b> </span>GARAN GARAN, KC HAPPY, TOMOKI TAMURA</h3>
             </div>
         </div>
