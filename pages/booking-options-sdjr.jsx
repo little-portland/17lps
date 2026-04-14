09333c24ab006b2d6468c32dac441e35eff35bbb
little-portland <110906749+little-portland@users.noreply.github.com>
2026-04-14 17:01:41 +0100

    Update booking-options-sdjr.jsx

--- pages/booking-options-sdjr.jsx
+++ pages/booking-options-sdjr.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>KRISTINA MAY & GeorgiOooOoO</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, Cesar Merveille</h3>
             </div>
         </div>
 
