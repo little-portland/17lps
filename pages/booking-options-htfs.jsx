b014e2d1382132dd46b792ae19b73220a4184620
little-portland <110906749+little-portland@users.noreply.github.com>
2026-04-14 11:07:49 +0100

    Update booking-options-htfs.jsx

--- pages/booking-options-htfs.jsx
+++ pages/booking-options-htfs.jsx
@@ -33,7 +33,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>01 MAY</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>TIM LANG</h3>
+              <h3><span>TENT <b>//</b> </span>Mr Shiver, TIM LANG</h3>
               <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
