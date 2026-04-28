6f778a78c5f3f4dd1eccc5cdebdb505b51c220fb
little-portland <110906749+little-portland@users.noreply.github.com>
2026-04-28 10:21:00 +0100

    Update booking-options-htfs.jsx

--- pages/booking-options-htfs.jsx
+++ pages/booking-options-htfs.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>TIM LANG, Zeynep Erbay</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, ZEINA</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
 
