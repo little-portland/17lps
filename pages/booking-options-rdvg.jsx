80e607dcda08fdb7e2014bd02937aa3f48d7a528
little-portland <110906749+little-portland@users.noreply.github.com>
2026-05-04 20:54:44 +0100

    Update booking-options-rdvg.jsx

--- pages/booking-options-rdvg.jsx
+++ pages/booking-options-rdvg.jsx
@@ -33,8 +33,8 @@ const BookingOptions = () => {
               <h2><span>SAT</span>09 MAY</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>GABRIEL RAI, Ché Wax</h3>
-              <h3><span>STUDIO <b>//</b> </span>ROB MELLO B2B JONNY ROCK</h3>
+              <h3><span>TENT <b>//</b> </span>Ché Wax, GABRIEL RAI</h3>
+              <h3><span>STUDIO <b>//</b> </span>JONNY ROCK B2B ROB MELLO</h3>
             </div>
         </div>
 
