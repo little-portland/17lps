74a55c1c615241d168e1445fafa7d200dc5b52e7
little-portland <110906749+little-portland@users.noreply.github.com>
2026-05-05 09:53:30 +0100

    Update booking-options-qnrj.jsx

--- pages/booking-options-qnrj.jsx
+++ pages/booking-options-qnrj.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>MR SHIVER, OLI SILVA</h3>
-              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
+              <h3><span>STUDIO <b>//</b> </span>Closed</h3>
             </div>
         </div>
 
