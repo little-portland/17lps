c19e981d857ec98b9d15810d18e22f7bb87e4259
little-portland <110906749+little-portland@users.noreply.github.com>
2026-05-11 17:07:56 +0100

    Update booking-options-erts.jsx

--- pages/booking-options-erts.jsx
+++ pages/booking-options-erts.jsx
@@ -33,8 +33,8 @@ const BookingOptions = () => {
               <h2><span>THU</span>21 May</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>JOSH KEIT, LOA SZALA</h3>
-              <h3><span>STUDIO <b>//</b> </span>SATOSHI TOMIIE</h3>
+              <h3><span>TENT <b>//</b> </span>JOSH KEIT, LOA SZALA, NATE OLDFIELD</h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI, SATOSHI TOMIIE</h3>
             </div>
         </div>
 
