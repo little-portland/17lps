a5aa6b5451b5e86afcb37ede6bea5a51460c7245
little-portland <110906749+little-portland@users.noreply.github.com>
2026-05-13 11:09:24 +0100

    Update booking-options-mjrs.jsx

--- pages/booking-options-mjrs.jsx
+++ pages/booking-options-mjrs.jsx
@@ -34,7 +34,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>KC HAPPY, KRISTINA MAY</h3>
-              <h3><span>STUDIO <b>//</b> </span>CEM OZDEN, GEORGIA, ELIA</h3>
+              <h3><span>STUDIO <b>//</b> </span>CEM OZDEN, elia nafzger, GEORGIA</h3>
             </div>
         </div>
 
