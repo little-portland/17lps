f2a76fd4000c9c855ece5e5725a92d3cd4570c93
little-portland <110906749+little-portland@users.noreply.github.com>
2026-05-26 12:47:09 +0100

    Update booking-options-dcft.jsx

--- pages/booking-options-dcft.jsx
+++ pages/booking-options-dcft.jsx
@@ -32,7 +32,7 @@ const BookingOptions = () => {
               <h2><span>SAT</span>13 Jun</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>KC HAPPY, MAYBE LAURA & REMI MAZET</h3>
+              <h3><span>TENT <b>//</b> </span>MAYBE LAURA & REMI MAZET, OLI SILVA</h3>
               <h3><span>STUDIO <b>//</b> </span>Emel Rowe, DER</h3>
             </div>
         </div>
