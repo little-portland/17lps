92c28147148b9d7b5a52c15edea266471a5e463c
little-portland <110906749+little-portland@users.noreply.github.com>
2026-07-07 12:19:22 +0100

    Update booking-options-dgtr.jsx

--- pages/booking-options-dgtr.jsx
+++ pages/booking-options-dgtr.jsx
@@ -35,7 +35,7 @@ const BookingOptions = () => {
               <h2><span>THU</span>16 JUL</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>KC HAPPY, PAUL DAY </h3>
+              <h3><span>TENT <b>//</b> </span>KC HAPPY, Nathan Colinet </h3>
               <h3><span>STUDIO <b>//</b> </span>CEM OZDEN, DANA RUH</h3>
             </div>
         </div>
