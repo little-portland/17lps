f1758fae8ff6f1a5bf09fb18a117f1f4549bcc4e
little-portland <110906749+little-portland@users.noreply.github.com>
2026-07-21 12:57:39 +0100

    Update booking-options-bcdf.jsx

--- pages/booking-options-bcdf.jsx
+++ pages/booking-options-bcdf.jsx
@@ -31,7 +31,7 @@ const BookingOptions = () => {
         
         <div className="event-info">
            <div class="info date">
-              <h2><span>SAT</span>08 Aug</h2>
+              <h2><span>SAT</span>01 Aug</h2>
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Denny Kem, Trixie</h3>
