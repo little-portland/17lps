c4f3267c909057411052eae7418a44e267b65d49
little-portland <110906749+little-portland@users.noreply.github.com>
2026-07-21 14:01:41 +0100

    Update booking-options-dgtr.jsx

--- pages/booking-options-dgtr.jsx
+++ pages/booking-options-dgtr.jsx
@@ -35,7 +35,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>FRANKIE VALENTINE, SPIKA</h3>
-              <h3><span>STUDIO <b>//</b> </span>ADRIEN CALVET, MAYA PEOPLE, VITO</h3>
+              <h3><span>STUDIO <b>//</b> </span>ADRIEN CALVET, VITTO</h3>
             </div>
         </div>
 
