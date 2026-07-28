c7b49a65e15a82d3fad8ac2f8f3a907c9f61f3d5
little-portland <110906749+little-portland@users.noreply.github.com>
2026-07-28 11:22:56 +0100

    Update booking-options-sdjr.jsx

--- pages/booking-options-sdjr.jsx
+++ pages/booking-options-sdjr.jsx
@@ -36,7 +36,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>KRESHNICK, ROBERTA CUTOLO</h3>
-              <h3><span>STUDIO <b>//</b> </span>LUTHER VINE, Tim Lang</h3>
+              <h3><span>STUDIO <b>//</b> </span>Funktonio, LUTHER VINE</h3>
             </div>
         </div>
 
