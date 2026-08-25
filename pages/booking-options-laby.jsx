16680b5d2ac9404e99af3a605356d1ec6eb23b24
little-portland <110906749+little-portland@users.noreply.github.com>
2026-08-25 11:49:55 +0100

    Update booking-options-laby.jsx

--- pages/booking-options-laby.jsx
+++ pages/booking-options-laby.jsx
@@ -35,7 +35,7 @@ const BookingOptions = () => {
               <h2><span>FRI</span>28 Aug</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>KC HAPPY, Pato</h3>
+              <h3><span>TENT <b>//</b> </span>Kerosh, Pato</h3>
               <h3><span>STUDIO <b>//</b> </span>THE MOLE</h3>
             </div>
         </div>
