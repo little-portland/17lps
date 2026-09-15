60a97eef6fdea2e598c22960ab655d58950a5515
little-portland <110906749+little-portland@users.noreply.github.com>
2026-09-15 10:42:21 +0100

    Update event date and artist lineup

--- pages/booking-options-laby.jsx
+++ pages/booking-options-laby.jsx
@@ -32,17 +32,17 @@ const BookingOptions = () => {
 
         <div className="event-info">
            <div class="info date">
-              <h2><span>FRI</span>28 Aug</h2>
+              <h2><span>FRI</span>02 OCt</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>Kerosh, Pato</h3>
-              <h3><span>STUDIO <b>//</b> </span>THE MOLE</h3>
+              <h3><span>TENT <b>//</b> </span>FRANKIE VALENTINE, PAUL DAY</h3>
+              <h3><span>STUDIO <b>//</b> </span>FELIX DICKINSON</h3>
             </div>
         </div>
 
         <div className="button-wrapper new-button-wrapper mobile-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xTnJjrkZ" target="_blank">
+            <a href="https://sevn.ly/xGkWrbhs" target="_blank">
               CLUB ENTRY [10PM]
             </a>
           </Button>
@@ -50,7 +50,7 @@ const BookingOptions = () => {
         
         <div className="button-wrapper new-button-wrapper desktop-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xTnJjrkZ" target="_blank">
+            <a href="https://sevn.ly/xGkWrbhs" target="_blank">
               CLUB ENTRY [10PM]
             </a>
           </Button>
