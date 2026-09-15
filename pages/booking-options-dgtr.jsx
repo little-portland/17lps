12b342fe7da22e084a26e14af32853c479ce98f8
little-portland <110906749+little-portland@users.noreply.github.com>
2026-09-15 10:45:34 +0100

    Update booking-options-dgtr.jsx

--- pages/booking-options-dgtr.jsx
+++ pages/booking-options-dgtr.jsx
@@ -26,22 +26,22 @@ const BookingOptions = () => {
     </Head>
 
       <div id="bookingOptions">
-        <h1 className="event-name desktop-only day-category"><span className="day">BLANCHE</span> Presents</h1> 
-        <h1 className="event-name mobile-only cat"><span className="cat-day">BLANCHE</span> Presents</h1>
+        <h1 className="event-name desktop-only day-category"><span className="day">Openblab</span> Presents</h1> 
+        <h1 className="event-name mobile-only cat"><span className="cat-day">Openblab</span> Presents</h1> 
         
         <div className="event-info">
            <div class="info date">
-              <h2><span>SAT</span>12 Sep</h2>
+              <h2><span>SAT</span>03 Oct</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>MATRIX, Wolf Drifta</h3>
-              <h3><span>STUDIO <b>//</b> </span>Cesar Merveille & Viken Arman</h3>
+              <h3><span>TENT <b>//</b> </span>ARIANE V</h3>
+              <h3><span>STUDIO <b>//</b> </span>LOGAN FISHER, TINTIN</h3>
             </div>
         </div>
 
         <div className="button-wrapper new-button-wrapper mobile-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/x7BMAkfH" target="_blank">
+            <a href="https://sevn.ly/xaUx6q7m" target="_blank">
               CLUB ENTRY [10PM]
             </a>
           </Button>
@@ -49,7 +49,7 @@ const BookingOptions = () => {
         
         <div className="button-wrapper new-button-wrapper desktop-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/x7BMAkfH" target="_blank">
+            <a href="https://sevn.ly/xaUx6q7m" target="_blank">
               CLUB ENTRY [10PM]
             </a>
           </Button>
