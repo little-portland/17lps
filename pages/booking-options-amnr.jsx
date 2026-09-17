288ba97d4601bb9dede738434ca6c112d17aafa8
little-portland <110906749+little-portland@users.noreply.github.com>
2026-09-17 17:05:53 +0100

    Change event day from Friday to Saturday

--- pages/booking-options-amnr.jsx
+++ pages/booking-options-amnr.jsx
@@ -10,10 +10,10 @@ import Button from "@components/UX/Button";
 
 const BookingOptions = () => {
   useEffect(() => {
-    document.body.classList.add("friday");
+    document.body.classList.add("saturday");
 
     return () => {
-      document.body.classList.remove("friday");
+      document.body.classList.remove("saturday");
     };
   }, []);
 
@@ -26,23 +26,22 @@ const BookingOptions = () => {
     </Head>
 
       <div id="bookingOptions">
-        <h1 className="event-name desktop-only day-category"><span className="day">Friday</span> Residents</h1>
+        <h1 className="event-name desktop-only day-category"><span className="day">Openblab</span> Presents</h1> 
+        <h1 className="event-name mobile-only cat"><span className="cat-day">Openblab</span> Presents</h1> 
         
-        <h1 className="event-name mobile-only cat"><span className="cat-day">Friday</span> Residents</h1>
-
         <div className="event-info">
            <div class="info date">
-              <h2><span>FRI</span>04 SEP</h2>
+              <h2><span>SAT</span>14 Noc</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>Ché Wax b2b DMC</h3>
-              <h3><span>STUDIO <b>//</b> </span>Bas Ibellini</h3>
+              <h3><span>TENT <b>//</b> </span>TBC</h3>
+              <h3><span>STUDIO <b>//</b> </span>CHEZ DE MILO, RALPH LAWSON</h3>
             </div>
         </div>
 
         <div className="button-wrapper new-button-wrapper mobile-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xoYS3uLZ" target="_blank">
+            <a href="https://sevn.ly/x9D9Brx3" target="_blank">
               CLUB ENTRY [10PM]
             </a>
           </Button>
@@ -50,7 +49,7 @@ const BookingOptions = () => {
         
         <div className="button-wrapper new-button-wrapper desktop-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xoYS3uLZ" target="_blank">
+            <a href="https://sevn.ly/x9D9Brx3" target="_blank">
               CLUB ENTRY [10PM]
             </a>
           </Button>
