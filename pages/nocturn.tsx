c8fd77db462b5c41771079b67bf3eaedce9c2073
little-portland <110906749+little-portland@users.noreply.github.com>
2026-09-21 10:54:42 +0100

    Remove links from flyer images in nocturn.tsx
    Removed links from flyer images in nocturn page.

--- pages/nocturn.tsx
+++ pages/nocturn.tsx
@@ -64,14 +64,10 @@ const Menu = ({ menuImage }) => {
            <div className="nocturn-wider-section">
             <div className="row">
                <div className="column">
-                  <a href="https://www.little-portland.com/booking-options-bcdf" target="_blank">
                      <img className="flyer" src="/images/nocturn/quiet_husband_noct_afterhours.jpg" />
-                  </a>
                  </div>
                <div className="column">
-                   <a href="https://sevn.ly/x7vHV9je" target="_blank">
                      <img className="flyer" src="/images/nocturn/cosmogram-of-eros-flyer.gif" />
-                   </a>
                  </div>
               </div>
               
