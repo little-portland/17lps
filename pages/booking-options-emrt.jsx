a31dfbef38b31a9fdba85e55bb978e62d4638872
little-portland <110906749+little-portland@users.noreply.github.com>
2026-09-22 09:43:06 +0100

    Update event date and artist lineup

--- pages/booking-options-emrt.jsx
+++ pages/booking-options-emrt.jsx
@@ -32,17 +32,17 @@ const BookingOptions = () => {
 
         <div className="event-info">
            <div class="info date">
-              <h2><span>FRI</span>18 Sep</h2>
+              <h2><span>FRI</span>09 Oct</h2>
             </div>
             <div class="info artists">
-              <h3><span>TENT <b>//</b> </span>Jos, VALENTINA PAHOR</h3>
-              <h3><span>STUDIO <b>//</b> </span>Daylomar, Zeina</h3>
+              <h3><span>TENT <b>//</b> </span>Kaan Düzarat, THE MUHSROOM EXPLORER </h3>
+              <h3><span>STUDIO <b>//</b> </span>BAS IBELLINI</h3>
             </div>
         </div>
 
         <div className="button-wrapper new-button-wrapper mobile-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xSw2KcH9" target="_blank">
+            <a href="https://sevn.ly/xfUwpbd6" target="_blank">
               CLUB ENTRY [10PM]
             </a>
           </Button>
@@ -50,7 +50,7 @@ const BookingOptions = () => {
         
         <div className="button-wrapper new-button-wrapper desktop-only">
           <Button classes="events-button ticketNew" btnType="hollow">
-            <a href="https://sevn.ly/xSw2KcH9" target="_blank">
+            <a href="https://sevn.ly/xfUwpbd6" target="_blank">
               CLUB ENTRY [10PM]
             </a>
           </Button>
