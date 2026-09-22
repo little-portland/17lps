4f880291da855003225e54b38c7111e241983e1f
little-portland <110906749+little-portland@users.noreply.github.com>
2026-09-22 10:53:05 +0100

    Update booking-options-rhft.jsx

--- pages/booking-options-rhft.jsx
+++ pages/booking-options-rhft.jsx
@@ -36,7 +36,7 @@ const BookingOptions = () => {
             </div>
             <div class="info artists">
               <h3><span>TENT <b>//</b> </span>Giacomo XL, Zeynep Erbay</h3>
-              <h3><span>STUDIO <b>//</b> </span>Paolo Bartolomeo, Ricky Montanit</h3>
+              <h3><span>STUDIO <b>//</b> </span>Paolo Bartolomeo, Ricky Montanari</h3>
             </div>
         </div>
 
